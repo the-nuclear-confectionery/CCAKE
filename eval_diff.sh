f299957dad0fa80daf711b764f3e0c29ac08a91e
Plumberg <plumberg@cc-login1.campuscluster.illinois.edu>
2021-04-12 16:40:34 -0500

    Change to v-USPhydro2.

--- eval_diff.sh
+++ eval_diff.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
-# clone BSQ and EBE-vUSPhydro, then: cd ./BSQ
+# clone BSQ and vUSPhydro2, then: cd ./BSQ
 
 BSQ_dir="../BSQ/Hydro-master/"
-EBE_dir="../EBE-vUSPhydro/EBE-Node/v-USPhydro/"
+EBE_dir="../v-USPhydro2/"
 OUT_dir="../BSQ/textfiles/"
 changes_file="../BSQ/textfiles/changes.txt"
 
 # diff between whole directories
-diff -q $BSQ_dir $EBE_dir >> full_diff_EBE-vUSPhydro_BSQ-Hydro-master.txt
+diff -q $BSQ_dir $EBE_dir >> full_diff_vUSPhydro2_BSQ-Hydro-master.txt
 
 # list files that differ
-diff -q $BSQ_dir $EBE_dir | grep "differ" >> changed_files_EBE-vUSPhydro_BSQ-Hydro-master.txt
+diff -q $BSQ_dir $EBE_dir | grep "differ" >> changed_files_vUSPhydro2_BSQ-Hydro-master.txt
 
 # save the differences into files
 while read p; 
