3c77c5699f53654bb3ff8c002d7e3ac3b2737be7
Christopher Plumberg <astrophysicist87@gmail.com>
2021-06-01 12:49:50 -0500

    Update test.cpp

--- EoS-Library/test.cpp
+++ EoS-Library/test.cpp
@@ -9,7 +9,7 @@ int main( int argc, char ** argv )
 	cout << "Hello, world!" << endl;
 
 	bool using_HDF = true;
-	EOS eos("inputfiles/quantityFile.h5", "inputfiles/derivFile.h5", 1, using_HDF);
+	eos EOS("inputfiles/quantityFile.h5", "inputfiles/derivFile.h5", 1, using_HDF);
 
 	return 0;
 }
