db6c48d11cfe2177ab3bf5d61a2e49cfb737fb3d
Christopher Plumberg <astrophysicist87@gmail.com>
2021-06-08 16:03:05 -0500

    Update hydrosim.cpp

--- Hydro-master/hydrosim.cpp
+++ Hydro-master/hydrosim.cpp
@@ -938,7 +938,7 @@ cout << "CHECK div_u: " << i << "   " << linklist._p[i].div_u
 		<< "   " << inner( linklist._p[i].u, linklist._p[i].du_dt)
 		<< "   " << linklist._p[i].sigma << endl;
 cout << "CHECK bigtheta: " << i << "   " << linklist._p[i].bigtheta
-		<< "   " << linklist._p[i].t
+		<< "   " << linklist.t
 		<< "   " << linklist._p[i].gamma << endl;
 
         Matrix <double,D,D> sub=linklist._p[i].pimin+linklist._p[i].shv.x[0][0]/linklist._p[i].g2*linklist._p[i].uu-1./linklist._p[i].gamma*linklist._p[i].piutot;
