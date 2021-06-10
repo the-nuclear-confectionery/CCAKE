5a374ecdcce8bc6b99da61af8592c2b40748f3ef
Christopher Plumberg <astrophysicist87@gmail.com>
2021-06-10 11:01:24 -0500

    Restructure a bit to get min-max's in one run

--- EoS_BQS_Derivatives/invert_EoS/invert_EoS.cpp
+++ EoS_BQS_Derivatives/invert_EoS/invert_EoS.cpp
@@ -14,74 +14,56 @@ const double hbarc = 197.327;
 int main(int argc, char *argv[])
 {
 	// check input first
-	if (argc >= 6)
+	if (argc < 6) exit(-1);
+
+	// read path to input file from command line
+	string path_to_file = string(argv[1]);
+	const int nTpts = stoi(argv[2]);
+	const int nmuBpts = stoi(argv[3]);
+	const int nmuQpts = stoi(argv[4]);
+	const int nmuSpts = stoi(argv[5]);
+
+	vector<double> Tvec, muBvec, muSvec, muQvec;
+	vector<double> evec, bvec, svec, qvec;
+
+	// then read in file itself
+	ifstream infile(path_to_file.c_str());
+	if (infile.is_open())
 	{
-		// read path to input file from command line
-		string path_to_file = string(argv[1]);
-		const int nTpts = stoi(argv[2]);
-		const int nmuBpts = stoi(argv[3]);
-		const int nmuQpts = stoi(argv[4]);
-		const int nmuSpts = stoi(argv[5]);
-
-		vector<double> Tvec, muBvec, muSvec, muQvec;
-		vector<double> evec, bvec, svec, qvec;
-	
-		// then read in file itself
-		ifstream infile(path_to_file.c_str());
-		if (infile.is_open())
+		size_t count = 0;
+		string line;
+		double dummy, Tin, muBin, muSin, muQin, bin, sin, qin, ein; 
+		while ( getline (infile, line) )
 		{
-			size_t count = 0;
-			string line;
-			double dummy, Tin, muBin, muSin, muQin, bin, sin, qin, ein; 
-			while ( getline (infile, line) )
-			{
-				istringstream iss(line);
-				iss >> Tin >> muBin >> muQin >> muSin >> dummy >> dummy
-					>> bin >> sin >> qin >> ein >> dummy;
-	
-				Tvec.push_back( Tin );
-				muBvec.push_back( muBin );
-				muSvec.push_back( muSin );
-				muQvec.push_back( muQin );
-				bvec.push_back( bin*Tin*Tin*Tin/(hbarc*hbarc*hbarc) );				// 1/fm^3
-				svec.push_back( sin*Tin*Tin*Tin/(hbarc*hbarc*hbarc) );				// 1/fm^3
-				qvec.push_back( qin*Tin*Tin*Tin/(hbarc*hbarc*hbarc) );				// 1/fm^3
-				evec.push_back( 0.001*ein*Tin*Tin*Tin*Tin/(hbarc*hbarc*hbarc) );	// GeV/fm^3
+			istringstream iss(line);
+			iss >> Tin >> muBin >> muQin >> muSin >> dummy >> dummy
+				>> bin >> sin >> qin >> ein >> dummy;
 
-				if (++count % 1000000 == 0) cout << "Read in " << count << " lines." << endl;
-			}
+			Tvec.push_back( Tin );
+			muBvec.push_back( muBin );
+			muSvec.push_back( muSin );
+			muQvec.push_back( muQin );
+			bvec.push_back( bin*Tin*Tin*Tin/(hbarc*hbarc*hbarc) );				// 1/fm^3
+			svec.push_back( sin*Tin*Tin*Tin/(hbarc*hbarc*hbarc) );				// 1/fm^3
+			qvec.push_back( qin*Tin*Tin*Tin/(hbarc*hbarc*hbarc) );				// 1/fm^3
+			evec.push_back( 0.001*ein*Tin*Tin*Tin*Tin/(hbarc*hbarc*hbarc) );	// GeV/fm^3
+
+			if (++count % 1000000 == 0) cout << "Read in " << count << " lines." << endl;
 		}
+	}
+
+	infile.close();
+
+	// loop over each T value to get 
+	for ( size_t iT = 0; iT < nTpts; iT++ )
+	{
+		//============================
+		// get min-max's in each b,q,s direction
 	
-		infile.close();
-
-		/*
-		// just look at a slice for testing purposes
-		vector<double> muBslice( muBvec.begin(), muBvec.begin() + nmuBpts*nmuSpts*nmuQpts );
-		vector<double> muSslice( muSvec.begin(), muSvec.begin() + nmuBpts*nmuSpts*nmuQpts );
-		vector<double> muQslice( muQvec.begin(), muQvec.begin() + nmuBpts*nmuSpts*nmuQpts );
-		vector<double> eslice(   evec.begin(),   evec.begin()   + nmuBpts*nmuSpts*nmuQpts );
-		vector<double> bslice(   bvec.begin(),   bvec.begin()   + nmuBpts*nmuSpts*nmuQpts );
-		vector<double> sslice(   svec.begin(),   svec.begin()   + nmuBpts*nmuSpts*nmuQpts );
-		vector<double> qslice(   qvec.begin(),   qvec.begin()   + nmuBpts*nmuSpts*nmuQpts );
-		*/
-
 		//============================
-		/*double e_min_max = get_e_min_max(evec, nTpts, nmuBpts, nmuQpts, nmuSpts);
-
-		cout << "e_min_max = " << Tvec[0] << "   " << e_min_max << endl;
-		
-		for (size_t i = 0; i < evec.size(); i++)
-			if ( abs( e_min_max - abs(evec[i]) ) < 1e-10 )
-				cout << "e range:" << i << "   " << Tvec[i] << "   " << muBvec[i] << "   "
-					<< muSvec[i] << "   " << muQvec[i] << "   "
-					<< evec[i] << "   " << bvec[i] << "   "
-					<< svec[i] << "   " << qvec[i] << endl;
-		*/
-
-		//============================
-		double b_min_max = get_b_min_max(bvec, nTpts, nmuBpts, nmuQpts, nmuSpts);
-
-		cout << "b_min_max = " << Tvec[0] << "   " << b_min_max << endl;
+		double b_min_max = get_b_min_max(bvec, iT, nTpts, nmuBpts, nmuQpts, nmuSpts);
+	
+		cout << "b_min_max = " << Tvec[iT] << "   " << b_min_max << endl;
 		
 		for (size_t i = 0; i < bvec.size(); i++)
 			if ( abs( b_min_max - abs(bvec[i]) ) < 1e-10 )
@@ -89,11 +71,11 @@ int main(int argc, char *argv[])
 					<< muSvec[i] << "   " << muQvec[i] << "   "
 					<< evec[i] << "   " << bvec[i] << "   "
 					<< svec[i] << "   " << qvec[i] << endl;
-
+	
 		//============================
-		double s_min_max = get_s_min_max(svec, nTpts, nmuBpts, nmuQpts, nmuSpts);
-
-		cout << "s_min_max = " << Tvec[0] << "   " << s_min_max << endl;
+		double s_min_max = get_s_min_max(svec, iT, nTpts, nmuBpts, nmuQpts, nmuSpts);
+	
+		cout << "s_min_max = " << Tvec[iT] << "   " << s_min_max << endl;
 		
 		for (size_t i = 0; i < svec.size(); i++)
 			if ( abs( s_min_max - abs(svec[i]) ) < 1e-10 )
@@ -101,11 +83,11 @@ int main(int argc, char *argv[])
 					<< muSvec[i] << "   " << muQvec[i] << "   "
 					<< evec[i] << "   " << bvec[i] << "   "
 					<< svec[i] << "   " << qvec[i] << endl;
-
+	
 		//============================
-		double q_min_max = get_q_min_max(qvec, nTpts, nmuBpts, nmuQpts, nmuSpts);
-
-		cout << "q_min_max = " << Tvec[0] << "   " << q_min_max << endl;
+		double q_min_max = get_q_min_max(qvec, iT, nTpts, nmuBpts, nmuQpts, nmuSpts);
+	
+		cout << "q_min_max = " << Tvec[iT] << "   " << q_min_max << endl;
 		
 		for (size_t i = 0; i < qvec.size(); i++)
 			if ( abs( q_min_max - abs(qvec[i]) ) < 1e-10 )
@@ -113,12 +95,12 @@ int main(int argc, char *argv[])
 					<< muSvec[i] << "   " << muQvec[i] << "   "
 					<< evec[i] << "   " << bvec[i] << "   "
 					<< svec[i] << "   " << qvec[i] << endl;
+
+		cout << endl << endl;
 	}
+
+
 
 	return 0;
 }
-
-
-
-
 
--- EoS_BQS_Derivatives/invert_EoS/invert_EoS.h
+++ EoS_BQS_Derivatives/invert_EoS/invert_EoS.h
@@ -43,12 +43,12 @@ double get_e_min_max( vector<double> & v, const size_t nTpts, const size_t nmuBp
 
 
 
-double get_b_min_max( vector<double> & v, const size_t nTpts, const size_t nmuBpts,
-										const size_t nmuQpts, const size_t nmuSpts )
+double get_b_min_max( vector<double> & v, const size_t iT,
+						const size_t nTpts, const size_t nmuBpts,
+						const size_t nmuQpts, const size_t nmuSpts )
 {
 	vector<double> maxima;
 	size_t count = 0;
-	for ( size_t iT = 0; iT < nTpts; iT++ )
 	for ( size_t imuS = 0; imuS < nmuSpts; imuS++ )
 	for ( size_t imuQ = 0; imuQ < nmuQpts; imuQ++ )
 	{
@@ -68,12 +68,12 @@ double get_b_min_max( vector<double> & v, const size_t nTpts, const size_t nmuBp
 }
 
 
-double get_q_min_max( vector<double> & v, const size_t nTpts, const size_t nmuBpts,
-										const size_t nmuQpts, const size_t nmuSpts )
+double get_q_min_max( vector<double> & v, const size_t iT,
+						const size_t nTpts, const size_t nmuBpts,
+						const size_t nmuQpts, const size_t nmuSpts )
 {
 	vector<double> maxima;
 	size_t count = 0;
-	for ( size_t iT = 0; iT < nTpts; iT++ )
 	for ( size_t imuB = 0; imuB < nmuBpts; imuB++ )
 	for ( size_t imuS = 0; imuS < nmuSpts; imuS++ )
 	{
@@ -92,11 +92,12 @@ double get_q_min_max( vector<double> & v, const size_t nTpts, const size_t nmuBp
 	return ( *std::min_element( maxima.begin(), maxima.end() ) );
 }
 
-double get_s_min_max( vector<double> & v, const size_t nTpts, const size_t nmuBpts,
-										const size_t nmuQpts, const size_t nmuSpts )
+double get_s_min_max( vector<double> & v, const size_t iT,
+						const size_t nTpts, const size_t nmuBpts,
+						const size_t nmuQpts, const size_t nmuSpts )
 {
 	vector<double> maxima;
-	size_t count = 0;
+	/*size_t count = 0;
 	for ( size_t iT = 0; iT < nTpts; iT++ )
 	for ( size_t imuB = 0; imuB < nmuBpts; imuB++ )
 	for ( size_t imuQ = 0; imuQ < nmuQpts; imuQ++ )
@@ -104,6 +105,21 @@ double get_s_min_max( vector<double> & v, const size_t nTpts, const size_t nmuBp
 		maxima.push_back( abs(
 			*std::max_element( v.begin() + count*nmuSpts,
 							   v.begin() + (count+1)*nmuSpts,
+								[](const double & a, const double & b)
+								{ return abs(a) < abs(b); }
+							) )
+						);
+		count++;
+	}*/
+	size_t count = 0;
+	for ( size_t imuB = 0; imuB < nmuBpts; imuB++ )
+	for ( size_t imuQ = 0; imuQ < nmuQpts; imuQ++ )
+	{
+		vector<double> slice;
+		for ( size_t imuS = 0; imuS < nmuSpts; imuS++ )
+			slice.push_back( v[indexer( iT, imuB, imuQ, imuS, nTpts, nmuBpts, nmuQpts, nmuSpts )] );
+		maxima.push_back( abs(
+			*std::max_element( slice.begin(), slice.end(),
 								[](const double & a, const double & b)
 								{ return abs(a) < abs(b); }
 							) )
