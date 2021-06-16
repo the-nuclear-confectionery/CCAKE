9fd8f0102de0cc708be03d66e3bec452fdf44eda
Christopher Plumberg <astrophysicist87@gmail.com>
2021-06-15 19:20:31 -0500

    Update eos.cpp

--- Hydro-master/eos.cpp
+++ Hydro-master/eos.cpp
@@ -1645,10 +1645,11 @@ bool eos::rootfinder4D(double e_or_s_Given, int e_or_s_mode, double rhoBGiven, d
     //declare x = (T, muB, muS)
     gsl_vector *x = gsl_vector_alloc(4);
 
-    gsl_vector_set(x, 0, T());
+    //gsl_vector_set(x, 0, T());
     //gsl_vector_set(x, 1, muB());
     //gsl_vector_set(x, 2, muQ());
     //gsl_vector_set(x, 3, muS());
+    gsl_vector_set(x, 0, 500.0/197.327);
     gsl_vector_set(x, 1, 50.0/197.327);
     gsl_vector_set(x, 2, 0.0);
     gsl_vector_set(x, 3, 0.0);
