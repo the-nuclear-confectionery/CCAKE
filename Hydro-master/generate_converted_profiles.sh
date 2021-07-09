284bab0c0c2196033165c8c854bbd76229778144
Christopher Plumberg <astrophysicist87@gmail.com>
2021-07-09 17:53:24 -0500

    Update generate_converted_profiles.sh

--- Hydro-master/generate_converted_profiles.sh
+++ Hydro-master/generate_converted_profiles.sh
@@ -7,6 +7,10 @@ suffix="_ev0.dat"
 
 nFiles=$(\ls -1 ${resultsDirectory}/${SPHprofileStem}*${suffix} | wc -l)
 
+# compile from scratch since it's so quick
+rm ./convert_SPH_to_physical
+g++ -std=c++11 convert_SPH_to_physical.cpp -o convert_SPH_to_physical
+
 for index in $(seq $nFiles)
 do
 	if [ ! -f "${resultsDirectory}/${physicalProfileStem}${index}${suffix}" ]
