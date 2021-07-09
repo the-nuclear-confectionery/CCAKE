701f6879348da0342989ce1189e72f23f4f983db
Christopher Plumberg <astrophysicist87@gmail.com>
2021-07-09 16:09:32 -0500

    Move frame generation loop inside python script

--- Hydro-master/generate_profile_evolution.sh
+++ Hydro-master/generate_profile_evolution.sh
@@ -3,14 +3,15 @@
 maxFrames=250
 resultsDirectory=outputfiles
 physicalProfileStem=physical_quantities
-suffix="_ev0.dat"
+insuffix="_ev0.dat"
 outsuffix="_ev0.png"
 
-nFiles=$(\ls -1 ${resultsDirectory}/${physicalProfileStem}*${suffix} | wc -l)
+nFiles=$(\ls -1 ${resultsDirectory}/${physicalProfileStem}*${insuffix} | wc -l)
 
-for index in $(seq $nFiles)
+# old version which looped over each file one at a time
+'''for index in $(seq $nFiles)
 do
-	infile=${resultsDirectory}/${physicalProfileStem}${index}${suffix}
+	infile=${resultsDirectory}/${physicalProfileStem}${index}${insuffix}
 
 	# if file exists, generate corresponding frame(s)
 	if [ -f "$infile" ] && [ $index -le $maxFrames ]
@@ -18,9 +19,18 @@ do
 		fwindex=`printf %03d $index`
 		python3 plot_profile.py $infile ${resultsDirectory}/"eProfile"${index}${outsuffix} '$e$ (MeV/fm$^3$)' 7
 		python3 plot_profile.py $infile ${resultsDirectory}/"rhoBProfile"${index}${outsuffix} '$\rho_B$ (fm$^{-3}$)' 8
-		echo 'Plotted profiles from' $infile 'at' $(date)
+		#echo 'Plotted profiles from' $infile 'at' $(date)
 	fi
-done
+done'''
+
+# use the minimum of these two quantities
+nFrames=$((nFiles<maxFrames ? nFiles : maxFrames))
+
+python3 plot_profile.py \
+	${resultsDirectory}/${physicalProfileStem} \
+	${resultsDirectory}/"eProfile" \
+	${insuffix} ${outsuffix} $nFiles '$e$ (MeV/fm$^3$)' 7
+
 
 framesPerSecond=25
 pngs2mp4 $framesPerSecond ${resultsDirectory}/"eProfile"%03d${outsuffix} eProfile.mp4
