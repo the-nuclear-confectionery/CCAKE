13688f5acb93dc6630e9d9e8bc894d23106428cc
Christopher Plumberg <astrophysicist87@gmail.com>
2021-07-09 16:10:53 -0500

    Update generate_profile_evolution.sh

--- Hydro-master/generate_profile_evolution.sh
+++ Hydro-master/generate_profile_evolution.sh
@@ -9,19 +9,19 @@ outsuffix="_ev0.png"
 nFiles=$(\ls -1 ${resultsDirectory}/${physicalProfileStem}*${insuffix} | wc -l)
 
 # old version which looped over each file one at a time
-'''for index in $(seq $nFiles)
-do
-	infile=${resultsDirectory}/${physicalProfileStem}${index}${insuffix}
-
-	# if file exists, generate corresponding frame(s)
-	if [ -f "$infile" ] && [ $index -le $maxFrames ]
-	then
-		fwindex=`printf %03d $index`
-		python3 plot_profile.py $infile ${resultsDirectory}/"eProfile"${index}${outsuffix} '$e$ (MeV/fm$^3$)' 7
-		python3 plot_profile.py $infile ${resultsDirectory}/"rhoBProfile"${index}${outsuffix} '$\rho_B$ (fm$^{-3}$)' 8
-		#echo 'Plotted profiles from' $infile 'at' $(date)
-	fi
-done'''
+#for index in $(seq $nFiles)
+#do
+#	infile=${resultsDirectory}/${physicalProfileStem}${index}${insuffix}
+#
+#	# if file exists, generate corresponding frame(s)
+#	if [ -f "$infile" ] && [ $index -le $maxFrames ]
+#	then
+#		fwindex=`printf %03d $index`
+#		python3 plot_profile.py $infile ${resultsDirectory}/"eProfile"${index}${outsuffix} '$e$ (MeV/fm$^3$)' 7
+#		python3 plot_profile.py $infile ${resultsDirectory}/"rhoBProfile"${index}${outsuffix} '$\rho_B$ (fm$^{-3}$)' 8
+#		echo 'Plotted profiles from' $infile 'at' $(date)
+#	fi
+#done
 
 # use the minimum of these two quantities
 nFrames=$((nFiles<maxFrames ? nFiles : maxFrames))
