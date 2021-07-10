c5b5c99036ec88f6a63aa59733e6b26e9973bba8
Christopher Plumberg <astrophysicist87@gmail.com>
2021-07-10 10:32:35 -0500

    Remove *.mp4 files before (re)generating them and adjust EOS tolerance

--- Hydro-master/generate_profile_evolution.sh
+++ Hydro-master/generate_profile_evolution.sh
@@ -41,6 +41,7 @@ python3 plot_profile.py \
 
 
 mkdir -p $moviesDirectory
+rm -rf ${moviesDirectory}/eProfile.mp4 ${moviesDirectory}/rhoBProfile.mp4
 pngs2mp4 $framesPerSecond ${resultsDirectory}/"eProfile"%03d${outsuffix} ${moviesDirectory}/eProfile.mp4
 pngs2mp4 $framesPerSecond ${resultsDirectory}/"rhoBProfile"%03d${outsuffix} ${moviesDirectory}/rhoBProfile.mp4
 
