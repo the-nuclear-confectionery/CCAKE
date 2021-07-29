906a18c667f59694a9d8c998b2935d6ad8c498db
Christopher Plumberg <astrophysicist87@gmail.com>
2021-07-28 21:48:43 -0500

    Powers less than 1?

--- Hydro-master/interp_thermo/interp_thermo.cpp
+++ Hydro-master/interp_thermo/interp_thermo.cpp
@@ -83,7 +83,7 @@ int main(int argc, char ** argv)
 				cout << endl;
 			}
 
-				for ( double power_in = 0.1; power_in <= 5.0; power_in += 0.1 )
+				for ( double power_in = 0.0; power_in <= 1.0; power_in += 0.1 )
 					interp_thermo::get_IDW_point_estimate( neighbors, point_to_check, power_in );
 			}
 			k *= 2;
