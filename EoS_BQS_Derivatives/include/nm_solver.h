ce347c9741fde091b5c93ec8128c2b98dd4e4f51
Christopher Plumberg <astrophysicist87@gmail.com>
2021-07-30 13:24:47 -0500

    No namespace

--- EoS_BQS_Derivatives/include/nm_solver.h
+++ EoS_BQS_Derivatives/include/nm_solver.h
@@ -3,12 +3,7 @@
 
 #include "Functions.h"
 
-using namespace std;
-
-namespace nm_solver
-{
-	void solve( double & e0, double & B0, double & S0, double & Q0,
-				double & Tout, double & muBout, double & muSout, double & muQout );
-}
+void solve( double & e0, double & B0, double & S0, double & Q0,
+			double & Tout, double & muBout, double & muSout, double & muQout );
 
 #endif
