7c8ef04e00e3b0e0f754803f2267c60d93d1519d
Christopher Plumberg <astrophysicist87@gmail.com>
2021-08-10 16:32:50 -0500

    Update EoS_BQS.c

--- EoS_BQS_Derivatives/src/EoS_BQS.c
+++ EoS_BQS_Derivatives/src/EoS_BQS.c
@@ -85,6 +85,28 @@ int main(int argc, char *argv[])
    /* Start clock for importing lists */
   	start = clock();
 
+		// load maxima from file
+		int interpgridlength = 1001;
+		double logegrid[interpgridlength], max_rBt[interpgridlength],
+				max_rSt[interpgridlength], max_rQt[interpgridlength];
+		printf("argc = %d\n", argc);
+		printf("argv[2] = %s\n", argv[2]);
+		errno = 0;
+		FILE * MaximaIn = fopen("maxima_cmp.dat", "r");
+		if (MaximaIn == 0)
+		{
+			fprintf(stderr, "Failed to open maxima file.\n");
+			perror("Error");
+			fprintf(stderr, "Error: %d (%s)\n", errno, strerror(errno));
+			exit(1);
+		}
+		for(int ii = 0; ii < interpgridlength; ii++)
+			fscanf(MaximaIn,"%lf %lf %lf %lf\n",
+							&logegrid[ii], &max_rBt[ii],
+							&max_rSt[ii], &max_rQt[ii]);
+		fclose(MaximaIn);
+
+
 	/* Parametrization parameters are read from the user-input file, and saved. */
 		printf("argc = %d\n", argc);
 		printf("argv[1] = %s\n", argv[1]);
@@ -180,26 +202,26 @@ int main(int argc, char *argv[])
 	int run_on_density_grid = 1;
 	if ( run_on_density_grid )
 	{
-		// load maxima from file
-		int interpgridlength = 1001;
-		double logegrid[interpgridlength], max_rBt[interpgridlength],
-				max_rSt[interpgridlength], max_rQt[interpgridlength];
-		printf("argc = %d\n", argc);
-		printf("argv[2] = %s\n", argv[2]);
-		errno = 0;
-		FILE * MaximaIn = fopen("maxima_cmp.dat", "r");
-		if (MaximaIn == 0)
-		{
-			fprintf(stderr, "Failed to open maxima file.\n");
-			perror("Error");
-			fprintf(stderr, "Error: %d (%s)\n", errno, strerror(errno));
-			exit(1);
-		}
-		for(int ii = 0; ii < interpgridlength; ii++)
-			fscanf(MaximaIn,"%lf %lf %lf %lf\n",
-							&logegrid[ii], &max_rBt[ii],
-							&max_rSt[ii], &max_rQt[ii]);
-		fclose(MaximaIn);
+//		// load maxima from file
+//		int interpgridlength = 1001;
+//		double logegrid[interpgridlength], max_rBt[interpgridlength],
+//				max_rSt[interpgridlength], max_rQt[interpgridlength];
+//		printf("argc = %d\n", argc);
+//		printf("argv[2] = %s\n", argv[2]);
+//		errno = 0;
+//		FILE * MaximaIn = fopen("maxima_cmp.dat", "r");
+//		if (MaximaIn == 0)
+//		{
+//			fprintf(stderr, "Failed to open maxima file.\n");
+//			perror("Error");
+//			fprintf(stderr, "Error: %d (%s)\n", errno, strerror(errno));
+//			exit(1);
+//		}
+//		for(int ii = 0; ii < interpgridlength; ii++)
+//			fscanf(MaximaIn,"%lf %lf %lf %lf\n",
+//							&logegrid[ii], &max_rBt[ii],
+//							&max_rSt[ii], &max_rQt[ii]);
+//		fclose(MaximaIn);
 
 		double logemin = logegrid[0], logemax = logegrid[interpgridlength-1];
 		double logestep = (logemax - logemin)/(interpgridlength-1.0);
