7e771fee4263de88721fb8d48f786e1f3d9c8c9b
Christopher Plumberg <astrophysicist87@gmail.com>
2021-08-10 12:08:12 -0500

    Typo

--- EoS_BQS_Derivatives/src/EoS_BQS.c
+++ EoS_BQS_Derivatives/src/EoS_BQS.c
@@ -297,10 +297,10 @@ int main(int argc, char *argv[])
 	long long gridEntry  = 0;
 
 	// set T and mu_i ranges
-	const int Tmin = 30, Tmax = 800, DeltaT = 5;
-	const int muBmin = -450, muBmax = 450, DeltamuB = 20;
-	const int muQmin = -450, muQmax = 450, DeltamuQ = 20;
-	const int muSmin = -450, muSmax = 450, DeltamuS = 20;
+//	const int Tmin = 30, Tmax = 800, DeltaT = 5;
+//	const int muBmin = -450, muBmax = 450, DeltamuB = 20;
+//	const int muQmin = -450, muQmax = 450, DeltamuQ = 20;
+//	const int muSmin = -450, muSmax = 450, DeltamuS = 20;
 
 	const int Tmin = 30, Tmax = 800, DeltaT = 5;
 	const int muBmin = -450, muBmax = 450, DeltamuB = 450;
