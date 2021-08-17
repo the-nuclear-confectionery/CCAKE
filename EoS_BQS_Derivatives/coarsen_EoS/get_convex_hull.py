0ce8d27ba6fb238d5eb6dbc98866ce55b27a6f4b
Christopher Plumberg <astrophysicist87@gmail.com>
2021-08-17 15:53:48 -0500

    Update get_convex_hull.py

--- EoS_BQS_Derivatives/coarsen_EoS/get_convex_hull.py
+++ EoS_BQS_Derivatives/coarsen_EoS/get_convex_hull.py
@@ -14,11 +14,11 @@ loge0,loge1,loge=normalize(loge); bt0,bt1,bt=normalize(bt); st0,st1,st=normalize
 
 e0,e1,e=normalize(e); b0,b1,b=normalize(b); s0,s1,s=normalize(s); q0,q1,q=normalize(q)
 
-#hull = ConvexHull(np.c_[ loge, bt, st, qt ])
-#grid=np.c_[range(len(T)),T,muB,muS,muQ,loge,bt,st,qt]
+hull = ConvexHull(np.c_[ loge, bt, st, qt ])
+grid=np.c_[range(len(T)),T,muB,muS,muQ,loge,bt,st,qt]
 
-hull = ConvexHull(np.c_[ e, b, s, q ])
-grid=np.c_[range(len(T)),T,muB,muS,muQ,e,b,s,q]
+#hull = ConvexHull(np.c_[ e, b, s, q ])
+#grid=np.c_[range(len(T)),T,muB,muS,muQ,e,b,s,q]
 
-np.savetxt('grid_ebsq.dat', grid, fmt=("%d" + " %12.8f"*8))
-np.savetxt('hull_ebsq.dat', grid[hull.vertices], fmt=("%d" + " %12.8f"*8))
+np.savetxt('grid_ebsq_normed.dat', grid, fmt=("%d" + " %12.8f"*8))
+np.savetxt('hull_ebsq_normed.dat', grid[hull.vertices], fmt=("%d" + " %12.8f"*8))
