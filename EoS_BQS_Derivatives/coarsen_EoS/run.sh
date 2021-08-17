976f9c9f0e547fe2b3f902787d2170e5caaa8ff6
Christopher Plumberg <astrophysicist87@gmail.com>
2021-08-17 09:09:26 -0500

    Update run.sh
    Tweak slurm script

--- EoS_BQS_Derivatives/coarsen_EoS/run.sh
+++ EoS_BQS_Derivatives/coarsen_EoS/run.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 #SBATCH -A qgp
 #SBATCH -p qgp
-#SBATCH -t 08:00:00
+#SBATCH -t 72:00:00
 
-./coarsen_EoS ../Thermodynamics_dense/EoS_Taylor_AllMu.dat
+#./coarsen_EoS ../Thermodynamics_dense/EoS_Taylor_AllMu.dat
+
+# Run with: sbatch --export=ALL,filename=...,r0=... run.sh
+
+./coarsen_EoS $filename $r0
 
