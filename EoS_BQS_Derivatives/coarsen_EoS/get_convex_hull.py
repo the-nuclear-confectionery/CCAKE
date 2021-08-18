0df79bfd2372d8cb3a1cf353b799fdf7f02cbc46
Christopher Plumberg <astrophysicist87@gmail.com>
2021-08-18 10:59:15 -0500

    Update get_convex_hull.py

--- EoS_BQS_Derivatives/coarsen_EoS/get_convex_hull.py
+++ EoS_BQS_Derivatives/coarsen_EoS/get_convex_hull.py
@@ -7,16 +7,16 @@ def normalize(v):
     return np.amin(v), np.amax(v), (v-np.amin(v))/(np.amax(v)-np.amin(v))
 
 # set up
-data=np.loadtxt('../Thermodynamics_dense/EoS_Taylor_AllMu.dat', usecols=(0,1,2,3,6,7,8,9))
+data=np.loadtxt('../Thermodynamics/EoS_Taylor_AllMu.dat', usecols=(0,1,2,3,6,7,8,9))
 [T,muB,muQ,muS,b,s,q,e]=data.T
 b*=T**3/hc**3; s*=T**3/hc**3; q*=T**3/hc**3; e*=T**4/hc**3;
 loge=np.log(e); bt=b/(e/hc)**0.75; st=s/(e/hc)**0.75; qt=q/(e/hc)**0.75;
 
 # e, b, s, q; actual values
-#hull = ConvexHull(np.c_[ e, b, s, q ])
-#grid=np.c_[range(len(T)),T,muB,muS,muQ,e,b,s,q]
-#np.savetxt('grid_ebsq_mag.dat', grid, fmt=("%d" + " %12.8f"*8))
-#np.savetxt('hull_ebsq_mag.dat', grid[hull.vertices], fmt=("%d" + " %12.8f"*8))
+hull = ConvexHull(np.c_[ e, b, s, q ])
+grid=np.c_[range(len(T)),T,muB,muS,muQ,e,b,s,q]
+np.savetxt('grid_ebsq_mag.dat', grid, fmt=("%d" + " %12.8f"*8))
+np.savetxt('hull_ebsq_mag.dat', grid[hull.vertices], fmt=("%d" + " %12.8f"*8))
 
 print('hull = ConvexHull(np.c_[ loge, bt, st, qt ]); mag')
 hull = ConvexHull(np.c_[ loge, bt, st, qt ])
