d20c38405568aae2661592d559f09ff49258a27e
Christopher Plumberg <astrophysicist87@gmail.com>
2021-08-18 11:02:59 -0500

    Update get_convex_hull.py

--- EoS_BQS_Derivatives/coarsen_EoS/get_convex_hull.py
+++ EoS_BQS_Derivatives/coarsen_EoS/get_convex_hull.py
@@ -13,10 +13,10 @@ b*=T**3/hc**3; s*=T**3/hc**3; q*=T**3/hc**3; e*=T**4/hc**3;
 loge=np.log(e); bt=b/(e/hc)**0.75; st=s/(e/hc)**0.75; qt=q/(e/hc)**0.75;
 
 # e, b, s, q; actual values
-hull = ConvexHull(np.c_[ e, b, s, q ])
+#hull = ConvexHull(np.c_[ e, b, s, q ])
 grid=np.c_[range(len(T)),T,muB,muS,muQ,e,b,s,q]
 np.savetxt('grid_ebsq_mag.dat', grid, fmt=("%d" + " %12.8f"*8))
-np.savetxt('hull_ebsq_mag.dat', grid[hull.vertices], fmt=("%d" + " %12.8f"*8))
+#np.savetxt('hull_ebsq_mag.dat', grid[hull.vertices], fmt=("%d" + " %12.8f"*8))
 
 print('hull = ConvexHull(np.c_[ loge, bt, st, qt ]); mag')
 hull = ConvexHull(np.c_[ loge, bt, st, qt ])
