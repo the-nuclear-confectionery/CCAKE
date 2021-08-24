569b3dba79d726cb9aa9b41e45bed8a552468e31
Christopher Plumberg <astrophysicist87@gmail.com>
2021-08-24 16:47:25 -0500

    Update delaunay.h

--- EoS_BQS_Derivatives/coarsen_EoS/delaunay.h
+++ EoS_BQS_Derivatives/coarsen_EoS/delaunay.h
@@ -71,7 +71,7 @@ int compute_delaunay(double * arr, const size_t pc_dimension, const size_t n_pts
 					 vector<vector<size_t> > & simplices)
 {
 	Qhull qhull;
-	qhull.runQhull("", pc_dimension, n_pts, arr, "d Qbb Qt");
+	qhull.runQhull("", pc_dimension, n_pts, arr, "d Qbb Qt Qc Qz Q12");
 	
 	for ( const auto & facet : qhull.facetList().toStdVector() )
 	{
