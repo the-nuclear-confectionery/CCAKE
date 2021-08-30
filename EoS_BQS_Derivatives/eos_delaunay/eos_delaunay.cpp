fbf97820f312a0834efad606f1cf63e7ef20a3d1
Christopher Plumberg <astrophysicist87@gmail.com>
2021-08-30 09:43:30 -0500

    Nevermind; for some reason doesn't work!

--- EoS_BQS_Derivatives/eos_delaunay/eos_delaunay.cpp
+++ EoS_BQS_Derivatives/eos_delaunay/eos_delaunay.cpp
@@ -478,17 +478,17 @@ void eos_delaunay::interpolate_NMNmode(const vector<double> & v0, vector<double>
 	// Qhull requires vertices as 1D vector
 	vector<double> verticesFlat;
 
-	for (int starting_index = 0; starting_index >= -1; --starting_index)
+	for (int starting_index = 0; starting_index >= 0; --starting_index)
 	{
 		// reset
 		vertices.clear();
 		verticesFlat.clear();
 
 		int vertexcount = 0;
-		for (int ii = starting_index; ii <= 1; ii++)
-		for (int jj = starting_index; jj <= 1; jj++) // only need containing hypercube
-		for (int kk = starting_index; kk <= 1; kk++) // vertices for the NMN method
-		for (int ll = starting_index; ll <= 1; ll++)
+		for (int ii = 0; ii <= 1; ii++)
+		for (int jj = 0; jj <= 1; jj++) // only need containing hypercube
+		for (int kk = 0; kk <= 1; kk++) // vertices for the NMN method
+		for (int ll = 0; ll <= 1; ll++)
 		{
 			// check that we're not going outside the grid
 			if ( iTNMN+ii < nT && iTNMN+ii >= 0
