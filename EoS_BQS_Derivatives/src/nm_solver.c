c344ce3808ceca15797444feac063ce0178b6cf7
Christopher Plumberg <astrophysicist87@gmail.com>
2021-09-01 09:34:35 -0500

    Update nm_solver.c

--- EoS_BQS_Derivatives/src/nm_solver.c
+++ EoS_BQS_Derivatives/src/nm_solver.c
@@ -203,7 +203,7 @@ void solve2( double densities[], double sols[], double minima[], double maxima[]
 
 
 	const size_t maxTries = 1000000;
-	const double ACCURACY = 1e-10;
+	const double ACCURACY = 1e-4;
 	const double hbarc = 197.327;
 	const double hbarc3 = hbarc*hbarc*hbarc;
 
