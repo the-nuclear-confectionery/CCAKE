484ab81100b2c67d55e5287afbd38c2cab735794
Christopher Plumberg <astrophysicist87@gmail.com>
2021-09-02 21:03:23 -0500

    Allow log and euclidean distance measures, switch between them with option (default: euclidean)

--- EoS_BQS_Derivatives/eos_delaunay/kdtree.h
+++ EoS_BQS_Derivatives/eos_delaunay/kdtree.h
@@ -35,15 +35,15 @@ public:
      * @param pt another point
      * @return distance squared from this point to the other point
      */
-    /*double distance(const point& pt) const {
+    double distance(const point& pt) const {
         double dist = 0;
         for (size_t i = 0; i < dimensions; ++i) {
             double d = get(i) - pt.get(i);
             dist += d * d;
         }
         return dist;
-    }*/
-    double distance(const point& pt) const {
+    }
+    double logdistance(const point& pt) const {
         double dist = 0;
         for (size_t i = 0; i < dimensions; ++i) {
             double d = (get(i)*pt.get(i)<=1e-10)? 1e10 : log(abs(get(i)/pt.get(i)));
@@ -83,6 +83,9 @@ private:
         double distance(const point_type& pt) const {
             return point_.distance(pt);
         }
+        double logdistance(const point_type& pt) const {
+            return point_.logdistance(pt);
+        }
         point_type point_;
         node* left_;
         node* right_;
@@ -114,11 +117,11 @@ private:
         return &nodes_[n];
     }
  
-    void nearest(node* root, const point_type& point, size_t index) {
+    void nearest(node* root, const point_type& point, size_t index, bool euclidean_distance) {
         if (root == nullptr)
             return;
         ++visited_;
-        double d = root->distance(point);
+        double d = (euclidean_distance) ? root->distance(point) : root->logdistance(point);
         if (best_ == nullptr || d < best_dist_) {
             best_dist_ = d;
             best_ = root;
@@ -127,10 +130,10 @@ private:
             return;
         double dx = root->get(index) - point.get(index);
         index = (index + 1) % dimensions;
-        nearest(dx > 0 ? root->left_ : root->right_, point, index);
+        nearest(dx > 0 ? root->left_ : root->right_, point, index, euclidean_distance);
         if (dx * dx >= best_dist_)
             return;
-        nearest(dx > 0 ? root->right_ : root->left_, point, index);
+        nearest(dx > 0 ? root->right_ : root->left_, point, index, euclidean_distance);
     }
 public:
     kdtree(const kdtree&) = delete;
@@ -177,13 +180,13 @@ public:
      * @param pt a point
      * @return the nearest point in the tree to the given point
      */
-    const point_type& nearest(const point_type& pt ) {
+    const point_type& nearest(const point_type& pt, bool euclidean_distance = true ) {
         if (root_ == nullptr)
             throw std::logic_error("tree is empty");
         best_ = nullptr;
         visited_ = 0;
         best_dist_ = 0;
-        nearest(root_, pt, 0);
+        nearest(root_, pt, 0, euclidean_distance);
         return best_->point_;
     }
 
@@ -194,13 +197,13 @@ public:
      * @param pt a point
      * @return the nearest point in the tree to the given point
      */
-    const point_type& nearest(const point_type& pt, size_t & best_oindex ) {
+    const point_type& nearest(const point_type& pt, size_t & best_oindex, bool euclidean_distance = true ) {
         if (root_ == nullptr)
             throw std::logic_error("tree is empty");
         best_ = nullptr;
         visited_ = 0;
         best_dist_ = 0;
-        nearest(root_, pt, 0);
+        nearest(root_, pt, 0, euclidean_distance);
 		best_oindex = best_->oindex_;
         return best_->point_;
     }
