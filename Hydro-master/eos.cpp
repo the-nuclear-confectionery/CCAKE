f7c400071545332a63fa31849655ecab50192fed
Christopher Plumberg <astrophysicist87@gmail.com>
2021-09-14 18:11:30 -0500

    Need to set found flag correctly

--- Hydro-master/eos.cpp
+++ Hydro-master/eos.cpp
@@ -1204,7 +1204,13 @@ bool eos::rootfinder4D(double e_or_s_Given, int e_or_s_mode,
 
 
     bool found = true; //to return variable
-    if ( iter >= steps ) {
+    if ( iter >= steps || status != 0 )
+	{
+		if ( status == GSL_EBADFUNC )
+			std::cout << "Error: something went to +/-Inf or NaN!" << std::endl;
+		else if ( status == GSL_ENOPROG )
+			std::cout << "Error: not making enough progress!" << std::endl;
+        }
         found = false;
     }
 
