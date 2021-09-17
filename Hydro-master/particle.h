4360da59c32499f21c305ce17ed2d391335146a8
Christopher Plumberg <astrophysicist87@gmail.com>
2021-09-16 22:59:59 -0500

    More printing

--- Hydro-master/particle.h
+++ Hydro-master/particle.h
@@ -364,6 +364,10 @@ void Particle<D>::return_bsqsv_A()
 
     Agam2=(Agam-eta_o_tau*(0.5-1/3.) -dwdsT1*shv.x[0][0])/gamma;
     Ctot=C+eta_o_tau*(1/g2-1)/2.;
+	cout << "   " << eta_o_tau << "   " << EOSs() << "   " << Agam2
+			<< "   " << Ctot << "   " << EOS.dwdB() * rhoB_an
+			<< "   " << EOS.dwdS() * rhoS_an << "   " << EOS.dwdQ() * rhoQ_an;
+
 }
 
 template <int D>
@@ -498,9 +502,15 @@ void Particle<D>::bsqsvsigset(double tin,int i)
     gradU=gamma*gradV+g3*(v*(v*gradV));
     bigPI= Bulk*sigma/gt ;
     C=EOSw()+ bigPI;
+	cout << "Check thermo1: " << i << "   " << tin << "   " << gamma
+			<< "   " << EOST()*197.327 << "   " << EOSdwds() << "   " << dwdsT1
+			<< "   " << sigma << "   " << dsigma_dt << "   " << sigl
+			<< "   " << gradU << "   " << gradV << "   " << v;
     return_bsqsv_A();
     Btot=(Agam*gamma+eta_o_tau/3*gamma)*sigl+ bigPI/tauRelax + dwdsT* (gt*shv33+  Bsub());
     check=sigl;
+	cout << "   " << EOSw() << "   " << bigPI << "   " << Agam
+			<< "   " << shv33 << "   " << Bsub() << "   " << Btot << endl;
 
 }
 
