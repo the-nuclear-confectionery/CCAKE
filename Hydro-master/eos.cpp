84d12fa7f775fea0bae4446639ea1e9f73dfce0e
Christopher Plumberg <astrophysicist87@gmail.com>
2021-09-20 16:31:13 -0500

    Extra print statements

--- Hydro-master/eos.cpp
+++ Hydro-master/eos.cpp
@@ -229,6 +229,7 @@ double eos::dq_dmuq()    { return calc_term_4("q","q"); }
 double eos::dq_dmus()    { return calc_term_4("q","s"); }
 
 double eos::calc_term_1() {
+	if ( VERBOSE > 1 ) std::cout << "Now in " << __PRETTY_FUNCTION__ << std::endl;
     gsl_vector *v = gsl_vector_alloc(3);
     gsl_matrix *m = gsl_matrix_alloc(3,3);
 
@@ -254,6 +255,8 @@ double eos::calc_term_1() {
 }
 
 double eos::calc_term_2(string i_char) {
+	if ( VERBOSE > 1 ) std::cout << "Now in " << __PRETTY_FUNCTION__
+								 << ": i_char = " << i_char << std::endl;
     gsl_vector *a = gsl_vector_alloc(3);
     gsl_matrix *m = gsl_matrix_alloc(3,3);
     gsl_vector *b = gsl_vector_alloc(3);
@@ -330,6 +333,8 @@ double eos::calc_term_2(string i_char) {
 }
 
 double eos::calc_term_3(string i_char) {
+	if ( VERBOSE > 1 ) std::cout << "Now in " << __PRETTY_FUNCTION__
+								 << ": i_char = " << i_char << std::endl;
     gsl_vector *a = gsl_vector_alloc(3);
     gsl_matrix *m = gsl_matrix_alloc(3,3);
     gsl_vector *b = gsl_vector_alloc(3);
@@ -406,6 +411,9 @@ double eos::calc_term_3(string i_char) {
 }
 
 double eos::calc_term_4(string j_char, string i_char) {
+	if ( VERBOSE > 1 ) std::cout << "Now in " << __PRETTY_FUNCTION__
+								 << ": j_char, i_char = "
+								<< j_char << "   " << i_char << std::endl;
     gsl_vector *a = gsl_vector_alloc(3);
     gsl_matrix *m = gsl_matrix_alloc(3,3);
     gsl_vector *b = gsl_vector_alloc(3);
