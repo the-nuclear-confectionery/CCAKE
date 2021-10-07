378a514f4ac38b9fef36953f1d76f26457950f69
Christopher Plumberg <astrophysicist87@gmail.com>
2021-10-07 15:32:13 -0500

    Whitespace changes to constants.h, preliminary changes to particle.h

--- OVERHAUL/particle.h
+++ OVERHAUL/particle.h
@@ -1,1 +1,41 @@
-//here goes my main program hydrosim.cpp
+#ifndef PARTICLE_H
+#define PARTICLE_H
+
+#include <algorithm>
+#include <cmath>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <fstream>
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <vector>
+
+struct SPH_thermo
+{
+  double T, muB, muS, muQ;
+  double e, p, s, B, S, Q;
+  double dwds, dwdB, dwdS, dwdQ;
+}
+
+class particle
+{
+  // Constructors and destructors.
+  // particle();
+  // ~particle();
+
+
+  private:
+    vector<double> position;  // (x,y) in fm
+
+    SPH_thermo thermodynamic_state;
+
+
+  public:
+
+
+
+}
+
+#endif
