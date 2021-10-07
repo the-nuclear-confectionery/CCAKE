048569062d985b75a896c7faf68a9b243ebff574
Christopher Plumberg <astrophysicist87@gmail.com>
2021-10-07 16:05:57 -0500

    Update particle.h

--- OVERHAUL/particle.h
+++ OVERHAUL/particle.h
@@ -12,13 +12,6 @@
 #include <string>
 #include <vector>
 
-struct SPH_thermo
-{
-  double T, muB, muS, muQ;
-  double e, p, s, B, S, Q;
-  double dwds, dwdB, dwdS, dwdQ;
-}
-
 class particle
 {
   // Constructors and destructors.
@@ -26,10 +19,41 @@ class particle
   // ~particle();
 
 
-  private:
-    vector<double> position;  // (x,y) in fm
 
-    SPH_thermo thermodynamic_state;
+
+  private:
+    // kinematic quantities
+    vector<double> r;  // (x,y) in fm
+    vector<double> v;  // velocity
+    vector<double> u;  // relativistic velocity
+
+    // thermodynamic quantities
+    struct thermodynamic_info
+    {
+      double T, muB, muS, muQ;
+      double e, p, s, B, S, Q, cs2, w, A;
+      double dwds, dwdB, dwdS, dwdQ;
+    }
+    thermodynamic_info thermo;
+
+    // getter functions
+    double T()    { return thermo.T;    }
+    double muB()  { return thermo.muB;  }
+    double muS()  { return thermo.muS;  }
+    double muQ()  { return thermo.muQ;  }
+
+    double p()    { return thermo.p;    }
+    double s()    { return thermo.s;    }
+    double e()    { return thermo.e;    }
+    double B()    { return thermo.B;    }
+    double S()    { return thermo.S;    }
+    double Q()    { return thermo.Q;    }
+    double w()    { return thermo.w;    }
+    double A()    { return thermo.A;    }
+    double dwds() { return thermo.dwds; }
+    double dwdB() { return thermo.dwdB; }
+    double dwdS() { return thermo.dwdS; }
+    double dwdQ() { return thermo.dwdQ; }
 
 
   public:
