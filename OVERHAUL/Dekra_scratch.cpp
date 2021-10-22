8e65b654a5a8487d134f82be6ae841d16496e0c2
Dekrayat <69705532+Dekrayat@users.noreply.github.com>
2021-10-22 12:02:59 -0500

    structure

--- OVERHAUL/Dekra_scratch.cpp
+++ OVERHAUL/Dekra_scratch.cpp
@@ -1,30 +1,11 @@
-#include "BSQHydro.h"
-
+#include "input_output.h"
 
 // Constructors and destructors.
-  BSQHydro::BSQHydro(){}
- ~BSQHydro::BSQHydro(){}
+  input_output::input_output(){}
+ ~input_output::input_output(){}
 
 
-void BSQHydro::load_settings_file(string path_to_settings_file ){}
-
-
-
-void BSQHydro::set_results_directory( string path_to_results_directory ){}
-
-
-
-void BSQHydro::read_in_initial_conditions(){}
-
-
-
-void BSQHydro::initialize_hydrodynamics(){}
-
-
-void BSQHydro::run(){}
-
-
-void BSQHydro::find_freeze_out_surface(){}
-
-
-void BSQHydro::print_results(){}
+void input_output::read_in_initial_conditions()
+{
+  
+}
--- OVERHAUL/input_output.cpp
+++ OVERHAUL/input_output.cpp
@@ -1,1 +1,11 @@
-//here goes my main program hydrosim.cpp
+#include "input_output.h"
+
+// Constructors and destructors.
+  input_output::input_output(){}
+ ~input_output::input_output(){}
+
+
+void input_output::read_in_initial_conditions()
+{
+  
+}
