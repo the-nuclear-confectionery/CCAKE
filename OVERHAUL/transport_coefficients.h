3b109c8c67f93a660e4dc7ec366415b1eea62448
Christopher Plumberg <astrophysicist87@gmail.com>
2021-10-22 09:48:48 -0500

    Update transport_coefficients.h

--- OVERHAUL/transport_coefficients.h
+++ OVERHAUL/transport_coefficients.h
@@ -1,1 +1,15 @@
-//here goes my main program hydrosim.cpp
+#ifndef TRANSPORT_COEFFICIENTS_H
+#define TRANSPORT_COEFFICIENTS_H
+
+namespace transport_coefficients
+{
+
+  double eta_over_s(double T)
+  {
+      return 0.08; 
+  }
+
+}
+
+
+#endif
