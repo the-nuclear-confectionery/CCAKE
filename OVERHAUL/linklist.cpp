83ffc37fd06622473027da3a8583244d75d10a0a
Christopher Plumberg <astrophysicist87@gmail.com>
2021-10-23 11:51:12 -0500

    Add needed class files for matrix and vector calculations, as well as updates to linklist* files

--- OVERHAUL/linklist.cpp
+++ OVERHAUL/linklist.cpp
@@ -1,6 +1,3 @@
-#ifndef _LINKLIST_H_
-#define _LINKLIST_H_
-
 #include "vector.h"
 #include "matrix.h"
 #include "particle.h"
@@ -21,9 +18,10 @@ LinkList::LinkList()
 {
     range = 2; //number of boxes on the sides
     for (int i=0; i<2; i++) uni.x[i]=1.0;
-
-
 }
+
+LinkList::~LinkList(){}
+
 
 void LinkList::destroy()
 {
@@ -45,12 +43,6 @@ void LinkList::setup(double it0, int ntot,double h, Particle<D> *_pin,double dts
     dael=new Vector<int,D>[_n];
     steps=100*(floor(tend-t0)+1);
 
-//    ep1=new double [steps];
-//    ep2=new double [steps];
-//    ec1=new double [steps];
-//    ec2=new double [steps];
-//    tsave=new double [steps];
-
     dt=dtsave;
     number_part=numpart;
     avgetasig=0.;
@@ -65,7 +57,6 @@ void LinkList::endEV()
     delete [] dael;
 }
 
-LinkList::~LinkList(){}
 
 int LinkList::triToSum(Vector<int,2>  dael, Vector<int,2>  size)
 {
@@ -93,11 +84,12 @@ void LinkList::conservation_entropy()
 
     S=0.;
 
-    for (int i=0; i<_n; i++) {
+    for (int i=0; i<_n; i++)
+    {
         S+= _p[i].eta_sigma*_p[i].sigmaweight;
-if (i==0)
-		cout << "\t\t --> " << i << "   " << _p[i].eta_sigma << "   "
-				<< _p[i].sigmaweight << "   " << S << endl;
+      if (i==0)
+          cout << "\t\t --> " << i << "   " << _p[i].eta_sigma << "   "
+              << _p[i].sigmaweight << "   " << S << endl;
     }
 
     if (first==1)
@@ -144,15 +136,15 @@ void LinkList::bsqsvconservation_E()
     E=0.;
     for (int i=0; i<_n; i++)
     {
-        E += ( _p[i].C* _p[i].g2 - _p[i].EOSp() - _p[i].bigPI + _p[i].shv.x[0][0] )
+        E += ( _p[i].C* _p[i].g2 - _p[i].eos.p() - _p[i].bigPI + _p[i].shv.x[0][0] )
               / _p[i].sigma*_p[i].sigmaweight*t;
 if (i==0)
 cout << "E: " << i << "   " << t
-		<< "   " << _p[i].EOST()
+		<< "   " << _p[i].eos.T()
 		<< "   " << _p[i].EOSe()
 		<< "   " << _p[i].C
 		<< "   " << _p[i].g2
-		<< "   " << _p[i].EOSp()
+		<< "   " << _p[i].eos.p()
 		<< "   " << _p[i].bigPI
 		<< "   " << _p[i].shv.x[0][0]
 		<< "   " << _p[i].sigma
@@ -201,10 +193,10 @@ void LinkList::bsqsvconservation_Ez()
 
     double t2=t*t;
     for (int i=0; i<_n; i++) {
-        dEz+=( _p[i].EOSp()+_p[i].bigPI+_p[i].shv33*t2)/_p[i].sigma*_p[i].sigmaweight;
+        dEz+=( _p[i].eos.p()+_p[i].bigPI+_p[i].shv33*t2)/_p[i].sigma*_p[i].sigmaweight;
 if (false)
 cout << "dEz: " << i << "   " << t
-		<< "   " << _p[i].EOSp()
+		<< "   " << _p[i].eos.p()
 		<< "   " << _p[i].bigPI
 		<< "   " << _p[i].shv33*t2
 		<< "   " << _p[i].sigma
@@ -232,7 +224,7 @@ void LinkList::initialize()
 
     for(int i=1; i<_n; i++)
     {
-        for(int j=0; j<D; j++) {
+        for(int j=0; j<2; j++) {
 
             if(_p[i].r.x[j]>max.x[j]) max.x[j]=_p[i].r.x[j];
 
@@ -244,23 +236,23 @@ void LinkList::initialize()
     //evaluate system size
 
     //2*range puts extra boxes on sides of grid
-    double sub=1./_h;
+    double sub=1.0/_h;
 
-    size=sub*(max-min)+(2.*range+1.)*uni;
+    size=sub*(max-min)+(2.0*range+1.0)*uni;
 
     //Size is the volume
     Size=1;
 
-    Vector<double,D> dsub;
+    Vector<double,2> dsub;
 
     // finds total volume of the system
-    for(int i=0; i<D; i++) Size*=size.x[i];
+    for(int i=0; i<2; i++) Size*=size.x[i];
 
 
     //dael: relates every particle with its linklist cube
     // also convert particle position to an integer
 
-    for(int j=0; j<_n; j++) dael[j]=sub*(_p[j].r-min)+(1.*range)*uni;
+    for(int j=0; j<_n; j++) dael[j]=sub*(_p[j].r-min)+(1.0*range)*uni;
 
     //lead: relates every linklist cube with one of the particles (leader) in it
     //link: links the leader particle of one cube with the others of the same cube
@@ -286,175 +278,175 @@ void LinkList::initialize()
 
 
 //if we include the SPH over rhoB, rhoS, rhoQ
-void LinkList::bsqsvoptimization(int a, bool init_mode /*== false*/)
+void LinkList::smooth_fields(int a, bool init_mode /*== false*/)
 {
-    _p[a].sigma = 0;
-    _p[a].eta = 0;
-    _p[a].rhoB_sub = 0.0;
-    _p[a].rhoS_sub = 0.0;
-    _p[a].rhoQ_sub = 0.0;
-	int neighbor_count = 0;
-    Vector<int,D> i;
-    for(i.x[0]=-2; i.x[0]<=2; i.x[0]++)
-        for(i.x[1]=-2; i.x[1]<=2; i.x[1]++)
-        {
-            int b = lead[triToSum(dael[a]+i, size)];
-            while( b!=-1 )
-            {
-                double kern  = kernel(_p[a].r-_p[b].r);
-				if (kern>0.0) neighbor_count++;
-                _p[a].sigma += _p[b].sigmaweight*kern;
-                _p[a].eta   += _p[b].sigmaweight*_p[b].eta_sigma*kern;
-                _p[a].rhoB_sub  += _p[b].rho_weight*_p[b].rhoB_an*kern;    //confirm with Jaki
-                _p[a].rhoS_sub  += _p[b].rho_weight*_p[b].rhoS_an*kern;    //confirm with Jaki
-                _p[a].rhoQ_sub  += _p[b].rho_weight*_p[b].rhoQ_an*kern;    //confirm with Jaki
+  const auto & pa    = _p[a];
+  pa.sigma           = 0.0;
+  pa.eta             = 0.0;
+  pa.rhoB_sub        = 0.0;
+  pa.rhoS_sub        = 0.0;
+  pa.rhoQ_sub        = 0.0;
+  int neighbor_count = 0;
 
-if (false)
-std::cout << "bsqsvoptimization(SPH particle == " << a << " ): "
-			<< b << "   " << _p[a].r
-			<< "   " << _p[a].sigma
-			<< "   " << _p[a].eta
-			<< "   " << _p[b].r
-			<< "   " << _p[b].sigmaweight
-			<< "   " << _p[b].eta_sigma
-			<< "   " << _p[b].rhoB_an
-			<< "   " << _p[a].rhoB_sub
-			<< "   " << _p[b].rhoS_an
-			<< "   " << _p[a].rhoS_sub
-			<< "   " << _p[b].rhoQ_an
-			<< "   " << _p[a].rhoQ_sub
-			<< "   " << kern << std::endl;
-
-                b=link[b];
-            }
-        }
-
-	//cout << "Check neighbor count: " << a << "   " << neighbor_count << endl;
-
-	// reset total B, S, and Q charge of each SPH particle to reflect
-	// smoothing from kernel function (ONLY ON FIRST TIME STEP)
-	//cout << "-----------------------------------------------------------------" << endl;
-	if ( init_mode )
-	{
-		//cout << "BEFORE: " << a << "   " << _p[a].B << "   " << _p[a].S << "   " << _p[a].Q << endl;
-		//cout << _p[a].rho_weight << "   " << _p[a].rhoB_an << "   " << _p[a].rhoS_an << "   " << _p[a].rhoQ_an << endl;
-		_p[a].B = _p[a].rhoB_sub * _p[a].rho_weight;
-		_p[a].S = _p[a].rhoS_sub * _p[a].rho_weight;
-		_p[a].Q = _p[a].rhoQ_sub * _p[a].rho_weight;
-		//cout << "AFTER: " << a << "   " << _p[a].B << "   " << _p[a].S << "   " << _p[a].Q << endl;
-		//cout << _p[a].rho_weight << "   " << _p[a].rhoB_sub << "   " << _p[a].rhoS_sub << "   " << _p[a].rhoQ_sub << endl;
-		//cout << "-----------------------------------------------------------------" << endl;
-	}
-
-    return;
-}
-
-
-
-
-void LinkList::bsqsvoptimization2(int a,double tin,int & count)
-{
-
-    _p[a].gradP=0.;
-    _p[a].gradBulk = 0.;
-    //_p[a].gradrhoB = 0.;
-    //_p[a].gradrhoS = 0.;
-    //_p[a].gradrhoQ = 0.;
-    _p[a].gradV = 0.;
-    _p[a].gradshear=0.;
-    _p[a].divshear=0.;
-
-    Vector<int,D> i;
-
-    if (_p[a].btrack!=-1) _p[a].btrack=0;
-
-    double rdis=0;
-
-    for(i.x[0]=-2; i.x[0]<=2; i.x[0]++)
+  Vector<int,2> i;
+  for ( i.x[0] =- 2; i.x[0] <= 2; i.x[0]++ )
+  for ( i.x[1] =- 2; i.x[1] <= 2; i.x[1]++ )
+  {
+    int b = lead[ triToSum( dael[a] + i, size ) ];
+    while ( b != -1 )
     {
-        for(i.x[1]=-2; i.x[1]<=2; i.x[1]++)
-        {
+      const auto & pb = _p[b];
+      double kern     = kernel( pa.r - pb.r );
+      pa.sigma       += pb.sigmaweight*kern;
+      pa.eta         += pb.sigmaweight*pb.eta_sigma*kern;
+      pa.rhoB_sub    += pb.rho_weight*pb.rhoB_an*kern;    //confirm with Jaki
+      pa.rhoS_sub    += pb.rho_weight*pb.rhoS_an*kern;    //confirm with Jaki
+      pa.rhoQ_sub    += pb.rho_weight*pb.rhoQ_an*kern;    //confirm with Jaki
 
-            int b=lead[triToSum(dael[a]+i, size)];
+      if (kern>0.0) neighbor_count++;
+      if (false)
+        std::cout << "bsqsvoptimization(SPH particle == " << a << " ): "
+        << b << "   " << pa.r
+        << "   " << pa.sigma
+        << "   " << pa.eta
+        << "   " << pb.r
+        << "   " << pb.sigmaweight
+        << "   " << pb.eta_sigma
+        << "   " << pb.rhoB_an
+        << "   " << pa.rhoB_sub
+        << "   " << pb.rhoS_an
+        << "   " << pa.rhoS_sub
+        << "   " << pb.rhoQ_an
+        << "   " << pa.rhoQ_sub
+        << "   " << kern << std::endl;
 
-
-            while(b!=-1 )
-            {
-
-                Vector<double,D> gradK=gradKernel(_p[a].r-_p[b].r, static_cast<bool>(a == 30 && b == 43));
-                Vector<double,D> va=rowp1(0,_p[a].shv);
-                Vector<double,D> vb=rowp1(0,_p[b].shv);
-                Matrix<double,D,D> vminia,vminib;
-                mini(vminia,_p[a].shv);
-                mini(vminib,_p[b].shv);
-                double sigsqra=1/(_p[a].sigma*_p[a].sigma);
-                double sigsqrb=1/(_p[b].sigma*_p[b].sigma);
-                Vector<double,D> sigsigK=_p[b].sigmaweight*_p[a].sigma*gradK;
-
-/*if (a == 30 && b == 43)
-	cout << "CHECK PARTICLE: " << a << "   " << b << "   " << t << "   "
-		<< _p[a].r << "   " << _p[b].r << "   "
-		<< _p[b].sigmaweight << "   " << _p[a].sigma << "   "
-		<< gradK << "   " << sigsigK << endl;*/
-
-                _p[a].gradP +=( sigsqrb*_p[b].EOSp()+ sigsqra*_p[a].EOSp() )*sigsigK;
-
-                if (((Norm(_p[a].r-_p[b].r)/_h)<=2)&&(a!=b)) {
-                    if (_p[a].btrack!=-1) _p[a].btrack++;
-                    if (_p[a].btrack==1) rdis=Norm(_p[a].r-_p[b].r)/_h;
-                }
-
-                _p[a].gradBulk += ( _p[b].Bulk/_p[b].sigma/_p[b].gamma + _p[a].Bulk/_p[a].sigma/_p[a].gamma)/tin*sigsigK;
-                //_p[a].gradrhoB += ( _p[b].rhoB/_p[b].sigma/_p[b].gamma + _p[a].rhoB/_p[a].sigma/_p[a].gamma)/tin*sigsigK;
-                //_p[a].gradrhoS += ( _p[b].rhoS/_p[b].sigma/_p[b].gamma + _p[a].rhoS/_p[a].sigma/_p[a].gamma)/tin*sigsigK;
-                //_p[a].gradrhoQ += ( _p[b].rhoQ/_p[b].sigma/_p[b].gamma + _p[a].rhoQ/_p[a].sigma/_p[a].gamma)/tin*sigsigK;
-                _p[a].gradV+=(_p[b].sigmaweight/_p[a].sigma*( _p[b].v -  _p[a].v ))*gradK;
-
-
-
-                _p[a].gradshear+=(inner(sigsigK,_p[a].v))*(sigsqrb*vb+sigsqra*va);
-                _p[a].divshear+=(sigsqrb*(sigsigK*transpose(vminib))+sigsqra*(sigsigK*transpose(vminia)));
-
-
-
-                if(isnan(_p[a].gradP.x[0])) {
-                    cout << "gradP stopped working" << endl;
-                    cout << t <<" "  << _p[a].gradP <<  " " << a << " " << b << endl;
-                    cout << _p[b].sigmaweight << " " <<     _p[a].sigma << " " <<  _p[b].EOSp() << " " << endl;
-                    cout <<   Size << " " <<  _p[b].EOSs() << " " << _p[a].EOSs() <<endl;
-
-                    cout << _p[a].r << endl;
-                    cout << _p[b].r << endl;
-                    cout << kernel(_p[a].r-_p[b].r) << endl;
-
-                }
-                else if (isnan(_p[a].gradP.x[1])) {
-                    cout << "1 " << gradPressure_weight(a,b) <<  " " << a << " " << b << endl;
-
-                }
-                else if (isnan(_p[a].gradP.x[2])) {
-                    cout << "2 " << gradPressure_weight(a,b) <<  " " << a << " " << b << endl;
-
-                }
-
-
-
-                b=link[b];
-            }
-        }
+      b = link[b];
     }
+  }
 
-    if ((_p[a].btrack==1)&&((_p[a].EOST()*197.3)>=150)) {
-        _p[a].frz2.t=tin;
-    }
-    else if ((_p[a].btrack==0)&&((_p[a].EOST()*197.3)>=150)&&(_p[a].Freeze<4)) {
-        cout <<"Missed " << a << " "<< tin << "  " << _p[a].EOST()*197.3 << " " << rdis << " " << cfon <<  endl;
-    }
+  //cout << "Check neighbor count: " << a << "   " << neighbor_count << endl;
 
+  // reset total B, S, and Q charge of each SPH particle to reflect
+  // smoothing from kernel function (ONLY ON FIRST TIME STEP)
+  //cout << "-----------------------------------------------------------------" << endl;
+  if ( init_mode )
+  {
+    //cout << "BEFORE: " << a << "   " << pa.B << "   "
+    //    << pa.S << "   " << pa.Q << endl;
+    //cout << pa.rho_weight << "   " << pa.rhoB_an << "   "
+    //    << pa.rhoS_an << "   " << pa.rhoQ_an << endl;
+    pa.B = pa.rhoB_sub * pa.rho_weight;
+    pa.S = pa.rhoS_sub * pa.rho_weight;
+    pa.Q = pa.rhoQ_sub * pa.rho_weight;
+    //cout << "AFTER: " << a << "   " << pa.B << "   "
+    //    << pa.S << "   " << pa.Q << endl;
+    //cout << pa.rho_weight << "   " << pa.rhoB_sub << "   "
+    //    << pa.rhoS_sub << "   " << pa.rhoQ_sub << endl;
+    //cout << "-----------------------------------------------------------------" << endl;
+  }
 
+  return;
 }
 
 
 
 
-#endif
+void LinkList::smooth_gradients( int a, double tin, int & count )
+{
+  const auto & pa    = _p[a];
+
+  pa.gradP     = 0.0;
+  pa.gradBulk  = 0.0;
+//  pa.gradrhoB  = 0.0;
+//  pa.gradrhoS  = 0.0;
+//  pa.gradrhoQ  = 0.0;
+  pa.gradV     = 0.0;
+  pa.gradshear = 0.0;
+  pa.divshear  = 0.0;
+
+  Vector<int,2> i;
+
+  if ( pa.btrack != -1 ) pa.btrack = 0;
+
+  double rdis = 0;
+
+  for ( i.x[0] =- 2; i.x[0] <= 2; i.x[0]++ )
+  for ( i.x[1] =- 2; i.x[1] <= 2; i.x[1]++ )
+  {
+
+    int b=lead[ triToSum( dael[a] + i, size ) ];
+
+    while( b != -1 )
+    {
+      const auto & pb          = _p[b];
+
+      Vector<double,2> gradK   = gradKernel( pa.r - pb.r,
+                                  static_cast<bool>( a == 30 && b == 43 ) );
+      Vector<double,2> va      = rowp1(0, pa.shv);
+      Vector<double,2> vb      = rowp1(0, pb.shv);
+      Matrix<double,2,2> vminia, vminib;
+
+      mini(vminia, pa.shv);
+      mini(vminib, pb.shv);
+
+      double sigsqra           = 1.0/(pa.sigma*pa.sigma);
+      double sigsqrb           = 1.0/(pb.sigma*pb.sigma);
+      Vector<double,2> sigsigK = pb.sigmaweight * pa.sigma * gradK;
+
+      pa.gradP                += ( sigsqrb*pb.eos.p()
+                                  + sigsqra*pa.eos.p() ) * sigsigK;
+
+      if ( ( ( Norm( pa.r - pb.r ) / _h ) <= 2 ) && ( a != b ) )
+      {
+        if ( pa.btrack != -1 ) pa.btrack++;
+        if ( pa.btrack ==  1 ) rdis = Norm(pa.r-pb.r)/_h;
+      }
+
+      pa.gradBulk             += ( pb.Bulk/pb.sigma/pb.gamma
+                                    + pa.Bulk/pa.sigma/pa.gamma)/tin*sigsigK;
+      //pa.gradrhoB             += ( pb.rhoB/pb.sigma/pb.gamma
+      //                            + pa.rhoB/pa.sigma/pa.gamma)/tin*sigsigK;
+      //pa.gradrhoS             += ( pb.rhoS/pb.sigma/pb.gamma
+      //                            + pa.rhoS/pa.sigma/pa.gamma)/tin*sigsigK;
+      //pa.gradrhoQ             += ( pb.rhoQ/pb.sigma/pb.gamma
+      //                            + pa.rhoQ/pa.sigma/pa.gamma)/tin*sigsigK;
+      pa.gradV                += pb.sigmaweight*( pb.v -  pa.v )*gradK/pa.sigma;
+
+      pa.gradshear            += inner(sigsigK, pa.v)*( sigsqrb*vb + sigsqra*va );
+      pa.divshear             += sigsqrb*sigsigK*transpose(vminib)
+                                  + sigsqra*sigsigK*transpose(vminia);
+
+      if ( isnan( pa.gradP.x[0] ) )
+      {
+        cout << "gradP stopped working" << endl;
+        cout << t <<" "  << pa.gradP << " " << a << " " << b << endl;
+        cout << pb.sigmaweight << " " << pa.sigma << " " << pb.eos.p() << endl;
+        cout << Size << " " << pb.eos.s() << " " << pa.eos.s() << endl;
+
+        cout << pa.r << endl;
+        cout << pb.r << endl;
+        cout << kernel( pa.r - pb.r ) << endl;
+      }
+      else if ( isnan( pa.gradP.x[1] ) )
+        cout << "1 " << gradPressure_weight(a, b)
+             << " " << a << " " << b << endl;
+      else if ( isnan( pa.gradP.x[2] ) )
+        cout << "2 " << gradPressure_weight(a, b)
+             << " " << a << " " << b << endl;
+
+      b=link[b];
+    }
+  }
+
+  if ( ( pa.btrack == 1 )
+        && ( ( pa.eos.T()*197.3 ) >= 150 ) )
+    pa.frz2.t=tin;
+  else if ( ( pa.btrack == 0 )
+            && ( ( pa.eos.T()*197.3 ) >= 150 )
+            && ( pa.Freeze < 4 ) )
+    cout << "Missed " << a << " " << tin << "  "
+         << pa.eos.T()*197.3 << " "
+         << rdis << " " << cfon <<  endl;
+
+  return;
+}
