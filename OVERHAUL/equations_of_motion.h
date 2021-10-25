aa268f2d651f3967e293e7ee57d7a583cb2680a6
Dekrayat <69705532+Dekrayat@users.noreply.github.com>
2021-10-25 16:37:47 -0500

    update equations of motion

--- OVERHAUL/equations_of_motion.h
+++ OVERHAUL/equations_of_motion.h
@@ -1,1 +1,21 @@
-//here goes my main program hydrosim.cpp
+#ifndef EQUATIONS_OF_MOTION_H
+#define EQUATIONS_OF_MOTION_H
+
+#include <algorithm>
+#include <cmath>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <fstream>
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <vector>
+#include <stdlib.h>
+
+
+#include "mathdef.h"
+#include "vector.h"
+#include "particle.h"
+#include "linklist.h"
+#include "system_state.h"
