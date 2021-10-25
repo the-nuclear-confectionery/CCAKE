c5dd9e29c4272568c6fa8bad980c17f0a44dd933
Dekrayat <69705532+Dekrayat@users.noreply.github.com>
2021-10-25 13:38:42 -0500

    Update Dekra_scratch.cpp

--- OVERHAUL/Dekra_scratch.cpp
+++ OVERHAUL/Dekra_scratch.cpp
@@ -24,34 +24,52 @@ infile.close();
 return;
 }
 //=============================================================================
-void input_output::read_in_initial_conditions()
+void input_output::read_in_initial_conditions( int &_Ntable3, 
+double factor, double const & efcheck,int & numpart)
 {
-void readICs_iccing(string filename, int &_Ntable3, double factor, double const & efcheck,int &
- numpart)
-{
-//const double hbarC = 0.1973;
-cout << "Reading in ICCING initial conditions!" << endl;
-//string filename;
-filename = ifolder+"ic0.dat";
-ifstream infile( filename.c_str() );
-cout << "Initial conditions file: " << filename << endl;
+  string initial_condition_type = input_parameters.IC_type;
+  int n_header_lines;
+  string IC_file = 'All_Initial_Conds/'
+  switch(initial_condition_type)
+  {
+    case 'ICCING' :
+      cout << "Reading in ICCING initial conditions!" << endl;
+      IC_file = IC_file+'Iccing_conditions.dat' // need to change ic0.dat
+    case default :
+      cout << "Selected initial condition type not supported."
+  }
+
+ifstream infile(IC_file.c_str());
+cout << "Initial conditions file: " << IC_file << endl;
 if (infile.is_open())
 {
-string line;
-vector<double> xsub,ysub,esub,rBsub,rSsub,rQsub;
-int count=0;
- while ( getline (infile, line) )
-{
-  istringstream iss(line);
+  string line;
+  vector<vector<double>> all_initials;
+  int count=0;
   if count<n_header_lines
-    { 
-     iss >> xsub >> ysub >> esub >> rBsub >> rSsub >> rQsub;
-    }
-    else 
-    {
-    iss >> xsub >> ysub >> esub >> rBsub >> rSsub >> rQsub;  
-    iss >> dummy;
-    xsub.push_back(dummy);
+while ( getline (infile, line) )
+        {
+            istringstream iss(line);
+            iss >> xsub >> ysub >> esub >> rBsub >> rSsub >> rQsub;
+
+        }
+else
+{
+ if (y[2]>0.00301)
+//if (y[2]>max(0.01,0.5*efcheck*hbarC)) //N.B. leave wiggle room around FO temp
+{
+// check if this file has finite energy density and zero charge densities
+// (seems to cause problems in EOS)
+const double eps_local = 1e-6;
+/*if (   y[2] >= eps_local && y[3] < eps_local
+	&& y[4] < eps_local  && y[5] < eps_local)
+	{
+		 y[3] = eps_local;
+		 y[4] = eps_local;
+		 y[5] = eps_local;
+	}*/
+iss >> dummy;
+    xsub.push_back(y[0]);
     iss>>dummy;
     ysub.push_back(y[1]);
     iss>>dummy;
@@ -62,111 +80,78 @@ int count=0;
     rSsub.push_back(y[4]);
     iss>>dummy;
     rQsub.push_back(y[5]);
-
-    }
-
+}
 count ++;
 
 if (!infile.is_open())
     {
-        cout << "Can't open " << filename << endl;
+        cout << "Can't open " << IC_file << endl;
         exit(1);
     }
   }
 }
-
-
+}
+{
 //cout << "CHECK(" << __LINE__ << "): " << j << "   " << x[j] << "   " << y[j] << endl;
         }
 }
-}
-}
-//==============================================================
-// Add new check here to enforce freeze-out criterion before
-// setting particle list size!!!
+infile.close();
 
-// do not scale by factor!!!
-//if ((factor*y[2])>0.01)
-if (y[2]>0.00301)
-//if (y[2]>max(0.01,0.5*efcheck*hbarC)) //N.B. leave wiggle room around FO temp
-{
-// check if this file has finite energy density and zero charge densities
-// (seems to cause problems in EOS)
+// Here goes the formation of SPH from the initial densities of ICCING 
+numberof_sph=xsub.size();
+p= new Particle<2>[numberof_sph];
 
-const double eps_local = 1e-6;
-
-/*if (   y[2] >= eps_local && y[3] < eps_local
-	&& y[4] < eps_local  && y[5] < eps_local)
+cout << "After e-cutoff and freeze-out: size = " << numberof_sph << endl;
+int kk=numberof_sph;
+initial_frozen_sph=0;	//number of frozen out particles initially
+for(int j=0; j<numberof_sph; j++)
 	{
-		 y[3] = eps_local;
-		 y[4] = eps_local;
-		 y[5] = eps_local;
-	}*/
-xsub.push_back(y[0]);
-ysub.push_back(y[1]);
-esub.push_back(y[2]);
-rBsub.push_back(y[3]);
-rSsub.push_back(y[4]);
-rQsub.push_back(y[5]);
-}
-
-input.close();
-
-_Ntable3=xsub.size();
-_p= new Particle<2>[_Ntable3];
-
-cout << "After e-cutoff and freeze-out: size = " << _Ntable3 << endl;
-
-int kk=_Ntable3;
-numpart=0;	//number of frozen out particles
-
-for(int j=0; j<_Ntable3; j++)
-	{
-const auto & pj = _p[j];
-pj.r.x[0]=xsub[j];
-pj.r.x[1]=ysub[j];
-// pj.e_sub=EOS.e_out(factor*esub[j]);
-pj.e_sub=esub[j]/hbarC;        // not s_an!!  convert to 1/fm^4 and do not rescale by factor!
-pj.u.x[0]=0;
-pj.u.x[1]=0;
-pj.eta_sigma = 1;
-pj.sigmaweight=stepx*stepy;
-pj.rho_weight = stepx*stepy;
-pj.Bulk = 0;
-pj.B=rBsub[j]*stepx*stepy;			
-pj.S=rSsub[j]*stepx*stepy;			
-pj.Q=rQsub[j]*stepx*stepy;			
-pj.rhoB_an=rBsub[j];					
-pj.rhoS_an=rSsub[j];					
-pj.rhoQ_an=rQsub[j];					
-pj.transverse_area = stepx*stepy;
+const auto & p = _p[j]; 
+p.r.x[0]=xsub[j]; // set the initial x position of an sph particle 
+p.r.x[1]=ysub[j]; // set the initial y position of an sph particle 
+// p.e_sub=EOS.e_out(factor*esub[j]);
+p.e_sub=esub[j]/hbarC; // set the initial energy density of an sph particle       
+// not s_an!!  convert to 1/fm^4 and do not rescale by factor!
+p.u.x[0]=0; // set the initial flow velocity
+p.u.x[1]=0; // 
+p.entropy_sigma = 1;// it was eta_sigma
+p.sigmaweight=stepx*stepy; // nu_\alpha
+p.rho_weight = stepx*stepy; // Chris?
+p.Bulk = 0;
+p.B=rBsub[j]*stepx*stepy;	// nu alpha * 		
+p.S=rSsub[j]*stepx*stepy;			
+p.Q=rQsub[j]*stepx*stepy;			
+p.rhoB_an=rBsub[j];					
+p.rhoS_an=rSsub[j];					
+p.rhoQ_an=rQsub[j];					
+p.transverse_area = stepx*stepy;
 
 if (j==0)
 	cout << "readICs_iccing(" << __LINE__ << "): "
 	<< "SPH particles: "
 	<< j << "   "
-	<< pj.r.x[0] << "   " << pj.r.x[1] << "   "
-	<< pj.e_sub << "   " << pj.rhoB_an << "   "
-	<< pj.rhoS_an << "   " << pj.rhoQ_an << "   "
-	<< pj.sigmaweight << endl;
+	<< p.r.x[0] << "   " << p.r.x[1] << "   "
+	<< p.e_sub << "   " << p.rhoB_an << "   "
+	<< p.rhoS_an << "   " << p.rhoQ_an << "   "
+	<< p.sigmaweight << endl;
 // make educated initial guess here for this particle's (T, mu_i) coordinates
 // (improve this in the future)
-	pj.SPH_cell.T   = 500.0/hbarc_MeVfm;	// rootfinder seems to work better going downhill than "uphill"
-	pj.SPH_cell.muB = 0.0/hbarc_MeVfm;
-	pj.SPH_cell.muS = 0.0/hbarc_MeVfm;
-	pj.SPH_cell.muQ = 0.0/hbarc_MeVfm;
+	p.SPH_cell.T   = 500.0/hbarc_MeVfm;	// rootfinder seems to work better going downhill than "uphill"
+	p.SPH_cell.muB = 0.0/hbarc_MeVfm;
+	p.SPH_cell.muS = 0.0/hbarc_MeVfm;
+	p.SPH_cell.muQ = 0.0/hbarc_MeVfm;
 
-  if (pj.e_sub>efcheck)	          // impose freeze-out check for e, not s
+  if (p.e_sub>efcheck)	          // impose freeze-out check for e, not s
   {
-            pj.Freeze=0;
+            p.Freeze=0;
         }
         else
         {
-            pj.Freeze=4;
+            p.Freeze=4;
             --kk;
-            ++numpart;
+            ++initial_frozen_sph;
         }
     }
-cout << "After freezeout (redundant): size = " << _Ntable3-numpart << endl;
+cout << "After freezeout (redundant): size = " << numberof_sph-initial_frozen_sph << endl;
 }
 }
