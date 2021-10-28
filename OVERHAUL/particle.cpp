a9e3a051692c3ce4dd85df3be6db8c53759ebf7e
Christopher Plumberg <astrophysicist87@gmail.com>
2021-10-28 18:48:01 -0500

    Use shared_ptr

--- OVERHAUL/particle.cpp
+++ OVERHAUL/particle.cpp
@@ -49,10 +49,16 @@ Particle::Particle( const Particle& p )
 
 
 ////////////////////////////////////////////////////////////////////////////////
-void Particle::set_EquationOfStatePtr( EquationOfState * eosPtr_in )
+//void Particle::set_EquationOfStatePtr( EquationOfState * eosPtr_in )
+//{
+//  eosPtr = eosPtr_in;
+//}
+
+void Particle::set_EquationOfStatePtr( std::shared_ptr<EquationOfState> eosPtr_in )
 {
   eosPtr = eosPtr_in;
 }
+
 
 
 ////////////////////////////////////////////////////////////////////////////////
