c80f52d053883a8f5ef0dfeea3eded3f633daa64
tdore738 <tdore738@gmail.com>
2021-10-28 11:33:56 -0500

    Update sph_workstation.cpp

--- OVERHAUL/sph_workstation.cpp
+++ OVERHAUL/sph_workstation.cpp
@@ -562,7 +562,7 @@ void SPHWorkstation::process_initial_conditions()
 
 		if (p.e_sub>settingsPtr->efcheck)	// impose freeze-out check for e, not s
     {
-      cout << "Found " << p.e_sub << " greater than " << systemPtr->efcheck << endl;
+    //   cout << "Found " << p.e_sub << " greater than " << systemPtr->efcheck << endl;
 			p.Freeze=0;
 		}
 		else
