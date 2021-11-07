0244d8ed05a72fa0965b5dce1c29d865eb5c934a
Christopher Plumberg <astrophysicist87@gmail.com>
2021-11-06 23:39:39 -0500

    Add N-dimensional interpolator

---
+++ OVERHAUL/interpolatorND.h
@@ -0,0 +1,43 @@
+#ifndef InterpolatorND_H
+#define InterpolatorND_H
+
+#include <string>
+#include <vector>
+
+using namespace std;
+
+template <int D>
+class InterpolatorND
+{
+  public:
+    InterpolatorND(){}
+    ~InterpolatorND(){}
+
+    void initialize( string filename );
+
+    void evaluate( vector<double> & coordinates, vector<double> & results );
+
+  private:
+    int n_points_in_hypercube;
+    vector<int> grid_sizes;
+    vector<double> grid_spacings, grid_mins, grid_maxs;
+
+    vector<vector<int> > hypercube_indices;
+    vector<vector<double> > grid, grid_points, fields;
+
+    inline size_t indexer( vector<int> & indices )
+    {
+      size_t result = indices[0];
+      for ( size_t ind = 1; ind < indices.size(); ind++ )
+        result = result * grid_sizes[ind] + indices[ind];
+      return result;
+    }
+
+    void load_data( string filename );
+    void construct_interpolant();
+
+};
+
+
+
+#endif
