efa40801ac7921758e9c77673a7a72b4f6da0fb7
Christopher Plumberg <astrophysicist87@gmail.com>
2021-11-12 12:33:38 -0600

    Try this?

--- OVERHAUL/input_output.cpp
+++ OVERHAUL/input_output.cpp
@@ -92,7 +92,7 @@ for ( auto & entry : all_parameters )
                        "equation of state!  Switching to gas of massless gluons"
                        " and 2.5 massless quarks" << std::endl;
           settingsPtr->EoS_type = "Conformal";
-          settingsPtr->Freeze_Out_Temperature = 0.01;  // MeV
+          settingsPtr->Freeze_Out_Temperature = 1e-10;  // MeV
         }
 
         infile.close();
