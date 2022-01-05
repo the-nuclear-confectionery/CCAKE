c0ffd10bb99620fecd991bef48d2803edc663329
Christopher Plumberg <astrophysicist87@gmail.com>
2022-01-05 14:14:32 -0600

    Update plot_trajectories.py

--- OVERHAUL/plotting_scripts/plot_trajectories.py
+++ OVERHAUL/plotting_scripts/plot_trajectories.py
@@ -1,6 +1,7 @@
 import numpy as np
 import sys
 
-data = np.stack([np.loadtxt(file, usecols=()) for file in sys.argv[1:]])
+data = np.stack([np.loadtxt(file, usecols=(5,6,7,8), skiprows=1) \
+                 for file in sys.argv[1:]])
 
 print(data.shape)
