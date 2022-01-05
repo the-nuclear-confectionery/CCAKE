8e7381ce20a71a25d280ef9b02829da3b2a178c6
Christopher Plumberg <astrophysicist87@gmail.com>
2022-01-05 14:32:34 -0600

    Update plot_trajectories.py

--- OVERHAUL/plotting_scripts/plot_trajectories.py
+++ OVERHAUL/plotting_scripts/plot_trajectories.py
@@ -1,7 +1,20 @@
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
 import numpy as np
 import sys
 
 data = np.stack([np.loadtxt(file, usecols=(5,6,7,8), skiprows=1) \
                  for file in sys.argv[1:]])
 
+data = np.swapaxes(data, 0, 1)
+
 print(data.shape)
+
+data = data[np.where(data[0,0] > 150.0)]
+
+print(data.shape)
+
+#plt.figure(figsize=(8,8), dpi=500)
+
+#plt.plot(  )
