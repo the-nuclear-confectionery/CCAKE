997c73dd4faa6c8290573cfbb31b1345ade24a4c
Christopher Plumberg <astrophysicist87@gmail.com>
2022-01-21 15:43:00 -0600

    Make rhoS and rhoQ animations by default as well

--- OVERHAUL/generate_profile_evolution.sh
+++ OVERHAUL/generate_profile_evolution.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-maxFrames=250
-framesPerSecond=12
+maxFrames=300
+framesPerSecond=30
 
 resultsDirectory=results
 physicalProfileStem=physical_quantities
@@ -22,9 +22,20 @@ python3 plot_profile.py \
 	${resultsDirectory}/${physicalProfileStem} \
 	${resultsDirectory}/"rhoBProfile" \
 	${insuffix} ${outsuffix} $nFrames '$\rho_B$ (fm$^{-3}$)' 8
+python3 plot_profile.py \
+	${resultsDirectory}/${physicalProfileStem} \
+	${resultsDirectory}/"rhoSProfile" \
+	${insuffix} ${outsuffix} $nFrames '$\rho_S$ (fm$^{-3}$)' 9
+python3 plot_profile.py \
+	${resultsDirectory}/${physicalProfileStem} \
+	${resultsDirectory}/"rhoQProfile" \
+	${insuffix} ${outsuffix} $nFrames '$\rho_Q$ (fm$^{-3}$)' 10
 
 mkdir -p $moviesDirectory
-rm -rf ${moviesDirectory}/eProfile.mp4 ${moviesDirectory}/rhoBProfile.mp4
+rm -rf ${moviesDirectory}/eProfile.mp4 ${moviesDirectory}/rhoBProfile.mp4 \
+       ${moviesDirectory}/rhoSProfile.mp4  ${moviesDirectory}/rhoQProfile.mp4
 pngs2mp4 $framesPerSecond ${resultsDirectory}/"eProfile"%03d${outsuffix} ${moviesDirectory}/eProfile.mp4
 pngs2mp4 $framesPerSecond ${resultsDirectory}/"rhoBProfile"%03d${outsuffix} ${moviesDirectory}/rhoBProfile.mp4
+pngs2mp4 $framesPerSecond ${resultsDirectory}/"rhoSProfile"%03d${outsuffix} ${moviesDirectory}/rhoSProfile.mp4
+pngs2mp4 $framesPerSecond ${resultsDirectory}/"rhoQProfile"%03d${outsuffix} ${moviesDirectory}/rhoQProfile.mp4
 
