2c1131f5648d48c7c42c57300122475490b4678a
Christopher Plumberg <astrophysicist87@gmail.com>
2022-01-22 15:46:58 -0600

    Change colormaps to be consistent with Patrick's conventions, generate a few extra animations, fix colorscale range from first frame

--- OVERHAUL/plot_profile.py
+++ OVERHAUL/plot_profile.py
@@ -2,8 +2,34 @@ import numpy as np
 import matplotlib
 matplotlib.use('Agg')
 import matplotlib.pyplot as plt
+from matplotlib.colors import LinearSegmentedColormap
 import os, sys
 
+cmap_energy \
+    = LinearSegmentedColormap.from_list( 'energy',\
+        ( (0.000, (1.000, 1.000, 1.000)),\
+          (0.250, (1.000, 1.000, 1.000)),\
+          (0.500, (1.000, 1.000, 1.000)),\
+          (0.750, (0.6392156862745098, 0.45098039215686275, 0.3176470588235294)),\
+          (1.000, (0.23137254901960785, 0.1607843137254902, 0.11372549019607843)) ) )
+
+cmap_baryon \
+    = LinearSegmentedColormap.from_list( 'baryon',\
+        ( (0.000, (0.0, 0.5764705882352941, 0.5764705882352941)),\
+          (0.500, (1.000, 1.000, 1.000)),\
+          (1.000, (0.9568627450980393, 0.47843137254901963, 0.000)) ) )
+
+cmap_strange \
+    = LinearSegmentedColormap.from_list( 'strange',\
+        ( (0.000, (0.5019607843137255, 0.0, 1.0)),\
+          (0.500, (1.000, 1.000, 1.000)),\
+          (1.000, (0.0, 0.5019607843137255, 0.000)) ) )
+
+cmap_electric \
+    = LinearSegmentedColormap.from_list( 'electric',\
+        ( (0.000, (0.0, 0.0, 1.0)),\
+          (0.500, (1.000, 1.000, 1.000)),\
+          (1.000, (1.0, 0.0, 0.000)) ) )
 
 filenameStem = sys.argv[1]
 outfilenameStem = sys.argv[2]
@@ -12,7 +38,19 @@ outsuffix = sys.argv[4]
 numberOfFrames = int(sys.argv[5])
 plotLabel = sys.argv[6]
 colToPlot = int(sys.argv[7])
+mode = sys.argv[8]
+colormap = {"energy_density": cmap_energy, \
+            "baryon_density": cmap_baryon, \
+            "strange_density": cmap_strange, \
+            "electric_density": cmap_electric, \
+            "temperature": plt.cm.inferno, \
+            "baryon_chemical_potential": cmap_baryon, \
+            "strange_chemical_potential": cmap_strange, \
+            "electric_chemical_potential": cmap_electric}[mode]
 
+fixed_maximum = True
+minimum = 0.0
+maximum = 0.0
 
 for i in range(1, numberOfFrames+1):
     outfilename = outfilenameStem + f'{i:03}' + outsuffix
@@ -25,12 +63,26 @@ for i in range(1, numberOfFrames+1):
     data = data[:,1:]  # set tau and then discard that column
     data = data[np.where((np.abs(data[:,0])<15.0) & (np.abs(data[:,1])<15.0))]
     [x, y, f] = data.T
+    
+    if i==1 or not fixed_maximum:
+        maximum = np.amax(np.abs(f))
+
+    minimum = {"energy_density": 0.0, \
+               "baryon_density": -maximum, \
+               "strange_density": -maximum, \
+               "electric_density": -maximum, \
+               "temperature": 0.0, \
+               "baryon_chemical_potential": -maximum, \
+               "strange_chemical_potential": -maximum, \
+               "electric_chemical_potential": -maximum}[mode]
 
     extent = np.min(x), np.max(x), np.min(y), np.max(y)
     fig, ax = plt.subplots( nrows=1, ncols=1 )
 
     length = int(np.sqrt(f.size))
-    psm = plt.imshow(f.reshape(length, length), cmap=plt.cm.inferno, interpolation='bicubic', extent=extent)
+    #psm = plt.imshow(f.reshape(length, length), cmap=plt.cm.inferno, interpolation='bicubic', extent=extent)
+    psm = plt.imshow(f.reshape(length, length), cmap=colormap, vmin=minimum, vmax=maximum,\
+                     interpolation='bicubic', extent=extent)
 
     plt.text(0.075, 0.925, r'$\tau = %(t)5.2f$ fm$/c$'%{'t': tau}, \
             {'color': 'white', 'fontsize': 12}, transform=ax.transAxes,
