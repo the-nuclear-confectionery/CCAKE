5cdc3d46cd5e67848f5faf69e9a84b5b59b3ac21
Christopher Plumberg <astrophysicist87@gmail.com>
2022-01-22 19:12:18 -0600

    Re-run just the energy profile with a different colorscale

--- OVERHAUL/generate_profile_evolution.sh
+++ OVERHAUL/generate_profile_evolution.sh
@@ -14,6 +14,8 @@ nFiles=$(\ls -1 ${resultsDirectory}/${physicalProfileStem}*${insuffix} | wc -l)
 # use the minimum of these two quantities
 nFrames=$((nFiles<maxFrames ? nFiles : maxFrames))
 
+
+'''
 python3 plot_profile.py \
 	${resultsDirectory}/${physicalProfileStem} \
 	${resultsDirectory}/"TProfile" \
@@ -30,10 +32,14 @@ python3 plot_profile.py \
 	${resultsDirectory}/${physicalProfileStem} \
 	${resultsDirectory}/"muQProfile" \
 	${insuffix} ${outsuffix} $nFrames '$\mu_Q$ (MeV)' 6 electric_chemical_potential
+  '''
+
 python3 plot_profile.py \
 	${resultsDirectory}/${physicalProfileStem} \
 	${resultsDirectory}/"eProfile" \
 	${insuffix} ${outsuffix} $nFrames '$e$ (MeV/fm$^3$)' 7 energy_density
+
+'''
 python3 plot_profile.py \
 	${resultsDirectory}/${physicalProfileStem} \
 	${resultsDirectory}/"rhoBProfile" \
@@ -46,20 +52,23 @@ python3 plot_profile.py \
 	${resultsDirectory}/${physicalProfileStem} \
 	${resultsDirectory}/"rhoQProfile" \
 	${insuffix} ${outsuffix} $nFrames '$\rho_Q$ (fm$^{-3}$)' 10 electric_density
-  
 
-mkdir -p $moviesDirectory
-rm -rf ${moviesDirectory}/eProfile.mp4 ${moviesDirectory}/rhoBProfile.mp4 \
-       ${moviesDirectory}/rhoSProfile.mp4  ${moviesDirectory}/rhoQProfile.mp4 \
-       ${moviesDirectory}/TProfile.mp4 ${moviesDirectory}/muBProfile.mp4 \
-       ${moviesDirectory}/muSProfile.mp4  ${moviesDirectory}/muQProfile.mp4
+'''
+
+#mkdir -p $moviesDirectory
+#rm -rf ${moviesDirectory}/eProfile.mp4 ${moviesDirectory}/rhoBProfile.mp4 \
+#       ${moviesDirectory}/rhoSProfile.mp4  ${moviesDirectory}/rhoQProfile.mp4 \
+#       ${moviesDirectory}/TProfile.mp4 ${moviesDirectory}/muBProfile.mp4 \
+#       ${moviesDirectory}/muSProfile.mp4  ${moviesDirectory}/muQProfile.mp4
+
+rm -f ${moviesDirectory}/eProfile.mp4
        
-pngs2mp4 $framesPerSecond ${resultsDirectory}/"TProfile"%03d${outsuffix} ${moviesDirectory}/eProfile.mp4
-pngs2mp4 $framesPerSecond ${resultsDirectory}/"muBProfile"%03d${outsuffix} ${moviesDirectory}/muBProfile.mp4
-pngs2mp4 $framesPerSecond ${resultsDirectory}/"muSProfile"%03d${outsuffix} ${moviesDirectory}/muSProfile.mp4
-pngs2mp4 $framesPerSecond ${resultsDirectory}/"muQProfile"%03d${outsuffix} ${moviesDirectory}/muQProfile.mp4
+#pngs2mp4 $framesPerSecond ${resultsDirectory}/"TProfile"%03d${outsuffix} ${moviesDirectory}/TProfile.mp4
+#pngs2mp4 $framesPerSecond ${resultsDirectory}/"muBProfile"%03d${outsuffix} ${moviesDirectory}/muBProfile.mp4
+#pngs2mp4 $framesPerSecond ${resultsDirectory}/"muSProfile"%03d${outsuffix} ${moviesDirectory}/muSProfile.mp4
+#pngs2mp4 $framesPerSecond ${resultsDirectory}/"muQProfile"%03d${outsuffix} ${moviesDirectory}/muQProfile.mp4
 pngs2mp4 $framesPerSecond ${resultsDirectory}/"eProfile"%03d${outsuffix} ${moviesDirectory}/eProfile.mp4
-pngs2mp4 $framesPerSecond ${resultsDirectory}/"rhoBProfile"%03d${outsuffix} ${moviesDirectory}/rhoBProfile.mp4
-pngs2mp4 $framesPerSecond ${resultsDirectory}/"rhoSProfile"%03d${outsuffix} ${moviesDirectory}/rhoSProfile.mp4
-pngs2mp4 $framesPerSecond ${resultsDirectory}/"rhoQProfile"%03d${outsuffix} ${moviesDirectory}/rhoQProfile.mp4
+#pngs2mp4 $framesPerSecond ${resultsDirectory}/"rhoBProfile"%03d${outsuffix} ${moviesDirectory}/rhoBProfile.mp4
+#pngs2mp4 $framesPerSecond ${resultsDirectory}/"rhoSProfile"%03d${outsuffix} ${moviesDirectory}/rhoSProfile.mp4
+#pngs2mp4 $framesPerSecond ${resultsDirectory}/"rhoQProfile"%03d${outsuffix} ${moviesDirectory}/rhoQProfile.mp4
 
