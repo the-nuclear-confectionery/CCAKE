ea963244d8d777bbbecd7e45a1f3ab4535fa6c63
Christopher Plumberg <astrophysicist87@gmail.com>
2022-01-26 12:55:37 -0600

    More diagnostics

--- OVERHAUL/eos.cpp
+++ OVERHAUL/eos.cpp
@@ -137,11 +137,11 @@ void EquationOfState::evaluate_thermodynamics(bool use_conformal)
     dtds    = thermodynamics[15];
     dt2     = thermodynamics[16];
 
-    /*for (int i = 0; i < 4; i++)
+    for (int i = 0; i < 4; i++)
       cout << tbqsPosition[i] << "   ";
     for (int i = 0; i < 17; i++)
       cout << thermodynamics[i] << "   ";
-    cout << endl;*/
+    cout << endl;
   }
   else if ( use_static_C_library )
   {
