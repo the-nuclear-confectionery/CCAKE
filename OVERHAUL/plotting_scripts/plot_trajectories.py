39df2996ffc00161f5f057ea1df3b7b86f322f77
Christopher Plumberg <astrophysicist87@gmail.com>
2022-02-14 12:36:56 -0600

    Update plot_trajectories.py

--- OVERHAUL/plotting_scripts/plot_trajectories.py
+++ OVERHAUL/plotting_scripts/plot_trajectories.py
@@ -26,6 +26,7 @@ def plot_all_particles():
     data = np.stack([np.loadtxt(file, usecols=(5,6,7,8), skiprows=1) \
                      for file in sys.argv[1:]])
 
+    print(data.shape)
 
     data0 = data[0,:,0]
     data = np.swapaxes(data, 0, 1)[np.where(data0 > 400.0)]
