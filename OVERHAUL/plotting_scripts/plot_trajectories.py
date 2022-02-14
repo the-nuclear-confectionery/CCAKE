c5a7580873d0b1b23287bedcf5eda28be2f073a8
Christopher Plumberg <astrophysicist87@gmail.com>
2022-02-14 14:34:44 -0600

    Update plot_trajectories.py

--- OVERHAUL/plotting_scripts/plot_trajectories.py
+++ OVERHAUL/plotting_scripts/plot_trajectories.py
@@ -29,6 +29,8 @@ def plot_one_particle(pid):
 def plot_all_particles():
     data = np.stack([np.loadtxt(file, usecols=(5,6,7,8), skiprows=1) \
                      for file in sys.argv[1:]])
+    
+    chosen_dpi = 200
 
     print(data.shape)
 
@@ -37,32 +39,37 @@ def plot_all_particles():
     data0 = data0[np.where(data0 > 400.0)]
     
     maximum, minimum = np.amax(data0), np.amin(data0)
+    
+    print("minimum =", minimum)
+    print("maximum =", maximum)
 
     print(data.shape)
 
-    plt.figure(figsize=(4,4), dpi=100)
+    plt.figure(figsize=(4,4), dpi=chosen_dpi)
 
     for iParticle, particle in enumerate(data):
         r,g,b,a = chosen_colormap((particle[0,0]-minimum)/(maximum-minimum))
         plt.plot( particle[:,1], particle[:,0], color=(r,g,b), alpha=0.3 )
 
-    plt.savefig('T_vs_muB.png', dpi=100, bbox_inches='tight', pad_inches = 0)
+    plt.savefig('T_vs_muB.png', dpi=chosen_dpi, bbox_inches='tight', pad_inches = 0)
     print('Saved to T_vs_muB.png')
 
-    plt.figure(figsize=(4,4), dpi=100)
+    plt.figure(figsize=(4,4), dpi=chosen_dpi)
 
     for iParticle, particle in enumerate(data):
-        plt.plot( particle[:,2], particle[:,0] )
+        r,g,b,a = chosen_colormap((particle[0,0]-minimum)/(maximum-minimum))
+        plt.plot( particle[:,2], particle[:,0], color=(r,g,b), alpha=0.3 )
 
-    plt.savefig('T_vs_muS.png', dpi=100, bbox_inches='tight', pad_inches = 0)
+    plt.savefig('T_vs_muS.png', dpi=chosen_dpi, bbox_inches='tight', pad_inches = 0)
     print('Saved to T_vs_muS.png')
 
-    plt.figure(figsize=(4,4), dpi=100)
+    plt.figure(figsize=(4,4), dpi=chosen_dpi)
 
     for iParticle, particle in enumerate(data):
-        plt.plot( particle[:,3], particle[:,0] )
+        r,g,b,a = chosen_colormap((particle[0,0]-minimum)/(maximum-minimum))
+        plt.plot( particle[:,3], particle[:,0], color=(r,g,b), alpha=0.3 )
 
-    plt.savefig('T_vs_muQ.png', dpi=100, bbox_inches='tight', pad_inches = 0)
+    plt.savefig('T_vs_muQ.png', dpi=chosen_dpi, bbox_inches='tight', pad_inches = 0)
     print('Saved to T_vs_muQ.png')
 
 
