85c7c5cd53c8dc53d4bb4aa94338fd7ff9a903e9
Christopher Plumberg <astrophysicist87@gmail.com>
2022-02-14 15:47:54 -0600

    Try plotting in different temperature ranges and cut out particles with Freeze == 5

--- OVERHAUL/plotting_scripts/plot_trajectories.py
+++ OVERHAUL/plotting_scripts/plot_trajectories.py
@@ -26,22 +26,26 @@ def plot_one_particle(pid):
 
 
 #########################################################################################
-def plot_all_particles():
-    data = np.stack([np.loadtxt(file, usecols=(5,6,7,8), skiprows=1) \
+def plot_all_particles(Tmin, Tmax):
+    data = np.stack([np.loadtxt(file, usecols=(5,6,7,8,30), skiprows=1) \
                      for file in sys.argv[1:]])
     
     chosen_dpi = 200
 
     print(data.shape)
 
-    data0 = data[0,:,0]
-    data = np.swapaxes(data, 0, 1)[np.where(data0 > 400.0)]
-    data0 = data0[np.where(data0 > 400.0)]
+    data0 = data[0,:,0]       # Temperature values at initial timestep
+    dataFreeze = data[-1,:,4] # Freeze flag column at final timestep
+    particleSelectionCriteria = (data0 <= float(Tmax))&(data0 >= float(Tmin))&(dataFreeze != 5)
+    data = np.swapaxes(data, 0, 1)[np.where(particleSelectionCriteria)]
+    #data0 = data0[np.where(particleSelectionCriteria)]
     
     maximum, minimum = np.amax(data0), np.amin(data0)
     
     print("minimum =", minimum)
     print("maximum =", maximum)
+
+    data0 = data0[np.where(particleSelectionCriteria)]
 
     print(data.shape)
 
@@ -51,8 +55,9 @@ def plot_all_particles():
         r,g,b,a = chosen_colormap((particle[0,0]-minimum)/(maximum-minimum))
         plt.plot( particle[:,1], particle[:,0], color=(r,g,b), alpha=0.3 )
 
-    plt.savefig('T_vs_muB.png', dpi=chosen_dpi, bbox_inches='tight', pad_inches = 0)
-    print('Saved to T_vs_muB.png')
+    plt.savefig('T_'+str(Tmin)+'_to_'+str(Tmax)+'_vs_muB.png', \
+                dpi=chosen_dpi, bbox_inches='tight', pad_inches = 0)
+    print('Saved to T_'+str(Tmin)+'_to_'+str(Tmax)+'_vs_muB.png')
 
     plt.figure(figsize=(4,4), dpi=chosen_dpi)
 
@@ -60,8 +65,9 @@ def plot_all_particles():
         r,g,b,a = chosen_colormap((particle[0,0]-minimum)/(maximum-minimum))
         plt.plot( particle[:,2], particle[:,0], color=(r,g,b), alpha=0.3 )
 
-    plt.savefig('T_vs_muS.png', dpi=chosen_dpi, bbox_inches='tight', pad_inches = 0)
-    print('Saved to T_vs_muS.png')
+    plt.savefig('T_'+str(Tmin)+'_to_'+str(Tmax)+'_vs_muS.png', \
+                dpi=chosen_dpi, bbox_inches='tight', pad_inches = 0)
+    print('Saved to T_'+str(Tmin)+'_to_'+str(Tmax)+'_vs_muS.png')
 
     plt.figure(figsize=(4,4), dpi=chosen_dpi)
 
@@ -69,12 +75,18 @@ def plot_all_particles():
         r,g,b,a = chosen_colormap((particle[0,0]-minimum)/(maximum-minimum))
         plt.plot( particle[:,3], particle[:,0], color=(r,g,b), alpha=0.3 )
 
-    plt.savefig('T_vs_muQ.png', dpi=chosen_dpi, bbox_inches='tight', pad_inches = 0)
-    print('Saved to T_vs_muQ.png')
+    plt.savefig('T_'+str(Tmin)+'_to_'+str(Tmax)+'_vs_muQ.png', \
+                dpi=chosen_dpi, bbox_inches='tight', pad_inches = 0)
+    print('Saved to T_'+str(Tmin)+'_to_'+str(Tmax)+'_vs_muQ.png')
 
 
 #########################################################################################
 if __name__== "__main__":
-    plot_all_particles()
+    plot_all_particles(600,700)
+    plot_all_particles(500,600)
+    plot_all_particles(400,500)
+    plot_all_particles(300,400)
+    plot_all_particles(200,300)
+    plot_all_particles(100,200)
     #plot_one_particle(7108)
 
