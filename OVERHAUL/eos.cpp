b2bdb5c56ee7f8651ee30ed416ee603bb5a31538
Christopher Plumberg <astrophysicist87@gmail.com>
2022-04-04 16:00:40 -0500

    Diagnostics

--- OVERHAUL/eos.cpp
+++ OVERHAUL/eos.cpp
@@ -88,7 +88,7 @@ bool EquationOfState::point_not_in_range(
 void EquationOfState::tbqs( double setT, double setmuB, double setmuQ,
                             double setmuS, pEoS_base peos )
 {
-cout << "here: " << peos->name << endl;
+cout << __PRETTY_FUNCTION__ << ": " << peos->name << endl;
   bool point_is_in_range = !point_not_in_range( setT, setmuB, setmuQ, setmuS, peos );
   if ( point_is_in_range )
   {
