ee12711aada53aa3d02697920d48e363401a8f48
Christopher Plumberg <astrophysicist87@gmail.com>
2022-04-04 11:45:12 -0500

    Typo

--- OVERHAUL/eos_header.h
+++ OVERHAUL/eos_header.h
@@ -6,7 +6,7 @@ static constexpr bool use_delaunay                        = !use_rootfinder;
 static constexpr bool use_static_C_library                = true;
 static constexpr bool accept_nearest_neighbor             = false;
 static constexpr bool discard_unsolvable_charge_densities = false;
-//static constexpr bool use_conformal_as_fallback           = true;
+static constexpr bool use_conformal_as_fallback           = true;
 static constexpr bool use_nonconformal_extension          = true;
 
 static constexpr int VERBOSE = 0;
