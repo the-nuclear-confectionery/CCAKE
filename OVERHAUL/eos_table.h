2ff2aeef701d55bb123dc7c2031d8ac022a6a262
Christopher Plumberg <astrophysicist87@gmail.com>
2022-04-04 13:15:32 -0500

    Must re-define virtual methods!

--- OVERHAUL/eos_table.h
+++ OVERHAUL/eos_table.h
@@ -1,5 +1,3 @@
-//#pragma once
-
 #ifndef EOS_TABLE_H
 #define EOS_TABLE_H
 
@@ -27,10 +25,6 @@ private:
 
   void get_eBSQ_densities_from_interpolator( double point[], double densities[] );
   void get_sBSQ_densities_from_interpolator( double point[], double densities[] );
-  void get_eBSQ( double point[], double results[] );
-  void get_sBSQ( double point[], double results[] );
-  void get_full_thermo( double point[], double results[] );
-
 
 public:
   // default constructor/destructor
@@ -39,6 +33,10 @@ public:
 
   // constructor from files containing tables
   EoS_table( string quantityFile, string derivFile );
+
+  void get_eBSQ( double point[], double results[] );
+  void get_sBSQ( double point[], double results[] );
+  void get_full_thermo( double point[], double results[] );
 };
 
 #endif
