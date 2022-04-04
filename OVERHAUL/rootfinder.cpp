d6bb329414ae2023159668f79b803d8284eea8fd
Christopher Plumberg <astrophysicist87@gmail.com>
2022-04-04 18:28:02 -0500

    Update rootfinder.cpp

--- OVERHAUL/rootfinder.cpp
+++ OVERHAUL/rootfinder.cpp
@@ -188,9 +188,9 @@ bool Rootfinder::rootfinder4D(double e_or_s_Given, int e_or_s_mode,
       previous_solver_step[iPrev] = gsl_vector_get(solver->x, iPrev);
 
     ++iter;
-//cout << "Status before(1): " << status << "   " << GSL_CONTINUE << endl;
+cout << "Status before(1): " << status << "   " << GSL_CONTINUE << endl;
     status = gsl_multiroot_fsolver_iterate(solver);
-//cout << "Status after(1): " << status << "   " << GSL_CONTINUE << endl;
+cout << "Status after(1): " << status << "   " << GSL_CONTINUE << endl;
 
     if(VERBOSE > 5 && status)
     {
@@ -281,9 +281,9 @@ bool Rootfinder::rootfinder4D(double e_or_s_Given, int e_or_s_mode,
            << gsl_vector_get(solver->f, 2) << ","
            << gsl_vector_get(solver->f, 3) << ")" << std::endl;
 
-//cout << "Status before(2): " << status << "   " << GSL_CONTINUE << endl;
+cout << "Status before(2): " << status << "   " << GSL_CONTINUE << endl;
     status = gsl_multiroot_test_residual(solver->f, error);
-//cout << "Status after(2): " << status << "   " << GSL_CONTINUE << endl;
+cout << "Status after(2): " << status << "   " << GSL_CONTINUE << endl;
 
   } while (status == GSL_CONTINUE && iter < steps);
 
@@ -341,7 +341,7 @@ bool Rootfinder::find_eBSQ_root( double ein, double Bin, double Sin, double Qin,
                           std::function<void(double[], double[])> function_to_evaluate,
                            vector<double> & tbqs_minima,
                            vector<double> & tbqs_maxima,
-                          vector<double> & updated_tbqs )
+                           vector<double> & updated_tbqs )
 {
     int number_of_attempts = 1;
     minT   = tbqs_minima[0]; maxT   = tbqs_maxima[0];
