a28898a1b6a30d83806cf4405787b170e29bb7be
Christopher Plumberg <astrophysicist87@gmail.com>
2022-04-05 14:01:34 -0500

    See if this works

--- OVERHAUL/eos_nonconformal_extension.h
+++ OVERHAUL/eos_nonconformal_extension.h
@@ -39,7 +39,9 @@ class EoS_nonconformal_extension: public EoS_base
       for (int i = 0; i < 4; i++) point_save[i] = point[i];
 
       // project point to boundary of reference_EoS (resets point)
-      eos_extension::project_to_boundary( point, minima, maxima );
+      eos_extension::project_to_boundary( point,
+                      p_reference_EoS->tbqs_minima_no_ext.data(),
+                      p_reference_EoS->tbqs_maxima_no_ext.data() );
 
       // set reference thermodynamics on projected point (on boundary)
       p_reference_EoS->get_sBSQ( point, results );
