d6896d4c5c3a53133aa9f8158ce744c36232bace
Christopher Plumberg <astrophysicist87@gmail.com>
2022-04-05 19:35:03 -0500

    Update eos_initialization.cpp

--- OVERHAUL/eos_initialization.cpp
+++ OVERHAUL/eos_initialization.cpp
@@ -42,7 +42,7 @@ void EquationOfState::init()
     for (double muS0 = -1000.0; muS0 <= 1000.01; muS0 += 250.0)
     for (double muQ0 = -1000.0; muQ0 <= 1000.01; muQ0 += 250.0)
     {
-      std::vector<double> v = get_thermodynamics( {T0, muB0, muQ0, muS0}, "conformal" );
+      std::vector<double> & v = get_thermodynamics( {T0, muB0, muQ0, muS0}, "conformal" );
       std::cout << T0 << "   " << muB0 << "   " << muQ0 << "   " << muS0 << "   " << v[0] << std::endl;
     }
     exit(11);
