495051fff39ac84f4f0034eae19321ff372e6842
Christopher Plumberg <astrophysicist87@gmail.com>
2022-04-07 16:06:46 -0500

    Fixed bug swapping S and Q in calculation of cs2

--- OVERHAUL/eos_extension.cpp
+++ OVERHAUL/eos_extension.cpp
@@ -22,21 +22,6 @@ namespace eos_extension
 
     double T0 = point[0], muB0 = point[1], muQ0 = point[2], muS0 = point[3];
 
-//    // random number check
-//    constexpr bool do_check = true;
-//    if (do_check)
-//    {
-//      T0 = 4633.4170691232885; muB0 = 6807.609126571999;
-//      muQ0 = 110.31935549473928; muS0 = 8676.461504695537;
-//      double thermodynamics0[] = {4873.084795168246, 4667.392176530491,
-//                        9222.825349747662, 8451.68380691301, 8046.726811312314,
-//                        684.8321682934966, 6976.250846056097, 3226.740793471554,
-//                        5470.034816047701, 7258.491777388856, 9159.660100769841,
-//                        5173.041982177994, 3932.206908293947, 6029.5289010392735,
-//                        8790.68377403672, 8668.268493577376, 4257.053638971822};
-//      thermodynamics = thermodynamics0;
-//    }
-
     double p0     = thermodynamics[0];
     double s0     = thermodynamics[1];
     double rhoB0  = thermodynamics[2];
@@ -293,8 +278,8 @@ std::cout << "------------------------------------------------------------------
 
     double C1T = results[1];
     double C1B = results[2];
-    double C1Q = results[3];
-    double C1S = results[4];
+    double C1S = results[3];
+    double C1Q = results[4];
 
     double C2B2 = results[7];
     double C2Q2 = results[8];
