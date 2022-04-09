c25deca0849a0f6f8f2ded35344638cec8cb9e88
Christopher Plumberg <astrophysicist87@gmail.com>
2022-04-09 15:01:52 -0500

    Update eos_initialization.cpp

--- OVERHAUL/eos_initialization.cpp
+++ OVERHAUL/eos_initialization.cpp
@@ -122,14 +122,17 @@ void EquationOfState::init(string quantityFile, string derivFile)
 
     // set muB scale using (Tmax,muBmax,0,0)
     tbqs( Tmax, muBmax, 0.0, 0.0, p_default_EoS );
+    cout << pTmax << "   " << pVal << "   " << c << "   " << muBmax << endl;
     double muB0 = pow(c,0.25) * muBmax / sqrt( sqrt(pTmax) - sqrt(pVal) );
 
     // set muQ scale using (Tmax,0,muQmax,0)
     tbqs( Tmax, 0.0, muQmax, 0.0, p_default_EoS );
+    cout << pTmax << "   " << pVal << "   " << c << "   " << muQmax << endl;
     double muQ0 = pow(c,0.25) * muQmax / sqrt( sqrt(pTmax) - sqrt(pVal) );
 
     // set muS scale using (Tmax,0,0,muSmax)
     tbqs( Tmax, 0.0, 0.0, muSmax, p_default_EoS );
+    cout << pTmax << "   " << pVal << "   " << c << "   " << muSmax << endl;
     double muS0 = pow(c,0.25) * muSmax / sqrt( sqrt(pTmax) - sqrt(pVal) );
 
     // set minima and maxima for rootfinder (can be arbitrarily large)
