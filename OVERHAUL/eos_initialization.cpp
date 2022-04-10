3aced14d3e5401f563b3750c5ff725f57d1dcc61
Christopher Plumberg <astrophysicist87@gmail.com>
2022-04-09 21:06:08 -0500

    Update eos_initialization.cpp

--- OVERHAUL/eos_initialization.cpp
+++ OVERHAUL/eos_initialization.cpp
@@ -220,14 +220,15 @@ void EquationOfState::run_closure_test()
 
   //==========================================================================
   std::cout << "Check non-conformal extension of table EoS:" << std::endl;
+  std::cout << "Made it here" << std::endl;
   double e_In, rhoB_In, rhoS_In, rhoQ_In;
   for (double T0 =  5000.0; T0 <= 5000.01; T0 += 500.0)
   for (double muB0 = 3000.0; muB0 <= 3000.01; muB0 += 500.0)
   for (double muS0 = -2000.0; muS0 <= -2000.01; muS0 += 500.0)
   for (double muQ0 = -2000.0; muQ0 <= -2000.01; muQ0 += 500.0)
   {
-    std::vector<double> point = {T0/hc, muB0/hc, muQ0/hc, muS0/hc};
     std::cout << "GETTING THERMODYNAMICS" << std::endl;
+    std::vector<double> point = {T0/hc, muB0/hc, muQ0/hc, muS0/hc};
     std::vector<double> v = get_thermodynamics( point, "table" );
     std::cout << "Check nc_ext_table: " << T0 << "   " << muB0 << "   "
               << muQ0 << "   "<< muS0 << "   " << v[0] << "   " << v[6] << "   "
