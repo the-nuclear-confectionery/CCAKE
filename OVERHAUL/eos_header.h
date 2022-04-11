6dc87c9f0b09a7b1ebfb06c809b7929889e7bee1
Christopher Plumberg <astrophysicist87@gmail.com>
2022-04-11 12:14:53 -0500

    Try turning off non-conformal extension?

--- OVERHAUL/eos_header.h
+++ OVERHAUL/eos_header.h
@@ -7,7 +7,7 @@ static constexpr bool use_static_C_library                = true;
 //static constexpr bool accept_nearest_neighbor             = false;
 //static constexpr bool discard_unsolvable_charge_densities = false;
 //static constexpr bool use_conformal_as_fallback           = true;
-static constexpr bool use_nonconformal_extension          = true;
+static constexpr bool use_nonconformal_extension          = false;
 
 static constexpr int VERBOSE = 0;
 static constexpr double TINY = 1e-25;
