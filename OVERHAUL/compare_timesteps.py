010f91ef72bf77637000f76d9e514f74b5a2ad52
Christopher Plumberg <astrophysicist87@gmail.com>
2022-04-28 11:54:32 -0500

    Typo

--- OVERHAUL/compare_timesteps.py
+++ OVERHAUL/compare_timesteps.py
@@ -4,18 +4,16 @@ import sys
 # initialize arrays
 print('Initializing arrays...')
 prev_i = 0
-previous_pids = np.genfromtxt(sys.argv[1], usecols=(0), skip_header=1, dtype=int)
 previous_EoSs = np.genfromtxt(sys.argv[1], usecols=(-1), skip_header=1, dtype=str)
                            
 # load all data
 print('Loading all data...')
-all_data = np.array([np.loadtxt(filename, usecols=tuple(range(2,17)), skiprows=1)
+all_data = np.array([np.loadtxt(filename, usecols=tuple(range(1,17)), skiprows=1)
                      for filename in sys.argv[1:]])
 print(all_data.shape)
 
-for curr_i, filename in enumerate(sys.argv[2:]):
+for curr_i, filename in enumerate(sys.argv[2:], 1):
     print('  - differences between', prev_i, 'and', curr_i, ':')
-    current_pids = np.genfromtxt(filename, usecols=(0), skip_header=1, dtype=int)
     current_EoSs = np.genfromtxt(filename, usecols=(-1), skip_header=1, dtype=str)
     
     particles_to_print = np.where( current_EoSs != previous_EoSs )
@@ -25,3 +23,6 @@ for curr_i, filename in enumerate(sys.argv[2:]):
         print(particle_to_print)
         print(all_data[:, particle_to_print])
         print('\n\n\n')
+        
+    prev_i = curr_i
+    previous_EoSs = current_EoSs
