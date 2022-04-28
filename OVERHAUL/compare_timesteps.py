166135ea73443b7691911ff3adba9026faa0e2b7
Christopher Plumberg <astrophysicist87@gmail.com>
2022-04-28 12:07:23 -0500

    Better formatting

--- OVERHAUL/compare_timesteps.py
+++ OVERHAUL/compare_timesteps.py
@@ -8,7 +8,7 @@ previous_EoSs = np.genfromtxt(sys.argv[1], usecols=(-1), skip_header=1, dtype=st
                            
 # load all data
 print('Loading all data...')
-all_data = np.array([np.loadtxt(filename, usecols=tuple(range(1,6)), skiprows=1)
+all_data = np.array([np.loadtxt(filename, usecols=tuple(range(1,14)), skiprows=1)
                      for filename in sys.argv[1:]])
 print(all_data.shape)
 
@@ -20,9 +20,10 @@ for curr_i, filename in enumerate(sys.argv[2:], 1):
     print(particles_to_print)
     
     for particle_to_print in particles_to_print:
+        print('-------------')
         print(particle_to_print)
-        print(all_data[:, particle_to_print])
-        print('-------------\n')
+        print(str(all_data[:, particle_to_print]).replace(' [', '').replace('[', '').replace(']', ''))
+        print('\n')
         
     prev_i = curr_i
     previous_EoSs = current_EoSs
