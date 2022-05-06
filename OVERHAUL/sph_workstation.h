c9107385b681155d9e9736313ad64b48b1391755
Christopher Plumberg <astrophysicist87@gmail.com>
2022-05-06 16:27:50 -0500

    Try guided

--- OVERHAUL/sph_workstation.h
+++ OVERHAUL/sph_workstation.h
@@ -80,7 +80,7 @@ public:
         {
           Stopwatch sw;
           sw.Start();
-          #pragma omp parallel for schedule(dynamic)
+          #pragma omp parallel for schedule(guided)
           for ( auto & p : systemPtr->particles )
             smooth_gradients( p, systemPtr->t );
           sw.Stop();
