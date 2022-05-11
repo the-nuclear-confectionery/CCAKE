ce97730fec7fad376e37d2a026b665cc97b5d5c8
Christopher Plumberg <astrophysicist87@gmail.com>
2022-05-11 11:10:46 -0500

    NOT STABLE

--- OVERHAUL/particle.cpp
+++ OVERHAUL/particle.cpp
@@ -30,24 +30,24 @@ Particle::Particle(vector<double> &fields)
   rhoB_an = fields[3];
   rhoS_an = fields[4];
   rhoQ_an = fields[5];
-  u(0)    = fields[6];
-  u(1)    = fields[7];
+  hydro.u(0)    = fields[6];
+  hydro.u(1)    = fields[7];
   if ( fields.size() > 8 ) // passing in shear tensor initialization as well
   {
     double pi11 = fields[8];
     double pi22 = fields[9];
     double pi12 = fields[10];
     double pi33 = fields[11];
-    shv(0,0)    = 0.0;
-    shv(0,1)    = 0.0;
-    shv(0,2)    = 0.0;
-    shv(1,0)    = 0.0;
-    shv(1,1)    = pi11;
-    shv(1,2)    = pi12;
-    shv(2,0)    = 0.0;
-    shv(2,1)    = pi12;
-    shv(2,2)    = pi22;
-    shv33       = pi33;
+    hydro.shv(0,0)    = 0.0;
+    hydro.shv(0,1)    = 0.0;
+    hydro.shv(0,2)    = 0.0;
+    hydro.shv(1,0)    = 0.0;
+    hydro.shv(1,1)    = pi11;
+    hydro.shv(1,2)    = pi12;
+    hydro.shv(2,0)    = 0.0;
+    hydro.shv(2,1)    = pi12;
+    hydro.shv(2,2)    = pi22;
+    hydro.shv33       = pi33;
   }
   s_an    = 0.0;
 }
@@ -60,9 +60,9 @@ Particle::Particle( const Particle& p )
   rhoB_an = p.rhoB_an;
   rhoS_an = p.rhoS_an;
   rhoQ_an = p.rhoQ_an;
-  u       = p.u;
-  shv     = p.shv;
-  shv33   = p.shv33;
+  hydro.u       = p.hydro.u;
+  hydro.shv     = p.hydro.shv;
+  hydro.shv33   = p.hydro.shv33;
   s_an    = p.s_an;
 }
 
@@ -164,74 +164,18 @@ void Particle::frzcheck( double tin, int &count, int N )
 ////////////////////////////////////////////////////////////////////////////////
 void Particle::reset_pi_tensor(double tin2)
 {
-  gamma    = gamcalc();
-  shv(2,1) = shv(1,2);
-  shv(0,1) = 1./gamma*inner(u,colp1(1,shv));
-  shv(0,2) = 1./gamma*inner(u,colp1(2,shv));
-  shv(1,0) = shv(0,1);
-  shv(2,0) = shv(0,2);
+  hydro.gamma    = gamcalc();
+  hydro.shv(2,1) = hydro.shv(1,2);
+  hydro.shv(0,1) = 1./hydro.gamma*inner(hydro.u,colp1(1,hydro.shv));
+  hydro.shv(0,2) = 1./hydro.gamma*inner(hydro.u,colp1(2,hydro.shv));
+  hydro.shv(1,0) = hydro.shv(0,1);
+  hydro.shv(2,0) = hydro.shv(0,2);
 
-  mini(pimin,shv);
-  uu       = u*u;
-  shv(0,0) = 1./gamma/gamma*con(uu,pimin);
-  shv33    = (shv(0,0)-shv(1,1)-shv(2,2))/tin2;
+  mini(hydro.pimin,hydro.shv);
+  hydro.uu       = hydro.u*hydro.u;
+  hydro.shv(0,0) = 1./hydro.gamma/hydro.gamma*con(hydro.uu,hydro.pimin);
+  hydro.shv33    = (hydro.shv(0,0)-hydro.shv(1,1)-hydro.shv(2,2))/tin2;
 }
-
-
-
-
-////////////////////////////////////////////////////////////////////////////////
-// NEW FUNCTIONS TO CLEAN UP EQUATIONS_OF_MOTION BELOW THIS LINE
-////////////////////////////////////////////////////////////////////////////////
-
-void Particle::set_vartheta(double t) { vartheta = dsigma_dt/sigma - 1.0/t; }
-
-
-////////////////////////////////////////////////////////////////////////////////
-double Particle::get_Theta_force() { return -vartheta/gamma; }
-
-
-////////////////////////////////////////////////////////////////////////////////
-double Particle::get_Pi_force(double t)
-{
-  bigPI = Bulk*sigma/(gamma*t);
-  return vartheta*(zeta/tauRelax + bigPI) - bigPI/(gamma*gamma*stauRelax);
-}
-
-
-////////////////////////////////////////////////////////////////////////////////
-double Particle::get_aleph_force(double t)
-{
-  double aleph_force = -gamma*t*shv33;
-  for (int i = 1; i < 3; i++)
-  for (int j = 1; j < 3; j++)
-    aleph_force += ( (shv(i,0)*u(j) + shv(0,j)*u(i))/gamma
-                      - shv(i,j) - shv(0,0)*u(i)*u(j)/(gamma*gamma) )
-                    * gradU(i,j);
-  return aleph_force;
-}
-
-
-////////////////////////////////////////////////////////////////////////////////
-Vector<double,2> Particle::get_Theta_mass()
-{
-  return v; // just the non-relativistic velocity
-}
-
-
-////////////////////////////////////////////////////////////////////////////////
-Vector<double,2> Particle::get_Pi_mass()
-{
-  return (-(zeta/tauRelax + bigPI)/gamma)*v;
-}
-
-
-////////////////////////////////////////////////////////////////////////////////
-Vector<double,2> Particle::get_aleph_mass()
-{
-  return shv(0,0)*v - colp1(0, shv);
-}
-
 
 
 ////////////////////////////////////////////////////////////////////////////////
@@ -240,23 +184,23 @@ void Particle::set_hydro_info(double t)
   hydro.ID              = ID;
   hydro.t               = t;
 
-  hydro.Agam            = Agam;
-  hydro.Agam2           = Agam2;
-  hydro.shv33           = shv33;
-
-  hydro.gamma           = gamma;
-  hydro.Bulk            = Bulk;
-  hydro.bigPI           = bigPI;
-  hydro.C               = C;
-  hydro.tauRelax        = tauRelax;
-  hydro.stauRelax       = stauRelax;
-  hydro.zeta            = zeta;
-  hydro.setas           = setas; 
-  hydro.Ctot            = Ctot;
-  hydro.Btot            = Btot;
-
-  hydro.sigma           = sigma;
-  hydro.dsigma_dt       = dsigma_dt;
+//  hydro.Agam            = Agam;
+//  hydro.Agam2           = Agam2;
+//  hydro.shv33           = shv33;
+//
+//  hydro.gamma           = gamma;
+//  hydro.Bulk            = Bulk;
+//  hydro.bigPI           = bigPI;
+//  hydro.C               = C;
+//  hydro.tauRelax        = tauRelax;
+//  hydro.stauRelax       = stauRelax;
+//  hydro.zeta            = zeta;
+//  hydro.setas           = setas; 
+//  hydro.Ctot            = Ctot;
+//  hydro.Btot            = Btot;
+//
+//  hydro.sigma           = sigma;
+//  hydro.dsigma_dt       = dsigma_dt;
 
   hydro.dwds            = dwds();
   hydro.dwdB            = dwdB();
@@ -266,35 +210,35 @@ void Particle::set_hydro_info(double t)
   hydro.rhoS            = rhoS();
   hydro.rhoQ            = rhoQ();
 
-  hydro.g2              = g2;
-  hydro.g3              = g3;
-  hydro.gt              = gt;
-  hydro.eta_o_tau       = eta_o_tau;
-  hydro.dwdsT1          = dwdsT1;
-  hydro.sigl            = sigl;
+//  hydro.g2              = g2;
+//  hydro.g3              = g3;
+//  hydro.gt              = gt;
+//  hydro.eta_o_tau       = eta_o_tau;
+//  hydro.dwdsT1          = dwdsT1;
+//  hydro.sigl            = sigl;
 
   hydro.T               = T();
   hydro.w               = w();
   hydro.s               = s();
 
   // vector members
-  hydro.v               = v;
-  hydro.u               = u;
-
-  hydro.gradP           = gradP;
-  hydro.gradBulk        = gradBulk;
-  hydro.divshear        = divshear;
-  hydro.gradshear       = gradshear;
-
-  // matrix members
-  hydro.Imat            = Imat;
-  hydro.gradV           = gradV;
-  hydro.gradU           = gradU;
-  hydro.uu              = uu;
-  hydro.pimin           = pimin;
-  hydro.piu             = piu;
-  hydro.piutot          = piutot;
-  hydro.shv             = shv;
+//  hydro.v               = v;
+//  hydro.u               = u;
+//
+//  hydro.gradP           = gradP;
+//  hydro.gradBulk        = gradBulk;
+//  hydro.divshear        = divshear;
+//  hydro.gradshear       = gradshear;
+//
+//  // matrix members
+//  hydro.Imat            = Imat;
+//  hydro.gradV           = gradV;
+//  hydro.gradU           = gradU;
+//  hydro.uu              = uu;
+//  hydro.pimin           = pimin;
+//  hydro.piu             = piu;
+//  hydro.piutot          = piutot;
+//  hydro.shv             = shv;
 }
 
 
@@ -302,36 +246,36 @@ void Particle::set_hydro_info(double t)
 ////////////////////////////////////////////////////////////////////////////////
 void Particle::update_from_hydro_info()
 {
-  Agam         = hydro.Agam;
-  Agam2        = hydro.Agam2;
-
-  bigPI        = hydro.bigPI;
-  C            = hydro.C;
-  Ctot         = hydro.Ctot;
-  Btot         = hydro.Btot;
-
-  sigma        = hydro.sigma;
-  dsigma_dt    = hydro.dsigma_dt;
-
-  g2           = hydro.g2;
-  g3           = hydro.g3;
-  gt           = hydro.gt;
-  eta_o_tau    = hydro.eta_o_tau;
-  dwdsT1       = hydro.dwdsT1;
-  sigl         = hydro.sigl;
-
-  gradU        = hydro.gradU;
-  uu           = hydro.uu;
-  pimin        = hydro.pimin;
-  piu          = hydro.piu;
-  piutot       = hydro.piutot;
-
-  div_u        = hydro.div_u;
-  bigtheta     = hydro.bigtheta;
-  inside       = hydro.inside;
-
-  dBulk_dt     = hydro.dBulk_dt;
-  detasigma_dt = hydro.detasigma_dt;
-  du_dt        = hydro.du_dt;
-  dshv_dt      = hydro.dshv_dt;
+//  Agam         = hydro.Agam;
+//  Agam2        = hydro.Agam2;
+//
+//  bigPI        = hydro.bigPI;
+//  C            = hydro.C;
+//  Ctot         = hydro.Ctot;
+//  Btot         = hydro.Btot;
+//
+//  sigma        = hydro.sigma;
+//  dsigma_dt    = hydro.dsigma_dt;
+//
+//  g2           = hydro.g2;
+//  g3           = hydro.g3;
+//  gt           = hydro.gt;
+//  eta_o_tau    = hydro.eta_o_tau;
+//  dwdsT1       = hydro.dwdsT1;
+//  sigl         = hydro.sigl;
+//
+//  gradU        = hydro.gradU;
+//  uu           = hydro.uu;
+//  pimin        = hydro.pimin;
+//  piu          = hydro.piu;
+//  piutot       = hydro.piutot;
+//
+//  div_u        = hydro.div_u;
+//  bigtheta     = hydro.bigtheta;
+//  inside       = hydro.inside;
+//
+//  dBulk_dt     = hydro.dBulk_dt;
+//  detasigma_dt = hydro.detasigma_dt;
+//  du_dt        = hydro.du_dt;
+//  dshv_dt      = hydro.dshv_dt;
 }
