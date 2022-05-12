3a3148fa66feb850a1e724665797f4fe8361eabd
Christopher Plumberg <astrophysicist87@gmail.com>
2022-05-12 12:17:55 -0500

    Just add densities file back and add structs without changing anything else

---
+++ OVERHAUL/densities.h
@@ -0,0 +1,16 @@
+#ifndef DENSITIES_H
+#define DENSITIES_H
+
+
+// distinguish CLEARLY between different sets of densities in simulation
+struct densities
+{
+  double e    = 0.0;
+  double s    = 0.0;
+  double rhoB = 0.0;
+  double rhoS = 0.0;
+  double rhoQ = 0.0;
+};
+
+
+#endif
