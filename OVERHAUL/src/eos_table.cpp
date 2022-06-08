ea484fa8436eef82f7035f9b08dd9f6a12b16d42
Christopher Plumberg <astrophysicist87@gmail.com>
2022-06-08 11:10:35 -0500

    Tweak output

--- OVERHAUL/src/eos_table.cpp
+++ OVERHAUL/src/eos_table.cpp
@@ -514,9 +514,10 @@ void EoS_table::get_full_thermo_safe( const double point_in[], double results[] 
   // option to print results
   //if ( VERBOSE > 8 )
   {
-    cout << "Thermo:" << endl;
+    cout << "Thermo:" << " ";
     for (int i = 0; i < 17; i++)
-      cout << results[i] << endl;
+      cout << results[i] << " ";
+    cout << endl;
   }
 
 }  
