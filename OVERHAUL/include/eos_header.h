59ae72bc61cfbbf4074a48ddf2cecca6b4481821
Christopher Plumberg <astrophysicist87@gmail.com>
2022-06-15 22:37:58 -0500

    Turn off static C library and re-run (check to see if it agrees!)

--- OVERHAUL/include/eos_header.h
+++ OVERHAUL/include/eos_header.h
@@ -3,7 +3,7 @@
 
 static constexpr bool use_rootfinder                      = true;
 static constexpr bool use_delaunay                        = !use_rootfinder;
-static constexpr bool use_static_C_library                = true;
+static constexpr bool use_static_C_library                = false;
 //static constexpr bool accept_nearest_neighbor             = false;
 //static constexpr bool discard_unsolvable_charge_densities = false;
 //static constexpr bool use_conformal_as_fallback           = true;
