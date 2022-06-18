7a9c328c86b1a44eb0383750a4b29fa3a3b4149d
Christopher Plumberg <astrophysicist87@gmail.com>
2022-06-18 16:30:04 -0500

    Update animate_EoSs.py

--- OVERHAUL/misc/plotting_scripts/animate_EoSs.py
+++ OVERHAUL/misc/plotting_scripts/animate_EoSs.py
@@ -30,7 +30,7 @@ def animate(i):
     y = np.array(frame['y'])[::100]
     #im = plt.scatter(x, y, c = np.array(frame['e']), s = 0.000004,
     #                 cmap = cm.get_cmap('plasma') )
-    im = plt.plot(x, y, 'bo')
+    im = plt.plot(x, y, 'bo', ms = 0.5)
     plt.xlim([-12, 12])
     plt.ylim([-12, 12])
     
