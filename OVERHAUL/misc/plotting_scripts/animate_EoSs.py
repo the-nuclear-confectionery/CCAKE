cfa4f0f18a87be52f9254715c45acd390aa4573e
Christopher Plumberg <astrophysicist87@gmail.com>
2022-06-18 14:00:51 -0500

    Update animate_EoSs.py

--- OVERHAUL/misc/plotting_scripts/animate_EoSs.py
+++ OVERHAUL/misc/plotting_scripts/animate_EoSs.py
@@ -9,7 +9,7 @@ import sys
 
 f = h5py.File(sys.argv[1], 'r')
 event = f['Event']
-n_timesteps = min([len(event.keys()),10])
+n_timesteps = min([len(event.keys()),1000])
 event_keys = list(event.keys())
 
 
