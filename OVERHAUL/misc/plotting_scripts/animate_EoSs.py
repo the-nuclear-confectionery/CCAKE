bfb054d55711e6ff82fbff8b7da7abd49a823486
Christopher Plumberg <astrophysicist87@gmail.com>
2022-06-18 13:59:59 -0500

    Update animate_EoSs.py

--- OVERHAUL/misc/plotting_scripts/animate_EoSs.py
+++ OVERHAUL/misc/plotting_scripts/animate_EoSs.py
@@ -9,7 +9,7 @@ import sys
 
 f = h5py.File(sys.argv[1], 'r')
 event = f['Event']
-n_timesteps = min([len(event.keys()),5])
+n_timesteps = min([len(event.keys()),10])
 event_keys = list(event.keys())
 
 
@@ -18,7 +18,7 @@ def init():
     x = np.array(frame['x'])
     y = np.array(frame['y'])
     #im = plt.scatter(x, y)
-    im = plt.plot(x, y, 'bo', ms = 0.01)
+    im = plt.plot(x, y, 'bo', ms = 0.025)
     #return im,
     return im
 
@@ -30,7 +30,7 @@ def animate(i):
     y = np.array(frame['y'])
     #im = plt.scatter(x, y, c = np.array(frame['e']), s = 0.000004,
     #                 cmap = cm.get_cmap('plasma') )
-    im = plt.plot(x, y, 'bo', ms = 0.01)
+    im = plt.plot(x, y, 'bo', ms = 0.025)
     
     #plt.imsave(fname='old_animation_frames/frame' + str(i) + '.png', \
     #           arr=image, cmap=chosen_colormap, format='png')
