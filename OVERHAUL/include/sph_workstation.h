e15d3354005e15979293a47e732f2a5df55ca06c
Christopher Plumberg <astrophysicist87@gmail.com>
2022-06-22 12:41:40 -0500

    Forgot to turn off premature exit!

--- OVERHAUL/include/sph_workstation.h
+++ OVERHAUL/include/sph_workstation.h
@@ -205,8 +205,8 @@ public:
     formatted_output::update("finished timestep in "
                               + to_string(sw.printTime()) + " s");
 
-    if ( systemPtr->number_of_elapsed_timesteps > 1 )
-      exit(-1);
+    //if ( systemPtr->number_of_elapsed_timesteps > 1 )
+    //  exit(-1);
 
     return;
   }
