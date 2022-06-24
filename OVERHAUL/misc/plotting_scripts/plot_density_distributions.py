453966bb8deb98d79d5ce969ae1efb798aab7352
Christopher Plumberg <astrophysicist87@gmail.com>
2022-06-24 16:22:55 -0500

    Update plot_density_distributions.py

--- OVERHAUL/misc/plotting_scripts/plot_density_distributions.py
+++ OVERHAUL/misc/plotting_scripts/plot_density_distributions.py
@@ -59,7 +59,7 @@ def plot_density_distribution_vs_time():
     print('Plotting...')
     plt.figure(figsize=(width, height), dpi=chosen_dpi)
 
-    plt.pcolormesh(xedges, yedges, H, cmap='inferno', shading='gouraud')
+    plt.pcolormesh(xedges, yedges, H, cmap='inferno')
     
     outfilename = 'e_vs_tau.png'
     plt.savefig(outfilename, dpi=chosen_dpi, bbox_inches='tight', pad_inches = 0)
