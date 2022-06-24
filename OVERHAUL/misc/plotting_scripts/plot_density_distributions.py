e97136c213c3d0e706afd3f63a124e9d9782e59a
Christopher Plumberg <astrophysicist87@gmail.com>
2022-06-24 16:21:52 -0500

    Update plot_density_distributions.py

--- OVERHAUL/misc/plotting_scripts/plot_density_distributions.py
+++ OVERHAUL/misc/plotting_scripts/plot_density_distributions.py
@@ -43,6 +43,8 @@ def plot_density_distribution_vs_time():
     
     data = data.reshape([data.size//2,2])
     
+    print(np.amin(data[:,0]), np.amax(data[:,0]))
+    
     # freeze-out cutoff [MeV/fm^3]
     eFO = 266.0
     data = data[ data[:,1] >= eFO ]
@@ -57,7 +59,7 @@ def plot_density_distribution_vs_time():
     print('Plotting...')
     plt.figure(figsize=(width, height), dpi=chosen_dpi)
 
-    plt.pcolormesh(xedges, yedges, H, cmap='inferno')
+    plt.pcolormesh(xedges, yedges, H, cmap='inferno', shading='gouraud')
     
     outfilename = 'e_vs_tau.png'
     plt.savefig(outfilename, dpi=chosen_dpi, bbox_inches='tight', pad_inches = 0)
