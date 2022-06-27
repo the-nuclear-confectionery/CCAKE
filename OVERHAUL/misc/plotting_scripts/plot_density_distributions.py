c2f8324991e4476094e739c83f7d298cdf81cede
Christopher Plumberg <astrophysicist87@gmail.com>
2022-06-27 13:39:21 -0500

    Update plot_density_distributions.py

--- OVERHAUL/misc/plotting_scripts/plot_density_distributions.py
+++ OVERHAUL/misc/plotting_scripts/plot_density_distributions.py
@@ -82,11 +82,11 @@ def plot_density_distribution_vs_time(quantity):
     
     # set y-axis ticks in an aesthetic way
     ymin, ymax = np.amin(yedges), np.amax(yedges)
-    minTick = int(np.ceil(np.log10(np.exp(ymin))))
-    maxTick = int(np.ceil(np.log10(np.exp(ymax))))
+    minTick = int(np.ceil(np.log10(ymin)))
+    maxTick = int(np.ceil(np.log10(ymax)))
     print(minTick,maxTick)
     print(list(map(lambda x:10**x, range(minTick,maxTick))))
-    ax.set_yticks(list(map(lambda x:10**x, range(minTick,maxTick))))
+    ax.set_yticks(np.log(list(map(lambda x:10**x, range(minTick,maxTick)))))
     
     #ax.set_yticks(yedges[::50])
     #ax.set_yticklabels(list(map(str,np.exp(yedges)[::50])))
