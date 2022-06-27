1c1ca8ff3915574d5c6cc772d4c48c0f5f0173a6
Christopher Plumberg <astrophysicist87@gmail.com>
2022-06-27 18:29:12 -0500

    Update animate_profile_from_HDF.py

--- OVERHAUL/misc/plotting_scripts/animate_profile_from_HDF.py
+++ OVERHAUL/misc/plotting_scripts/animate_profile_from_HDF.py
@@ -117,8 +117,10 @@ def animate(i):
     extent = xmin, xmax, ymin, ymax
     print(i)
     print(f.reshape(n, n))
+    #im = ax.imshow(f.reshape(n, n)+1e-15, cmap=colormap,\
+    #                norm=LogNorm(vmin=minimum+1e-15, vmax=maximum),\
+    #                interpolation='bicubic', extent=extent)
     im = ax.imshow(f.reshape(n, n)+1e-15, cmap=colormap,\
-                    norm=LogNorm(vmin=minimum+1e-15, vmax=maximum),\
                     interpolation='bicubic', extent=extent)
     #im.set_data(f.reshape(n, n)+1e-15)
 
