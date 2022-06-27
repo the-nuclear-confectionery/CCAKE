7deaea3f2189deb4e250e701553e2f996c7899e3
Christopher Plumberg <astrophysicist87@gmail.com>
2022-06-27 14:15:16 -0500

    Update plot_density_distributions.py

--- OVERHAUL/misc/plotting_scripts/plot_density_distributions.py
+++ OVERHAUL/misc/plotting_scripts/plot_density_distributions.py
@@ -116,9 +116,9 @@ def plot_density_distribution_vs_time(quantity):
 #########################################################################################
 if __name__== "__main__":
     plot_density_distribution_vs_time('e')
-    #plot_density_distribution_vs_time('B')
-    #plot_density_distribution_vs_time('S')
-    #plot_density_distribution_vs_time('Q')
+    plot_density_distribution_vs_time('B')
+    plot_density_distribution_vs_time('S')
+    plot_density_distribution_vs_time('Q')
 
 
 
