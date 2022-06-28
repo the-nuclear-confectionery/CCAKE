f63163b27b10a2da5dc1947c445c90324b51e7a2
Christopher Plumberg <astrophysicist87@gmail.com>
2022-06-28 13:50:38 -0500

    Make script more general so that it can animate other quantities as well

--- OVERHAUL/misc/plotting_scripts/animate_profile_from_HDF.py
+++ OVERHAUL/misc/plotting_scripts/animate_profile_from_HDF.py
@@ -7,27 +7,72 @@ import matplotlib.animation as animation
 from matplotlib.colors import LinearSegmentedColormap, LogNorm, SymLogNorm
 import os, sys, time
 
-fixed_maximum = True
-
+#########################################################################################
 # load arguments
-f = h5.File(sys.argv[1], 'r')
+infilename = sys.argv[1]
+h = float(sys.argv[2])
+quantity = sys.argv[3]
+outfilename = sys.argv[4]
+
+f = h5.File(infilename, 'r')
 event = f['Event']
 event_keys = list(event.keys())
-n_timesteps = min([len(event.keys()),1000])
-
-h = float(sys.argv[2])
-knorm = 10.0/(7.0*np.pi*h*h)
-
-#quantity = sys.argv[3]
+n_timesteps = min([len(event_keys),10])
 
 fig = plt.figure(figsize=(12,12), dpi=125)
 ax = fig.add_subplot(111)
-
 xmin, xmax, ymin, ymax = -15, 15, -15, 15
-
 n = 51
-colormap = plt.cm.inferno
 
+knorm = 10.0/(7.0*np.pi*h*h)
+fixed_maximum = True
+
+#########################################################################################
+colormap                                              \
+    = {"energy_density":              cmap_energy,    \
+       "baryon_density":              cmap_baryon,    \
+       "strange_density":             cmap_strange,   \
+       "electric_density":            cmap_electric,  \
+       "temperature":                 plt.cm.inferno, \
+       "baryon_chemical_potential":   cmap_baryon,    \
+       "strange_chemical_potential":  cmap_strange,   \
+       "electric_chemical_potential": cmap_electric}[quantity]
+
+#########################################################################################
+quantityLabel                                 \
+    = {"energy_density":              'e',    \
+       "baryon_density":              'rhoB', \
+       "strange_density":             'rhoS', \
+       "electric_density":            'rhoQ', \
+       "temperature":                 'T',    \
+       "baryon_chemical_potential":   'muB',  \
+       "strange_chemical_potential":  'muS',  \
+       "electric_chemical_potential": 'muQ'}[quantity]
+    
+#########################################################################################
+plotLabel                                                      \
+    = {"energy_density":              r'$e$ (MeV/fm$^3$)',     \
+       "baryon_density":              r'$\rho_B$ (fm$^{-3}$)', \
+       "strange_density":             r'$\rho_S$ (fm$^{-3}$)', \
+       "electric_density":            r'$\rho_Q$ (fm$^{-3}$)', \
+       "temperature":                 r'$T$ (MeV)',            \
+       "baryon_chemical_potential":   r'$\mu_B$ (MeV)',        \
+       "strange_chemical_potential":  r'$\mu_S$ (MeV)',        \
+       "electric_chemical_potential": r'$\mu_Q$ (MeV)'}[quantity]
+
+#########################################################################################
+use_log_scale                                 \
+    = {"energy_density":              True,   \
+       "baryon_density":              True,   \
+       "strange_density":             True,   \
+       "electric_density":            True,   \
+       "temperature":                 False,  \
+       "baryon_chemical_potential":   False,  \
+       "strange_chemical_potential":  False,  \
+       "electric_chemical_potential": False}[quantity]
+
+
+#########################################################################################
 data = None
 maximum = None
 minimum = None
@@ -92,15 +137,19 @@ def init():
 def animate(i):
     global data, maximum, minimum, im, n
     print('Plotting frame', i, flush=True)
+    tic = time.perf_counter()
+
     ax.clear()
     frame = event[event_keys[i]]
-    #frame = event[event_keys[0]]
     tau = frame.attrs['Time']
     x = np.array(frame['x'])
     y = np.array(frame['y'])
     T = np.array(frame['T'])
     data = np.c_[ x, y, T ]
     
+    toc = time.perf_counter()
+    print(f"Set up frame in {toc - tic:0.4f} seconds", flush=True)
+
     X, Y = np.meshgrid( np.linspace(xmin, xmax, n), np.linspace(ymin, ymax, n) )
     tic = time.perf_counter()
 
@@ -112,53 +161,66 @@ def animate(i):
     
     if i==0 or not fixed_maximum:
         maximum = np.amax(np.abs(f))
-        minimum = np.amin(f[np.abs(f)>0.0])
+        #minimum = np.amin(f[np.abs(f)>0.0])
+        minimum = {"energy_density": np.amin(f[np.abs(f)>0.0]), \
+                   "baryon_density": -maximum, \
+                   "strange_density": -maximum, \
+                   "electric_density": -maximum, \
+                   "temperature": np.amin(f[np.abs(f)>0.0]), \
+                   "baryon_chemical_potential": -maximum, \
+                   "strange_chemical_potential": -maximum, \
+                   "electric_chemical_potential": -maximum}[mode]
     
     
     extent = xmin, xmax, ymin, ymax
     tic = time.perf_counter()
-    im = ax.imshow(f.reshape(n, n)+1e-10, cmap=colormap,\
-                    norm=LogNorm(vmin=minimum+1e-15, vmax=maximum),\
-                    interpolation='bicubic', extent=extent)
+        
+    #im = ax.imshow(f.reshape(n, n)+1e-10, cmap=colormap,\
+    #                norm=LogNorm(vmin=minimum+1e-15, vmax=maximum),\
+    #                interpolation='bicubic', extent=extent)
+    if use_log_scale:
+        if quantity == "energy_density" or quantity == "temperature":
+            im = ax.imshow(f.reshape(n, n)+1e-10, cmap=colormap,\
+                           norm=LogNorm(vmin=minimum+1e-15, vmax=maximum),\
+                           interpolation='bicubic', extent=extent)
+        else:
+            # set range around zero to be linear instead of logarithmic
+            linthresh = 0.01*np.amax(np.abs(f))
+            im = ax.imshow(f.reshape(n, n), cmap=colormap,\
+                           norm=SymLogNorm(linthresh, vmin=minimum, vmax=maximum),\
+                           interpolation='bicubic', extent=extent)
+    else:
+        im = ax.imshow(f.reshape(n, n), cmap=colormap,\
+                       vmin=minimum, vmax=maximum,\
+                       interpolation='bicubic', extent=extent)
+    
     toc = time.perf_counter()
     print(f"Generated frame in {toc - tic:0.4f} seconds")
 
-
     plt.xlim([xmin, xmax])
     plt.ylim([ymin, ymax])
-    #plt.text(0.075, 0.925, r'$n = %(n)5.2f$ fm$/c$'%{'n': n}, \
-    #        {'color': 'white', 'fontsize': 24}, transform=ax.transAxes,
-    #        horizontalalignment='left', verticalalignment='top')
     plt.text(0.075, 0.925, r'$\tau = %(t)5.2f$ fm$/c$'%{'t': tau}, \
             {'color': 'white', 'fontsize': 24}, transform=ax.transAxes,
             horizontalalignment='left', verticalalignment='top')
-    
-    #if i==0:
-    #    fig.savefig('frame' + str(i) + '.png', format='png')
-        
-    #n += 10
-    
+    ax.set_xlabel(r'$x$ (fm)', fontsize=16)
+    ax.set_ylabel(r'$y$ (fm)', fontsize=16)
+    cbar = fig.colorbar(im, ax=ax)
+    cbar.set_label(plotLabel, fontsize=16)
+
     return [im]
 
 
 
 #########################################################################################
 def main():
-
+    global outfilename
     plt.margins(0, 0)
     
     ani = animation.FuncAnimation(fig, animate, np.arange(n_timesteps), \
                                   init_func=init, blit=True)
-    #ani = animation.FuncAnimation(fig, animate, frames=10)
-
-    out = "T_evo.gif"
-    #out = sys.argv[2]
-    print('Saving to', out)
-    ani.save(out, writer='imagemagick', fps=25)
-
-    out = "T_evo.mp4"
+    
     FFwriter = animation.FFMpegWriter(fps=25, extra_args=['-vcodec', 'libx264'])
-    ani.save(out, writer=FFwriter)
+    ani.save(outfilename, writer=FFwriter)
 
     print('Finished everything.')
 
