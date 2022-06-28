353f99e45aed7ad6ecfc2a0d16bd8e181c981520
Christopher Plumberg <astrophysicist87@gmail.com>
2022-06-27 19:15:35 -0500

    Update animate_profile_from_HDF.py

--- OVERHAUL/misc/plotting_scripts/animate_profile_from_HDF.py
+++ OVERHAUL/misc/plotting_scripts/animate_profile_from_HDF.py
@@ -139,7 +139,7 @@ def main():
     plt.margins(0, 0)
     
     animate(0)
-    exit 1
+    exit
     
     #ani = animation.FuncAnimation(fig, animate, np.arange(n_timesteps), \
     #                              init_func=init, blit=True)
