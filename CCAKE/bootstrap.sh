4c84d1c02c4acc51845e7aad7cf18ebce541d962
Christopher Plumberg <astrophysicist87@gmail.com>
2022-07-13 16:13:07 -0500

    Script to simplify setup and installation

---
+++ CCAKE/bootstrap.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+#----------
+
+ICdir=initial_conditions
+EoSdir=EoS/Houston
+
+#-------------------------------------------------------------------------------
+# set up example ICCING initial conditions
+mkdir -p ${ICdir}
+
+ICCINGexample="https://zenodo.org/record/6829000/files/ICCING_example.dat?download=1"
+wget -O ${ICdir}/ICCING_example.dat ${ICCINGexample}
+
+
+#-------------------------------------------------------------------------------
+# set up lattice-based EoS table (text and HDF format)
+mkdir -p ${EoSdir}
+
+EoSTXTTable="https://zenodo.org/record/6829115/files/thermo.dat?download=1"
+EoSHDFTable="https://zenodo.org/record/6829115/files/thermo.h5?download=1"
+wget -O ${EoSdir}/thermo.dat ${EoSTXTTable}
+wget -O ${EoSdir}/thermo.h5 ${EoSHDFTable}
+
+
+#-------------------------------------------------------------------------------
+# compile with CMake
+mkdir -p build
+( cd build && cmake .. && make && make install )
