fe246400d5fd139bf07529e9eff3a4936a15136e
Christopher Plumberg <astrophysicist87@gmail.com>
2023-01-27 12:59:34 -0800

    Update interface_to_HDF5.h
    Typo in previous commit

--- CCAKE/include/interface_to_HDF5.h
+++ CCAKE/include/interface_to_HDF5.h
@@ -165,7 +165,7 @@ class interface_to_HDF5
         //   label_array[i] = labels[i];
         //
         // dataset.write(label_array, PredType::NATIVE_INT);
-        dataset.write(label.data(), PredType::NATIVE_INT);
+        dataset.write(labels.data(), PredType::NATIVE_INT);
       }
 
 //if (1) exit(-1);
