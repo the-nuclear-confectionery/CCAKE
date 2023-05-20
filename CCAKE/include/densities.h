eebd831808714d24403208ec5f8902921674d020
Willian M. Serenone <willian.matioli@gmail.com>
2023-05-20 13:41:56 -0500

    Add enum and macro helpers for thermodynamics_info and densities structs

--- CCAKE/include/densities.h
+++ CCAKE/include/densities.h
@@ -2,7 +2,16 @@
 #define DENSITIES_H
 
 
-// distinguish CLEARLY between different sets of densities in simulation
+/// \brief struct to hold densities
+/// \details This struct is used to hold the densities carried by an SPH particle.
+
+/// NOTE: Beware these information needs to be passed to the cabana AoSoA. To this
+/// end, we define helper macros at the bottom of this file. If one wants to
+/// add a new member to this struct, one needs to add it to the macros as well.
+/// Also, remmember to pass new datamembers to cabana AoSoA in
+/// `SystemState::allocate_cababa_particles()`
+namespace ccake
+{
 struct densities
 {
   double e    = 0.0;
@@ -12,5 +21,16 @@ struct densities
   double rhoQ = 0.0;
 };
 
+namespace densities_info{
+  enum density_info{
+    e,
+    s,
+    rhoB,
+    rhoS,
+    rhoQ,
+    NUM_DENSITIES
+  };
+  #define DENSITY_INFO double[ccake::densities_info::NUM_DENSITIES]
+}}
 
 #endif
--- CCAKE/include/thermodynamic_info.h
+++ CCAKE/include/thermodynamic_info.h
@@ -1,8 +1,16 @@
 #ifndef THERMODYNAMIC_INFO_H
 #define THERMODYNAMIC_INFO_H
 
+#include "Cabana_AoSoA.hpp"
+namespace ccake{
 
-// thermodynamic quantities (with default initialization)
+/// \brief struct to hold thermodynamic info of the particle.
+
+/// NOTE: Beware these information needs to be passed to the cabana AoSoA. To this
+/// end, we define helper macros at the bottom of this file. If one wants to
+/// add a new member to this struct, one needs to add it to the macros as well.
+/// Also, remmember to pass new datamembers to cabana AoSoA in
+/// `SystemState::allocate_cababa_particles()`
 struct thermodynamic_info
 {
   // (T,mu_i) coordinates depend on which EoS was used!
@@ -15,5 +23,28 @@ struct thermodynamic_info
 
 };
 
+namespace thermo_info{
+  enum thermo_scalar_info{
+    T,
+    muB,
+    muS,
+    muQ,
+    e,
+    s,
+    rhoB,
+    rhoS,
+    rhoQ,
+    p,
+    cs2,
+    w,
+    A,
+    dwds,
+    dwdB,
+    dwdS,
+    dwdQ,
+    NUM_THERMO_INFO
+  };
+#define THERMO_SCALAR_INFO double[ccake::thermo_info::NUM_THERMO_INFO]
+}}
 
 #endif
