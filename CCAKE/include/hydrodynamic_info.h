8f112fd7cd26046cb9bdff9cef50904ae27e3a8e
Willian M. Serenone <willian.matioli@gmail.com>
2023-05-20 11:58:39 -0500

    Template hydro info, documents it and add Cabana helpers

--- CCAKE/include/hydrodynamic_info.h
+++ CCAKE/include/hydrodynamic_info.h
@@ -6,56 +6,68 @@
 #include "matrix.h"
 #include "vector.h"
 
-// hydrodynamic quantities (with default initialization)
+/// \brief Strut to store hydrodynamic information
+/// \tparam D Dimensionality of the problem
+/// \details This struct is used to store hydrodynamic information for each
+/// particle.
+///
+/// Beware these information needs to be passed to the cabana AoSoA. To this
+/// end, we define helper macros at the bottom of this file. If one wants to
+/// add a new member to this struct, one needs to add it to the macros as well.
+/// Also, remmember to pass new datamembers to cabana AoSoA in
+/// `SystemState::allocate_cababa_particles()`
+namespace ccake
+{
+template<unsigned int D>
 struct hydrodynamic_info
 {
   bool print_particle    = false;
 
-  int ID                 = -1;  // for debugging purposes only
-  double t               = 0.0; // current time in hydro simulation
+  int ID                 = -1;  ///< for debugging purposes only
+  double t               = 0.0; ///< current time in hydro simulation
 
-  double Agam            = 0.0;
-  double Agam2           = 0.0;
-  double shv33           = 0.0;
+  double Agam            = 0.0; ///< Agam = A ///TODO: check this
+  double Agam2           = 0.0; ///< Agam2 = -Atot ///TODO: check this
+  double shv33           = 0.0; ///< shear viscosity pi^33 (or pi_33) ///TODO: check this
 
-  double gamma           = 0.0; // Lorentz factor
-  double Bulk            = 0.0; // Bulk Viscosity weight
-  double bigPI           = 0.0; // total bulk viscosity
+  double gamma           = 0.0; ///< Lorentz factor
+  double Bulk            = 0.0; ///< Bulk Viscosity weight ///TODO: what is this?
+  double bigPI           = 0.0; ///< total bulk viscosity
   double C               = 0.0;
-  double tauRelax        = 0.0; // Bulk Relaxation time
-  double stauRelax       = 0.0; // Shear Relxation time
-  double zeta            = 0.0; // bulk coefficient
-  double setas           = 0.0; 
-  double Ctot            = 0.0;
-  double Btot            = 0.0;
+  double tauRelax        = 0.0; ///< Bulk Relaxation time
+  double stauRelax       = 0.0; ///< Shear Relxation time
+  double zeta            = 0.0; ///< bulk coefficient
+  double setas           = 0.0; ///< half Shear coefficient eta ///TODO: check this
+  double Ctot            = 0.0; ///< See Jaki's notes, Eq. (270)
+  double Btot            = 0.0; ///< See Jaki's notes, Eq. (274)
 
-  double sigma           = 0.0; // especific volume
-  double dsigma_dt       = 0.0; // derivative of especific volume
+  double sigma           = 0.0; ///< especific volume
+  double dsigma_dt       = 0.0; ///< derivative of especific volume
 
-  double g2              = 0.0;
-  double g3              = 0.0;
-  double gt              = 0.0;
-  double eta_o_tau       = 0.0;
-  double dwdsT1          = 0.0;
-  double sigl            = 0.0;
+  double g2              = 0.0; ///< gamma^2
+  double g3              = 0.0; ///< gamma^3
+  double gt              = 0.0; ///< gamma*tau
+  double eta_o_tau       = 0.0; ///< shear visc coeff eta/tau/2 \\\TODO: check this
+  double dwdsT1          = 0.0; ///< 1 -  (1/T) dw/ds
+  double sigl            = 0.0; ///< (1/sigma^*) d sigma^*/dt - 1/tau
 
-  double varsigma        = 0.0; // defined to be p + Pi (pressure + bulk)
+  double varsigma        = 0.0; ///< defined to be p + Pi (pressure + bulk)
 
   // vector members
-  Vector<double,2> v;                     // velocity
-  Vector<double,2> u;                     // relativistic velocity
+  Vector<double,D> v;                     ///< velocity
+  Vector<double,D> u;                     ///< relativistic velocity
 
-  Vector<double,2> gradP;                 // Gradient of Pressure
-  Vector<double,2> gradBulk;              // Gradient of Bulk Viscosity
-  Vector<double,2> divshear, gradshear;
+  Vector<double,D> gradP;                 ///< Gradient of Pressure
+  Vector<double,D> gradBulk;              ///< Gradient of Bulk Viscosity
+  Vector<double,D> divshear, gradshear;
 
 
-  // matrix members
-  Matrix<double,2,2> Imat;
-  Matrix<double,2,2> gradV, gradU;        // Gradient of velocity needed for shear
-  Matrix<double,2,2> uu, pimin, piu, piutot;
-  Matrix<double,3,3> shv;
-  Matrix<double,2,2> shv1, shv2, shv3, shv4;
+  // matrix members //TODO:2D only for now. Needs to be generalized to D dimensions
+  Matrix<double,D,D> Imat;
+  Matrix<double,D,D> gradV, gradU;        // Gradient of velocity needed for shear
+  Matrix<double,D,D> uu, pimin, piu, piutot;
+  Matrix<double,D+1,D+1> shv;
+  Matrix<double,D,D> shv1, shv2, shv3, shv4;
 
 
   // quantities (possibly?) set in EoM classes
@@ -67,13 +79,77 @@ struct hydrodynamic_info
   // derivatives
   double dBulk_dt        = 0.0;
 
-  Vector<double, 2> du_dt;
-  Matrix<double, 2, 2> dshv_dt;
-
+  Vector<double, D> du_dt;
+  Matrix<double, D, D> dshv_dt;
 
 };
 
-typedef std::shared_ptr<hydrodynamic_info> pHydrodynamic_info;
+namespace hydro_info
+{
 
+enum hydro_scalar_info
+{
+  t,
+  Agam,
+  Agam2,
+  shv33,
+  gamma,
+  Bulk,
+  bigPI,
+  C,
+  tauRelax,
+  stauRelax,
+  zeta,
+  setas,
+  Ctot,
+  Btot,
+  sigma,
+  dsigma_dt,
+  g2,
+  g3,
+  gt,
+  eta_o_tau,
+  dwdsT1,
+  sigl,
+  varsigma,
+  NUM_HYDRO_SCALAR_INFO
+};
+#define HYDRO_SCALAR_INFO double[ccake::hydro_info::NUM_HYDRO_SCALAR_INFO]
+enum hydro_vector_info
+{
+  v,
+  u,
+  gradP,
+  gradBulk,
+  divshear,
+  gradshear,
+  NUM_HYDRO_VECTOR_INFO
+};
+#define HYDRO_VECTOR_INFO double[ccake::hydro_info::NUM_HYDRO_VECTOR_INFO][3]
+enum hydro_space_matrix_info
+{
+  Imat,
+  gradV,
+  gradU,
+  uu,
+  pimin,
+  piu,
+  piutot,
+  shv1,
+  shv2,
+  shv3,
+  shv4,
+  dshv_dt,
+  NUM_HYDRO_SPACE_MATRIX_INFO
+};
+#define HYDRO_SPACE_MATRIX_INFO double[ccake::hydro_info::NUM_HYDRO_SPACE_MATRIX_INFO][3][3]
+enum hydro_spacetime_matrix_info
+{
+  shv,
+  NUM_HYDRO_SPACETIME_MATRIX_INFO
+};
+#define HYDRO_SPACETIME_MATRIX_INFO double[ccake::hydro_info::NUM_HYDRO_SPACETIME_MATRIX_INFO][4][4]
+
+}}
 
 #endif
