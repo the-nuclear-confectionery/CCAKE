8a2f5a4cae74cf85dc2b49779bfab72a6162df5f
Willian M. Serenone <willian.matioli@gmail.com>
2023-05-20 13:51:02 -0500

    Add templated version of particle.cpp

--- CCAKE/src/particle.cpp
+++ CCAKE/src/particle.cpp
@@ -9,15 +9,24 @@
 #include <string>
 #include <vector>
 
-#include "../include/particle.h"
+#include "particle.h"
+
+using namespace ccake;
+
+//Template instantiations
+template class Particle<1>;
+template class Particle<2>;
+template class Particle<3>;
 
 //==============================================================================
 // Default constructor
-Particle::Particle() { input.s = 0.0; }
+template<unsigned int D>
+Particle<D>::Particle() { input.s = 0.0; }
 
 //==============================================================================
 // Copy constructor
-Particle::Particle( const Particle& p )
+template<unsigned int D>
+Particle<D>::Particle( const Particle<D>& p )
 {
   r             = p.r;
   input.e       = p.input.e;
@@ -31,21 +40,27 @@ Particle::Particle( const Particle& p )
 }
 
 //==============================================================================
-double Particle::gamcalc() { return sqrt( Norm2(hydro.u) + 1.0 ); }
+template<unsigned int D>
+double Particle<D>::gamcalc() { return sqrt( Norm2(hydro.u) + 1.0 ); }
 
 //==============================================================================
-void Particle::reset_pi_tensor(double tin2)
+template<unsigned int D>
+void Particle<D>::reset_pi_tensor(double tin2)
 {
   hydro.gamma    = gamcalc();
-  hydro.shv(2,1) = hydro.shv(1,2);
-  hydro.shv(0,1) = 1./hydro.gamma*inner(hydro.u,colp1(1,hydro.shv));
-  hydro.shv(0,2) = 1./hydro.gamma*inner(hydro.u,colp1(2,hydro.shv));
-  hydro.shv(1,0) = hydro.shv(0,1);
-  hydro.shv(2,0) = hydro.shv(0,2);
+  for( unsigned int i=1; i<D+1; i++ )
+    hydro.shv(0,i) = 1./hydro.gamma*inner(hydro.u,colp1(i,hydro.shv));
+
+  for( unsigned int i=0; i<D+1; i++ )
+    for( unsigned int j=i+1; j<D+1; j++ )
+      hydro.shv(j,i) = hydro.shv(i,j);
 
   mini(hydro.pimin,hydro.shv);
   hydro.uu       = hydro.u*hydro.u;
   hydro.shv(0,0) = 1./hydro.gamma/hydro.gamma*con(hydro.uu,hydro.pimin);
-  hydro.shv33    = (hydro.shv(0,0)-hydro.shv(1,1)-hydro.shv(2,2))/tin2;
+  hydro.shv33    = hydro.shv(0,0);
+  for ( unsigned int i=1; i<D+1; i++ )
+    hydro.shv33 -= hydro.shv(i,i);
+  hydro.shv33   /= tin2;
 }
 
