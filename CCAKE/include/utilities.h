e975bc6519846a3c58bcd6d8b79e73b58a04fb36
Willian M. Serenone <willian.matioli@gmail.com>
2023-06-01 18:39:31 -0500

    Add missing kokkos/cabana definitions

--- CCAKE/include/utilities.h
+++ CCAKE/include/utilities.h
@@ -6,6 +6,26 @@
 #include <sys/sysinfo.h>
 #endif
 
+/// Global dfinitions used in the Cabana and Kokkos libraries
+#include <Cabana_Core.hpp>
+
+#define VECTOR_LENGTH 32 ///< The length of the vector used in the Cabana library
+/// Choose between CPU or GPU execution
+#ifdef __CUDACC__
+using MemorySpace = Kokkos::CudaSpace;
+using ExecutionSpace = Kokkos::Cuda;
+#else
+using MemorySpace = Kokkos::HostSpace;
+using ExecutionSpace = Kokkos::OpenMP;
+#endif
+
+using DeviceType = Kokkos::Device<ExecutionSpace, MemorySpace>;
+
+/// Alias for the particle list
+using ListAlgorithm = Cabana::FullNeighborTag;
+using ListLayout = Cabana::VerletLayoutCSR; ///< There are other options for the list layout. //TODO: Check which one is the best
+using ListType = Cabana::VerletList<MemorySpace, ListAlgorithm, ListLayout>;
+
 class Utilities
 {
 public:
