2511e8c83bcc4efdaab1378f5786585eb1d405e6
Christopher Plumberg <astrophysicist87@gmail.com>
2023-06-05 17:26:19 -0700

    Update input_output.cpp

--- CCAKE/src/input_output.cpp
+++ CCAKE/src/input_output.cpp
@@ -146,7 +146,7 @@ void InputOutput::load_settings_file( string path_to_settings_file )
 
 
   // set particles to print
-  settingsPtr->particles_to_print = vector<int>({});
+  settingsPtr->particles_to_print = vector<int>({0});
 
 
   // if HDF was successfully included
