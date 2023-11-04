7befbe745adb746044025b93e9227500dd938636
Willian M. Serenone <willian.matioli@gmail.com>
2023-11-04 17:46:29 -0500

    Fix kernel normalization issues

--- CCAKE/src/kernel.cpp
+++ CCAKE/src/kernel.cpp
@@ -7,20 +7,21 @@ template class SPHkernel<1>;
 template class SPHkernel<2>;
 template class SPHkernel<3>;
   
-template<> const double SPHkernel<2>::knorm  = 10./(7.*M_PI);
+template<> const double SPHkernel<1>::knorm  = 2./3.; //Used for q < 1 
+template<> const double SPHkernel<1>::knorm2 = 1./6.; //Used for 1 < q < 2
+template<> const double SPHkernel<1>::kgrad  = -1./2.; 
+template<> const double SPHkernel<1>::kgrad2 = 1.; 
+
+template<> const double SPHkernel<2>::knorm  = 10./(7*M_PI);
 template<> const double SPHkernel<2>::knorm2 = 5./(14.*M_PI);
 template<> const double SPHkernel<2>::kgrad  = -15./(14.*M_PI);
-template<> const double SPHkernel<2>::kgrad2 = 10./(7.*M_PI);
+template<> const double SPHkernel<2>::kgrad2 = 15./(7.*M_PI);
 
-template<> const double SPHkernel<3>::knorm  = 1./(4.*M_PI); 
-template<> const double SPHkernel<3>::knorm2 = 1./(16.*M_PI); 
-template<> const double SPHkernel<3>::kgrad  = -3./(14.*M_PI); 
-template<> const double SPHkernel<3>::kgrad2 = 1./(4.*M_PI); 
+template<> const double SPHkernel<3>::knorm  = 1./M_PI; 
+template<> const double SPHkernel<3>::knorm2 = 1./(4*M_PI); 
+template<> const double SPHkernel<3>::kgrad  = -3./(4.*M_PI); 
+template<> const double SPHkernel<3>::kgrad2 = 3./(2.*M_PI); 
 
-template<> const double SPHkernel<1>::knorm  = 1./6.;
-template<> const double SPHkernel<1>::knorm2 = 1./24.;
-template<> const double SPHkernel<1>::kgrad  = -3./24.; 
-template<> const double SPHkernel<1>::kgrad2 = 1./6.; 
 
 
 
@@ -28,8 +29,8 @@ template<unsigned int D>
 KOKKOS_INLINE_FUNCTION
 double SPHkernel<D>::kernel(double distance, double hT )
 { 
-  double norm       = knorm/pow(hT,D);
-  double norm2      = knorm2/pow(hT,D);
+  double norm       = knorm/Kokkos::pow(hT,D);
+  double norm2      = knorm2/Kokkos::pow(hT,D);
   double q = distance/hT;
   if ( q >= 2.0 )
     return 0.0;
@@ -54,8 +55,8 @@ double SPHkernel<D>::distance(const double* r1, const double* r2)
 
 /// @brief Calculates the gradient of the kernel function
 /// @tparam D The dimensionality of the simulation
-/// @param rel_dist The relative distance between two particles
-/// @param r Norm of the relative distance
+/// @param rel_dist An array containing the relative distance between two particles, i.e. ra-rb
+/// @param r Norm of the relative distance array above
 /// @param h SPH smoothing length
 /// @param grad The array to store the gradient of the kernel
 template<unsigned int D>
@@ -79,7 +80,7 @@ void SPHkernel<D>::gradKernel(double const* rel_dist, double r, double h, double
     return;
   }
   for (int idir=0; idir<2; idir++)
-      grad[idir] = norm_grad2*( -3.0+2.25*q )*rel_dist[idir];
+      grad[idir] = norm_grad2*( 1.5*q - 2 )*rel_dist[idir];
   return;
 }  
 
