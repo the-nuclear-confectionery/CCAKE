2e7c1657f9f07bbefe608794bfd446b842d2a2e1
Willian M. Serenone <willian.matioli@gmail.com>
2023-11-05 18:09:10 -0600

    Incorporates unit tests for 2D kernel gradient

--- CCAKE/unit_tests/kernel_test.cpp
+++ CCAKE/unit_tests/kernel_test.cpp
@@ -113,3 +113,81 @@ TEST( KernelTest, Kernel3D_q_2p0 )
   << "Kernel value is "<< ccake::SPHkernel<3>::kernel(dist,h) 
   << " instead of expected "  << 0.;
 }
+
+TEST( GradKernelTest, GradKernel2D_q_0p5 )
+{
+  double pos_a[2] = {.25,.0};
+  double pos_b[2] = {-.25,.0};
+  double rel_pos[2];
+  double const target_val = -75./56./M_PI;
+  double h = 1;
+  double gradK[2];
+  
+  double dist = ccake::SPHkernel<2>::distance(pos_a,pos_b);
+  for (int idir=0; idir<2;++idir) rel_pos[idir] = pos_a[idir] - pos_b[idir];
+  ccake::SPHkernel<2>::gradKernel(rel_pos,dist,h,gradK);
+  
+  EXPECT_LT( fabs(gradK[0] - target_val), TOL) << "gradK[0] = " << gradK[0] <<". Expected " << target_val;
+  EXPECT_LT( fabs(gradK[1]), TOL) << "gradK[1] = " << gradK[1] <<". Expected 0" << endl;
+  
+  double pos_a2[2] = {0.,.25};
+  double pos_b2[2] = {0.,-.25};
+  for (int idir=0; idir<2;++idir) rel_pos[idir] = pos_a2[idir] - pos_b2[idir];
+  dist = ccake::SPHkernel<2>::distance(pos_a2,pos_b2);
+  ccake::SPHkernel<2>::gradKernel(rel_pos,dist,h,gradK);
+  EXPECT_LT( fabs(gradK[0]), TOL) << "gradK[0] = " << gradK[0] <<". Expected 0" << endl;
+  EXPECT_LT( fabs(gradK[1] - target_val), TOL) << "gradK[1] = " << gradK[1] <<". Expected " << target_val;
+  
+}
+
+TEST( GradKernelTest, GradKernel2D_q_1p0 )
+{
+  double pos_a[2] = {.5,.0};
+  double pos_b[2] = {-.5,.0};
+  double rel_pos[2];
+  double const target_val = -15./14./M_PI;
+  double h = 1;
+  double gradK[2];
+  
+  double dist = ccake::SPHkernel<2>::distance(pos_a,pos_b);
+  for (int idir=0; idir<2;++idir) rel_pos[idir] = pos_a[idir] - pos_b[idir];
+  ccake::SPHkernel<2>::gradKernel(rel_pos,dist,h,gradK);
+  
+  EXPECT_LT( fabs(gradK[0] - target_val), TOL) << "gradK[0] = " << gradK[0] <<". Expected " << target_val;
+  EXPECT_LT( fabs(gradK[1]), TOL) << "gradK[1] = " << gradK[1] <<". Expected 0" << endl;
+  
+  double pos_a2[2] = {0.,.5};
+  double pos_b2[2] = {0.,-.5};
+  for (int idir=0; idir<2;++idir) rel_pos[idir] = pos_a2[idir] - pos_b2[idir];
+  dist = ccake::SPHkernel<2>::distance(pos_a2,pos_b2);
+  ccake::SPHkernel<2>::gradKernel(rel_pos,dist,h,gradK);
+  EXPECT_LT( fabs(gradK[0]), TOL) << "gradK[0] = " << gradK[0] <<". Expected 0" << endl;
+  EXPECT_LT( fabs(gradK[1] - target_val), TOL) << "gradK[1] = " << gradK[1] <<". Expected " << target_val;
+  
+}
+
+TEST( GradKernelTest, GradKernel2D_q_1p5 )
+{
+  double pos_a[2] = {.75,.0};
+  double pos_b[2] = {-.75,.0};
+  double rel_pos[2];
+  double const target_val = -15./56./M_PI;
+  double h = 1;
+  double gradK[2];
+  
+  double dist = ccake::SPHkernel<2>::distance(pos_a,pos_b);
+  for (int idir=0; idir<2;++idir) rel_pos[idir] = pos_a[idir] - pos_b[idir];
+  ccake::SPHkernel<2>::gradKernel(rel_pos,dist,h,gradK);
+  
+  EXPECT_LT( fabs(gradK[0] - target_val), TOL) << "gradK[0] = " << gradK[0] <<". Expected " << target_val;
+  EXPECT_LT( fabs(gradK[1]), TOL) << "gradK[1] = " << gradK[1] <<". Expected 0" << endl;
+  
+  double pos_a2[2] = {0.,.75};
+  double pos_b2[2] = {0.,-.75};
+  for (int idir=0; idir<2;++idir) rel_pos[idir] = pos_a2[idir] - pos_b2[idir];
+  dist = ccake::SPHkernel<2>::distance(pos_a2,pos_b2);
+  ccake::SPHkernel<2>::gradKernel(rel_pos,dist,h,gradK);
+  EXPECT_LT( fabs(gradK[0]), TOL) << "gradK[0] = " << gradK[0] <<". Expected 0" << endl;
+  EXPECT_LT( fabs(gradK[1] - target_val), TOL) << "gradK[1] = " << gradK[1] <<". Expected " << target_val;
+  
+}
