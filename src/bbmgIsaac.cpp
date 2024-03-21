766f86afbb90488d886bf54d0cadd3fba6a5cf13
iml1020 <i-long@onu.edu>
2024-03-21 18:15:57 -0500

    Checking if we get some greater initial jet energy

--- src/bbmgIsaac.cpp
+++ src/bbmgIsaac.cpp
@@ -153,11 +153,11 @@ void BBMG::propagate()
       full_sph_field[i].on    = 0;
       full_sph_field[i].line += 0.5 * kappa * pow(tau,z) * pow(full_sph_field[i].rho0, c) * settingsPtr->dt; /* flow(ff[i])*/
       //ff[i].line *= efluc();
-      cout << "Hard Check on all values: " << kappa << " " << tau << " " << full_sph_field[i].rho0 << " " << settingsPtr->dt << endl;
-      cout << "Hard Check on all the bbmg exponenets: " << z << " " << c << endl;
-      cout << "Hard Check on full quantities: " << pow(tau, z) << " " << pow(full_sph_field[i].rho0, c) << endl;
-      cout << "Checking values of line integration AFTER the jets hit FO Temperature: " << full_sph_field[i].line << endl;
-      abort();
+      //cout << "Hard Check on all values: " << kappa << " " << tau << " " << full_sph_field[i].rho0 << " " << settingsPtr->dt << endl;
+      //cout << "Hard Check on all the bbmg exponenets: " << z << " " << c << endl;
+      //cout << "Hard Check on full quantities: " << pow(tau, z) << " " << pow(full_sph_field[i].rho0, c) << endl;
+      //cout << "Checking values of line integration AFTER the jets hit FO Temperature: " << full_sph_field[i].line << endl;
+
       P0g  = Pfg + Cg * full_sph_field[i].line; //* pow(Pfg, 1-a)
       P0q  = Pfq + Cq * full_sph_field[i].line; //* pow(Pfq, 1-a) 
       cout << "P0g: " << P0g << "MeV, P0q: " << P0q << "MeV" << endl;
