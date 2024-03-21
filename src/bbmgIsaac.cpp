4d0b5c434e36a39c7e51064d4739d4c2dbdcfd09
iml1020 <i-long@onu.edu>
2024-03-21 15:19:30 -0500

    Debugging interpolator

--- src/bbmgIsaac.cpp
+++ src/bbmgIsaac.cpp
@@ -136,6 +136,9 @@ void BBMG::propagate()
       
     inter( full_sph_field[i] ); //interpolation of the field
     cout << "Interpolated field temp is: " << full_sph_field[i].T << "\n";
+    abort();
+
+    
     if ( ( full_sph_field[i].on == 1 ) && ( full_sph_field[i].T > Freezeout_Temp ) )
     {
       full_sph_field[i].line += pow(tau, z) * pow(full_sph_field[i].rho, c) * systemPtr->dt; // * flow(ff[i])
@@ -206,10 +209,10 @@ void BBMG::inter( field &f )
       double gridy = settingsPtr->stepy;
       //f.T      += p.T()*constants::hbarc_MeVfm*0.06*0.06*kk; //Here I changed the hardcoded grid size to a read in of the default grid from settings.
       // In all quantities below, I have just changed += to only =, as I feel it is just adding to the temperatures and not decreasing down to the freeze out I need in propagate
-      f.T      += p.T()*constants::hbarc_MeVfm*gridx*gridy*kern/fac; // After correcting with the constants list, almost correct --------- WHY IS THIS += AND NOT JUST =
+      f.T      += p.T()*constants::hbarc_MeVfm*gridx*gridy*kern; // After correcting with the constants list, almost correct --------- WHY IS THIS += AND NOT JUST =
       cout << "Interpolated temp value is " << f.T << "\n";
-      if (f.T > 900)
-      {abort();}
+      /*if (f.T > 900)
+      {abort();}*/
       f.rho    += (p.p()/p.T())*kern;
       f.v[0]   += p.hydro.v(0)*kern;
       //cout << "X velocity is: " << f.v[0] << endl; This is fine for now, not seemingly above 1
@@ -222,7 +225,7 @@ void BBMG::inter( field &f )
       cout << "Value of normalization (?) " << fac << endl;
     }
   }
-
+  //This fac quantity seems to be fools gold, every time I have used it I get either 0's or no values at all
   //double fac = den / area; //may need to move these inside the for loop?? including these returns 0 for all quantities below
   //f.T       *= constants::hbarc_MeVfm;
   //f.T       /= fac;
