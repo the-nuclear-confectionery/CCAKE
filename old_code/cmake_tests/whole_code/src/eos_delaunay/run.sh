5eb0a0de7fe3e66a0890fb090e1b2c91225746c8
Willian M. Serenone <willian.matioli@gmail.com>
2024-04-11 19:52:18 -0300

    Move everytrhing in CCAKE folder to project root.
    Removes "old_code" folder.

--- old_code/cmake_tests/whole_code/src/eos_delaunay/run.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-#SBATCH -A qgp
-#SBATCH -p qgp
-#SBATCH -t 72:00:00
-
-#./interpolate_ebsq ../Thermodynamics_improved_dense/EoS_Taylor_AllMu.dat ../Thermodynamics_staggered/EoS_Taylor_AllMu.dat
-
-./interpolate_ebsq original_grid.dat staggered_grid.dat
