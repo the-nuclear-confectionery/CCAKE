279cf8d536a73b25f1d2a11f35b3bc4b245cf8ee
Kevin Ingles <elkevin111@gmail.com>
2024-07-19 22:32:29 -0400

    Updating files so they will compile with clang
    The minimum necessary version of C++ had to be bumped to C++17 to avoid
    any warning message. Additionally, clang is more opinionated on how and
    when to use `constexpr`. Static memeber variables can be declared with
    `constexpr` keyword, however, if the initialization of that variable is
    from a function that is not `constexpr`, such as `std::pow` (at least
    until C++26) clang will complain.
    
    So these static initializations need to be moved out of line.

--- include/eos_conformal_diagonal.h
+++ include/eos_conformal_diagonal.h
@@ -17,13 +17,13 @@ private:
 
   double c, T0, muB0, muS0, muQ0;
   static constexpr double four_thirds = 4.0/3.0;
-  static constexpr double two_to_two_thirds = pow(2.0, 2.0/3.0);
+  static double two_to_two_thirds;
 
   inline double sgn(double val) { return (0.0 < val) - (val < 0.0); }
 
 public:
   // default constructor/destructor
-  EoS_conformal_diagonal(){std::cout << "In default constructor!\n"; abort();}
+  EoS_conformal_diagonal() = default;
   virtual ~EoS_conformal_diagonal(){}
 
   EoS_conformal_diagonal( const double c_in,
@@ -205,5 +205,6 @@ public:
   }
   
 };
+
 
 #endif
