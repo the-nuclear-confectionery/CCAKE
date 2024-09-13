d4ff522f173f5ac445d9aba5c253923d516467ae
iml1020 <i-long@onu.edu>
2024-09-13 17:02:02 -0500

    Update bbmgIsaac.cpp
    Debugging segmentation fault error

--- src/bbmgIsaac.cpp
+++ src/bbmgIsaac.cpp
@@ -151,7 +151,8 @@ void BBMG::propagate()
         auto new_end = std::remove_if(jetInfo.begin(), jetInfo.end(),
             [this, &condition](auto& jetPropagation) {
                 if (condition(jetPropagation)) {
-                    jetFreezeOut.push_back(std::move(jetPropagation));
+                    jetFreezeOut.push_back(jetPropagation);
+                    //jetFreezeOut.push_back(std::move(jetPropagation));
                     return true; // Mark element for removal
                 }
                 return false; // Keep element
