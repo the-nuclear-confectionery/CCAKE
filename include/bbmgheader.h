bacfc94f04077dde3b97a7ff57c61f6e93730913
iml1020 <i-long@onu.edu>
2025-01-24 15:59:59 -0600

    New initial function in bbmg
    Created a random sampling function to initialize the same amount of jets for each event

--- include/bbmgheader.h
+++ include/bbmgheader.h
@@ -51,7 +51,7 @@ private:
     double Pfg, Pfq;
 
 
-    const static int phimax = 14;
+    const static int phimax = 7;
     double phi[phimax];
     //double Rjetq[phimax], Rjetg[phimax];
     //vector<double> rr;
