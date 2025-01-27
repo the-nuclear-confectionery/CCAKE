9ea592f6fceb28507fd74e1271f83f872a92c118
kevinpala <kevin.p.pala@gmail.com>
2025-01-27 15:07:49 -0600

    Changed trento2ccake to read UIUC trento version

--- utilities/trento2ccake.py
+++ utilities/trento2ccake.py
@@ -13,36 +13,40 @@ def check_input():
         sys.exit(1)
 
 def write_header(stepx, stepy, stepEta, xmin, ymin, etamin):
-    #TODO: parse from a trento input file
+    # Open the output file and write the header
     f = open(sys.argv[4], "w")
     f.write(f"#0 {stepx} {stepy} {stepEta} 0 {xmin} {ymin} {etamin}\n")
     return f
 
 def main():
     check_input()
-    f = open(sys.argv[1], "r")
-    lines = f.readlines()
-    f.close()
+    
+    # Read the input file and skip the first line
+    with open(sys.argv[1], "r") as infile:
+        lines = infile.readlines()[1:]
 
-    stepx  = float(sys.argv[2])
-    stepy  = float(sys.argv[2])
-    stepEta  = .1
+    stepx = float(sys.argv[2])
+    stepy = float(sys.argv[2])
+    stepEta = 0.1
     xmin = -float(sys.argv[3])
     ymin = -float(sys.argv[3])
-    etamin = -.5
+    etamin = -0.5
 
-    f = write_header(stepx, stepy, stepEta, xmin, ymin, etamin)
+    # Write the header to the output file
+    outfile = write_header(stepx, stepy, stepEta, xmin, ymin, etamin)
 
-    for ix, line in enumerate(lines):
-        if line[0] == "#":
+    for line in lines:
+        # Skip comment lines
+        if line.startswith("#"):
             continue
-        else:
-            x = xmin + ix*stepx
-            data = line.split()
-            for iy, eps in enumerate(data):
-                y = ymin + iy*stepy
-                f.write(f"{x} {y} 0 {eps} 0 0 0 0 0 0 0 0 0 0 0 0 0\n")
-    f.close()
+
+        # Parse x, y, and eps from the line
+        x, y, eps = map(float, line.split())
+
+        # Write the formatted output
+        outfile.write(f"{x} {y} 0 {eps} 0 0 0 0 0 0 0 0 0 0 0 0 0\n")
+
+    outfile.close()
 
 if __name__ == "__main__":
     main()
