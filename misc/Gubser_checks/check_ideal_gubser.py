2e83465dbc95b32c14d69f8d44ed821d461263b0
Christopher Plumberg <astrophysicist87@gmail.com>
2025-04-24 16:08:01 -0700

    Add YAML files and plotting scripts for Gubser 2+1D and 3+1D checks

---
+++ misc/Gubser_checks/check_ideal_gubser.py
@@ -0,0 +1,144 @@
+import h5py as h5
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+import numpy as np
+import sys
+
+use_log_scale = False
+
+infilename = sys.argv[1]
+outdirectory = sys.argv[2]
+axisMode = sys.argv[3]
+
+# HDF file structure
+f = h5.File(infilename, 'r')
+event = f['Event']
+n_timesteps = min([len(event.keys()),1000])
+event_keys = list(event.keys())
+
+q  = 1.
+e0 = 1.0
+
+Nc = 3.
+Nf = 2.5
+cp = (2.*(Nc**2-1.) + 3.5*Nc*Nf)*np.pi**2/90.
+
+quantities = ['T','e','ux','uy']
+cols = dict(zip(quantities,range(2,len(quantities)+2)))
+
+
+#===============================================================================
+def eGubser(tau, r):
+    return (e0/tau**4)*( (2.*q*tau)**(8./3.)
+                        / ( 1. + 2.*q**2*(tau**2 + r**2) + q**4*(tau**2 - r**2)**2 )**(4./3.)
+                         )
+
+#===============================================================================
+def TGubser(tau, r):
+    return ( eGubser(tau, r) / (3.*cp) )**0.25
+
+#===============================================================================
+def eFromT(T):
+    return 3.*cp*T**4
+
+#===============================================================================
+def urGubser(tau, r):
+    return 2.0*q**2*r*tau / np.sqrt( 1. + 2.*q**2*(tau**2 + r**2) + q**4*(tau**2 - r**2)**2 )
+
+#===============================================================================
+#def get_selection(Tmin, Tmax):
+#    frame    = event[event_keys[0]]
+#    return np.where( (T0 >= Tmin) & (T0 <= Tmax) & (eos_tags == 0) )
+
+
+#===============================================================================
+#def frame_to_array(i, Tmin, Tmax):
+#    frame = event[event_keys[i]]
+#    tau = frame.attrs['Time']
+#    T = np.array(frame['T'])
+#    return np.c_[ T[selection], np.array(frame['muB'])[selection],\
+#                  np.array(frame['muS'])[selection], np.array(frame['muQ'])[selection] ]
+
+
+#===============================================================================
+#def plot_all_particles():
+#    # Set up
+#    print('Setting up...', flush=True)
+#    global selection
+#    Tmin, Tmax = 400, 650
+#    selection = get_selection(Tmin, Tmax)
+#    data = np.stack([frame_to_array(i, Tmin, Tmax) for i in range(n_timesteps)])
+
+
+
+
+#===============================================================================
+def plot_slice(ax, hydroOutput, tau, axis, quantity):
+    # c : column of quantity to plot in array
+    c = cols[quantity]
+    if axis == '0':
+        yEqAxisData = hydroOutput[np.where( np.abs(hydroOutput[:,1]) < 1e-6 )]
+        ax.plot( yEqAxisData[:,0], yEqAxisData[:,c], 'r-' )
+        cf   = [TGubser, eGubser, urGubser, urGubser][c]
+        xpts = np.linspace(np.amin(yEqAxisData[:,0]), np.amax(yEqAxisData[:,0]), 1001)
+        ax.plot( xpts, cf(tau, xpts), 'b:' )
+    elif axis == 'x':
+        yeqxData = hydroOutput[np.where( np.isclose( hydroOutput[:,0], hydroOutput[:,1] ) )]
+        rpts = np.sqrt(yeqxData[:,0]**2 + yeqxData[:,1]**2)
+        ax.plot( rpts, yeqxData[:,c], 'r-' )
+        cf   = [TGubser, eGubser, urGubser, urGubser][c]
+        rpts = np.linspace(0.0, np.amax(rpts), 1001)
+        ax.plot( rpts, cf(tau, rpts), 'b:' )
+    
+
+#===============================================================================
+if __name__ == "__main__":
+
+    # set up figure
+    toPlot = ['T', 'e', 'ux']
+    
+    ncols = 3
+    nrows = 1
+    fig, axs = plt.subplots( ncols=ncols, nrows=nrows, figsize=(5*ncols, 5*nrows) )
+
+    # plot hydro output files
+    for timestep in event_keys:
+        # load Gubser check output files produced by hydro code
+        # (eventually) use format: x [fm], y [fm], e [1/fm^4], u_x, u_y, ...
+        frame = event[timestep]
+        tau = frame.attrs['Time']
+        print('tau=', tau)
+
+        print(frame.keys())
+        exit(1)
+
+        x = np.array(frame['x'])
+        y = np.array(frame['y'])
+        T = np.array(frame['T'])
+        e = np.array(frame['e'])
+        ux = np.array(frame['ux'])
+        uy = np.array(frame['uy'])
+
+        #exit(1)
+        #hydroOutput = np.loadtxt( checkfile, skiprows=1 )
+        hydroOutput = np.c_[ x, y, T, e, ux, uy ]
+
+        # plot comparison along y==0 slice
+        for i, ax in enumerate(axs.ravel()):
+            if use_log_scale and ['T','e'].count(toPlot[i]) > 0:
+                ax.set_yscale('log')
+            plot_slice( ax, hydroOutput, tau, axisMode, toPlot[i] )
+            if axisMode == '0':
+                ax.set_xlim([-4.5, 4.5])
+                ax.set_xlabel(r'$x$ (fm)')
+            else:
+                ax.set_xlim([0.0, 4.5])
+                ax.set_xlabel(r'$r$ (fm)')
+            if toPlot[i] == 'ux' or toPlot[i] == 'uy':
+                ax.set_ylim([-2.0, 2.0])
+            #ax.ylabel(r'$e$ (fm$^{-4}$)')
+            
+    #plt.show()
+    plt.savefig('./yeq' + axisMode + '_slice.pdf')
+    
---
+++ utilities/ideal_2+1d_gubser_ic_generator.py
@@ -0,0 +1,72 @@
+#!/usr/bin/env python
+
+import sys
+import numpy as np
+
+def check_input():
+    if len(sys.argv) != 2:
+        print(f"Usage: {sys.argv[0]} <output>")
+        sys.exit(1)
+
+def write_header(stepx, stepy, stepEta, xmin, ymin, etamin):
+    #TODO: parse from a trento input file
+    f = open(sys.argv[1], "w")
+    f.write(f"#0 {stepx} {stepy} {stepEta} 0 {xmin} {ymin} {etamin}\n")
+    return f
+
+
+tau0 = 1.0
+eps0 = 80
+n0 = 1.0
+q = 1.0
+
+# Define Gubser model functions
+def eps_a(r):
+    return (eps0 / tau0**(4./3.)) * (2 * q)**(8./3.) / \
+           (1 + 2 * q**2 * (tau0**2 + r**2) + (q**2 * (tau0**2 - r**2))**2)**(4./3.)
+def kappa(r):
+    return np.arctanh(2 * q**2 * tau0 * r / (1 + (q * tau0)**2 + (q * r)**2))
+def velocity_x(x, r):
+    return np.sinh(kappa(r)) * x / r if r != 0 else 0
+def velocity_y(y, r):
+    return np.sinh(kappa(r)) * y / r if r != 0 else 0
+
+def main():
+    check_input()
+    stepx = .025
+    stepy = .025
+    xmax = 5
+    ymax = 5
+    xmin = -xmax
+    ymin = -ymax
+    hbarc = 0.1973269804 
+    # scale for conformal EoS
+    Nc    = 3.0 # three colors
+    Nf    = 2.5 # u+d massless, s 'half massless'
+    cpLoc = np.pi*np.pi*(2.0*(Nc*Nc-1.0)+(7.0/2.0)*Nc*Nf)/90.0 #In units of fm^-4
+    T0   = hbarc # T0 = 1 fm^-1 = .197 GeV
+
+    #f = open(sys.argv[1], "r")
+    #lines = f.readlines()
+    #f.close()
+
+    f = write_header(stepx, stepy, .1, xmin, ymin, -.1)
+
+    for x in np.arange(xmin, xmax, stepx):
+        for y in np.arange(ymin, ymax, stepy):
+            r = np.sqrt(float(x)**2 + float(y)**2)
+            ux = velocity_x(float(x), r)
+            uy = velocity_y(float(y), r)
+            eps = eps_a(r)
+            pixx = 0.
+            piyy = 0.
+            pixy = 0.
+            pizz = 0.
+            #print(eps)
+            
+            f.write(f"{x} {y} 0 {eps} 0 0 0 {ux} {uy} 0 0 {pixx} {pixy} 0 {piyy} 0 {pizz}\n")
+
+    f.close()
+
+if __name__ == "__main__":
+    main()
