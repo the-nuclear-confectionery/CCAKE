a2f84a5938ed88f860d6cb177f2311c442717f40
Christopher Plumberg <astrophysicist87@gmail.com>
2025-05-09 10:42:55 -0700

    Another small adjustment

--- misc/Gubser_checks/check_ideal_3+1d_gubser.py
+++ misc/Gubser_checks/check_ideal_3+1d_gubser.py
@@ -72,7 +72,7 @@ def plot_slice(ax, hydroOutput, tau, axis, quantity):
     cf   = [None, None, None, shifted_eGubser, shifted_urGubser, shifted_uetaGubser][c]
     
     sliceData = hydroOutput[np.where( (np.isclose(hydroOutput[:,1], 0.0, atol=1e-2)) \
-                                      & (np.isclose(hydroOutput[:,2], eta0, atol=3e-2)) )] # y == 0 ===>>> r == x
+                                      & (np.isclose(hydroOutput[:,2], eta0, atol=5e-2)) )] # y == 0 ===>>> r == x
     ax.plot( sliceData[:,0], sliceData[:,c], 'r-' )
     xpts = np.linspace(np.amin(sliceData[:,0]), np.amax(sliceData[:,0]), 1001)
     ax.plot( xpts, cf(tau, xpts, eta0), 'b:' )
