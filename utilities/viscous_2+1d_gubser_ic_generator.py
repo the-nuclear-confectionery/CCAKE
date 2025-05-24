3a51c4750f3808da72a298c4999078a6dfae55fd
Christopher Plumberg <astrophysicist87@gmail.com>
2025-05-24 15:02:33 -0700

    Boost invariant viscous script and YAML file

---
+++ utilities/viscous_2+1d_gubser_ic_generator.py
@@ -0,0 +1,138 @@
+#!/usr/bin/env python
+
+import sys
+import numpy as np
+from scipy.special import hyp2f1
+
+def check_input():
+    if len(sys.argv) != 2:
+        print(f"Usage: {sys.argv[0]} <output>")
+        sys.exit(1)
+
+def write_header(stepx, stepy, stepEta, xmin, ymin, etamin):
+    #TODO: parse from a trento input file
+    f = open(sys.argv[1], "w")
+    f.write(f"#0 {stepx} {stepy} {stepEta} 0 {xmin} {ymin} {etamin}\n")
+    return f
+
+hbarc = 0.1973269804              # GeV*fm
+tau0 = 1.0                        # fm
+eps0 = 80                         # (GeV*fm)^4
+shearOVERs = 0.134                # i.e., specific shear viscosity (eta/s)
+q = 1.0                           # 1/fm
+
+
+Nc = 3.
+Nf = 2.5
+cp = (2.*(Nc**2-1.) + 3.5*Nc*Nf)*np.pi**2/90.
+fs = cp                         # dimensionless
+
+# derived parameters
+H0 = 4.0*fs**0.25*shearOVERs/3.0  # dimensionless
+T0 = 0.25*eps0**0.25*fs**0.75     # dimensionless
+
+H0_by_9T0 = H0 / (9.0*T0)
+
+
+#==============================================================================
+# Define Gubser model functions
+#==============================================================================
+def kappa(tau, r):
+    return np.arctanh(2 * q**2 * tau * r / (1 + (q * tau)**2 + (q * r)**2))
+#==============================================================================
+def rho(tau,r):
+    return np.arcsinh( (q**2 * (tau**2 - r**2) - 1.) / (2. * q * tau) )
+#==============================================================================
+def T_a(tau, r):
+    s = np.sinh(rho(tau,r))
+    return (hbarc/(tau * fs**0.25)) * ( T0 / np.cosh(rho(tau, r))**(2./3.) ) \
+            * ( 1.0 + H0_by_9T0 * s**3 * hyp2f1(3./2., 7./6., 5./2., -s**2) )
+#==============================================================================
+def eps_a(tau, r):
+    return fs*T_a(tau, r)**4/hbarc**3
+#==============================================================================
+def velocity_tau(tau, r):
+    return np.cosh(kappa(tau, r))
+#==============================================================================
+def velocity_x(tau, x, r):
+    return np.sinh(kappa(tau, r)) * x / r if r != 0 else 0
+#==============================================================================
+def velocity_y(tau, y, r):
+    return np.sinh(kappa(tau, r)) * y / r if r != 0 else 0
+#==============================================================================
+def pimunu(tau, x, y, r):
+    shear = H0*(hbarc**3*eps_a(tau, r))**0.75
+    prefactor = 4.*shear*np.tanh(rho(tau, r))/(3.*tau*hbarc**2) # N.B. - missing minus sign and fixed power of tau relative to 2503.XXXXX
+    ux = velocity_x(tau, x, r)
+    uy = velocity_y(tau, y, r)
+    utau = velocity_tau(tau, r)
+    return prefactor * np.array([[ux**2+uy**2, ux*utau, uy*utau,           0],
+                                 [ux*utau,     1+ux**2,   ux*uy,           0],
+                                 [uy*utau,       ux*uy, 1+uy**2,           0],
+                                 [0,                  0,       0, -2./tau**2]])
+#==============================================================================
+
+def main():
+    #print('eps_a(1,0) =', eps_a(1.0,0.0))
+    #if True:
+    #    exit(1)
+       
+    
+    check_input()
+    stepx = 0.025
+    stepy = 0.025
+    xmax = 5.0
+    ymax = 5.0
+    xmin = -xmax
+    ymin = -ymax
+    #hbarc = 0.1973269804 
+    # scale for conformal EoS
+    #Nc    = 3.0 # three colors
+    #Nf    = 2.5 # u+d massless, s 'half massless'
+    #cpLoc = np.pi*np.pi*(2.0*(Nc*Nc-1.0)+(7.0/2.0)*Nc*Nf)/90.0 #In units of fm^-4
+    #T0   = hbarc # T0 = 1 fm^-1 = .197 GeV
+
+    #f = open(sys.argv[1], "r")
+    #lines = f.readlines()
+    #f.close()
+    
+    #if True:
+    #    for tau in np.arange(1.0, 1.2, 0.001):
+    #        print('tau =', tau, ':', shifted_pimunu(tau, 0.0, 0.0, 0.0, 0.0)[1,1])
+    #    exit(1)
+    
+    f = write_header(stepx, stepy, .1, xmin, ymin, -.1)
+
+    for x in np.arange(xmin, xmax+stepx, stepx):
+        for y in np.arange(ymin, ymax+stepy, stepy):
+            r = np.sqrt(float(x)**2 + float(y)**2)
+            ux       = velocity_x(tau0, float(x), r)
+            uy       = velocity_y(tau0, float(y), r)
+            eps      = eps_a(tau0, r)
+            piM      = pimunu(tau0, x, y, r)
+            pixx     = piM[1,1]
+            piyy     = piM[2,2]
+            pixy     = piM[1,2]
+            pixeta   = piM[1,3]
+            piyeta   = piM[2,3]
+            pietaeta = piM[3,3]
+            #utau = np.sqrt(1.0+ux**2+uy**2+(tau0*ueta)**2)
+            #print('----------------------------------------')
+            #np.set_printoptions(precision=8)
+            #print('u =',utau,ux,uy,tau0**2*ueta)
+            #print('pi =',piM)
+            #if True:
+            #    exit(1)
+            #print('tr(pi) =',piM[0,0]-piM[1,1]-piM[2,2]-tau0**2*piM[3,3])
+            #print('u*pi(0) =',utau*piM[0,0]-ux*piM[0,1]-uy*piM[0,2]-tau0**2*ueta*piM[0,3])
+            #print('u*pi(1) =',utau*piM[1,0]-ux*piM[1,1]-uy*piM[1,2]-tau0**2*ueta*piM[1,3])
+            #print('u*pi(2) =',utau*piM[2,0]-ux*piM[2,1]-uy*piM[2,2]-tau0**2*ueta*piM[2,3])
+            #print('u*pi(3) =',utau*piM[3,0]-ux*piM[3,1]-uy*piM[3,2]-tau0**2*ueta*piM[3,3])
+            #print(eps)
+            
+            f.write(f"{x} {y} 0 {eps} 0 0 0 {ux} {uy} {ueta} 0 {pixx} {pixy} {pixeta} {piyy} {piyeta} {pietaeta}\n")
+
+    f.close()
+
+if __name__ == "__main__":
+    main()
