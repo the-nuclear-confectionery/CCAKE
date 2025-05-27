e91ce461acb85948c57095d7e760f67ce90af62f
Christopher Plumberg <astrophysicist87@gmail.com>
2025-05-27 14:11:02 -0700

    Getting negative entropy density.  Try better numbers?

--- utilities/viscous_2+1d_gubser_ic_generator.py
+++ utilities/viscous_2+1d_gubser_ic_generator.py
@@ -17,9 +17,9 @@ def write_header(stepx, stepy, stepEta, xmin, ymin, etamin):
 
 hbarc = 0.1973269804              # GeV*fm
 tau0 = 1.0                        # fm
-eps0 = 80                         # (GeV*fm)^4
+eps0 = 175                         # (GeV*fm)^4
 shearOVERs = 0.134                # i.e., specific shear viscosity (eta/s)
-q = 1.0                           # 1/fm
+q = 1.0/4.3                           # 1/fm
 
 
 Nc = 3.
