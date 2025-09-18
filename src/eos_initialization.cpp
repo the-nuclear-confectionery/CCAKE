63be781e98331d81979301c7b5d909de80c7f6a3
kevinpala <kevin.p.pala@gmail.com>
2025-09-18 15:38:09 -0500

    Reverted EOS parameters

--- src/eos_initialization.cpp
+++ src/eos_initialization.cpp
@@ -330,7 +330,7 @@ void EquationOfState::set_up_chosen_EOSs()
 
 	    double TmaxIni   = settingsPtr->Freeze_Out_Temperature/constants::hbarc_MeVfm;
 	    double Tmax      = 1.1 * TmaxIni;
-      Tmax   = maxima[0];
+      //Tmax   = maxima[0];
       formatted_output::detail("Tmax = " + to_string(Tmax));
       // set overall scale using (Tmax,0,0,0)
       tbqs( Tmax, 0.0, 0.0, 0.0, p_default_EoS );
@@ -373,7 +373,7 @@ void EquationOfState::set_up_chosen_EOSs()
 
 	    double TmaxIni   = settingsPtr->Freeze_Out_Temperature/constants::hbarc_MeVfm;
 	    double Tmax      = 1.1 * TmaxIni;
-      Tmax = maxima[0];
+      //Tmax = maxima[0];
 
       // set overall scale using (Tmax,0,0,0)
       tbqs( Tmax, 0.0, 0.0, 0.0, p_default_EoS );
